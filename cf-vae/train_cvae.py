d35fe0c870936f74044f57ec39ffec6750253c82
zakosai <linh.nguyen.1992@gmail.com>
2018-02-05 12:10:21 +0900

    change setting for amazon dataset

--- cf-vae/train_cvae.py
+++ cf-vae/train_cvae.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 import scipy.io
 import matplotlib.pyplot as plt
 from cf_vae_cpmf import cf_vae, params
+from scipy.sparse import load_npz
 
 
 np.random.seed(0)
@@ -10,10 +11,11 @@ tf.set_random_seed(0)
 
 def load_cvae_data():
   data = {}
-  data_dir = "data/citeulike-a/"
-  variables = scipy.io.loadmat(data_dir + "mult_nor.mat")
-  data["content"] = variables['X']
-
+  data_dir = "data/amazon/"
+  # variables = scipy.io.loadmat(data_dir + "mult_nor.mat")
+  # data["content"] = variables['X']
+  variables = load_npz("data/amazon/mult_nor.npz")
+  data["content"] = variables.toarray()
   data["train_users"] = load_rating(data_dir + "cf-train-1-users.dat")
   data["train_items"] = load_rating(data_dir + "cf-train-1-items.dat")
   data["test_users"] = load_rating(data_dir + "cf-test-1-users.dat")
@@ -50,7 +52,7 @@ params.max_iter_m = 1
 
 data = load_cvae_data()
 num_factors = 50
-model = cf_vae(num_users=5551, num_items=16980, num_factors=num_factors, params=params,
+model = cf_vae(num_users=768438, num_items=166049, num_factors=num_factors, params=params,
     input_dim=8000, encoding_dims=[200, 100], z_dim = 50, decoding_dims=[100, 200, 8000],
     loss_type='cross_entropy')
 model.fit(data["train_users"], data["train_items"], data["content"], params)
