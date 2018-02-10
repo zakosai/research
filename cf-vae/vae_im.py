ef29556d3eea931e686e7da23fffd67072daffb7
zakosai <linh.nguyen.1992@gmail.com>
2018-02-10 20:42:39 +0900

    train again image

--- cf-vae/vae_im.py
+++ cf-vae/vae_im.py
@@ -86,7 +86,7 @@ class vanilla_vae:
         sess = tf.Session()
         sess.run(tf.global_variables_initializer())
         saver = tf.train.Saver(tf.get_collection(tf.GraphKeys.VARIABLES, scope=scope))
-        ckpt_file = "pre_model/" + "vae_%s_2.ckpt" %scope
+        ckpt_file = "pre_model/" + "vae_%s.ckpt" %scope
         if train == True:
             # num_turn = x_input.shape[0] / self.batch_size
             start = time.time()
