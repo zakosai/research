9481d3cf80cf96de9cb704078f26d310c8248496
zakosai <linh.nguyen.1992@gmail.com>
2018-02-27 14:35:10 +0900

    new denoising

---
+++ cf-vae/train_dae.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+import numpy as np
+from dae import vanilla_vae
+import scipy.io as sio
+from scipy.sparse import load_npz
+
+np.random.seed(0)
+tf.set_random_seed(0)
+
+# variables = sio.loadmat("data/citeulike-a/mult_nor.mat")
+# data = variables['X']
+variables = load_npz("data/amazon/mult_nor-small.npz")
+data = variables.toarray()
+idx = np.random.rand(data.shape[0]) < 0.8
+train_X = data[idx]
+test_X = data[~idx]
+
+#
+# images = np.fromfile("data/amazon/images.bin")
+# images = images.reshape((16000, 3072))
+# train_img = images[idx]
+# test_img = images[~idx]
+
+model = vanilla_vae(input_dim=8000, encoding_dims=[200, 100], z_dim=50, decoding_dims=[100, 200, 8000], loss='cross_entropy')
+# As there will be an additional layer from 100 to 50 in the encoder. in decoder, we also take this layer
+                    # lr=0.01, batch_size=128, print_step=50)
+print('fitting data starts...')
+model.fit(train_X, epochs=10000,learning_rate=0.001, batch_size=500, print_size=50, train=True, scope="structure")
