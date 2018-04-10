b2180f7b87ce80d75785c723dd1df1fe41989bb5
zakosai <linh.nguyen.1992@gmail.com>
2018-04-10 18:12:53 +0900

    add layer

--- cf-vae/cf_vae_cpmf_extend.py
+++ cf-vae/cf_vae_cpmf_extend.py
@@ -77,8 +77,8 @@ class cf_vae_extend:
         with tf.variable_scope("text"):
             x = self.x_
             depth_inf = len(self.encoding_dims)
-            noisy_level = 1
-            x = x + noisy_level*tf.random_normal(tf.shape(x))
+            # noisy_level = 1
+            # x = x + noisy_level*tf.random_normal(tf.shape(x))
             for i in range(depth_inf):
                 x = dense(x, self.encoding_dims[i], scope="enc_layer"+"%s" %i, activation=tf.nn.sigmoid)
                 # print("enc_layer0/weights:0".graph)
