61155741c29bd7be76384a7c93ab0af2e9585c1f
zakosai <linh.nguyen.1992@gmail.com>
2018-04-10 21:48:15 +0900

    image fix

--- cf-vae/vae_im.py
+++ cf-vae/vae_im.py
@@ -80,10 +80,10 @@ class vanilla_vae:
 
             x = block_layer(inputs=x, filters=512, block_fn=building_block, blocks=num_blocks,
                                  strides=2, is_training=is_training, name='block_layer4', data_format=data_format)
-            # x = block_layer(inputs=x, filters=512, block_fn=building_block, blocks=num_blocks,
-            #                      strides=2, is_training=is_training, name='block_layer5', data_format=data_format)
-            # x = block_layer(inputs=x, filters=512, block_fn=building_block, blocks=num_blocks,
-            #                      strides=2, is_training=is_training, name='block_layer5', data_format=data_format)
+            x = block_layer(inputs=x, filters=512, block_fn=building_block, blocks=num_blocks,
+                                 strides=2, is_training=is_training, name='block_layer5', data_format=data_format)
+            x = block_layer(inputs=x, filters=512, block_fn=building_block, blocks=num_blocks,
+                                 strides=2, is_training=is_training, name='block_layer5', data_format=data_format)
             flat = Flatten()(x)
             print(flat.shape)
             h_encode = Dense(self.intermediate_dim, activation='relu')(flat)
@@ -95,8 +95,8 @@ class vanilla_vae:
 
             # generative process
             h_decode = dense(z, self.intermediate_dim, activation=tf.nn.relu)
-            h_upsample = dense(h_decode, 8192, activation=tf.nn.relu)
-            y = Reshape((4,4,512))(h_upsample)
+            h_upsample = dense(h_decode, 512, activation=tf.nn.relu)
+            y = Reshape((1,1,512))(h_upsample)
 
             # for i in range(self.num_conv-1):
             #     y = conv2d_transpose(y, self.filter*np.power(2,self.num_conv-2-i), kernel_size=(2,2),
@@ -105,8 +105,8 @@ class vanilla_vae:
             # y = conv2d_transpose(y, self.channel, scope="dec_layer"+"%s" %(self.num_conv-1) , kernel_size=(2,2),
             #                          strides=(2,2), activation=tf.nn.relu)
                     # if last_layer_nonelinear: depth_gen -1
-            # y = conv2d_transpose(y, 512, kernel_size=(3,3), strides=(2,2), scope="dec_layer0", activation=tf.nn.relu)
-            # y = conv2d_transpose(y, 512, kernel_size=(3,3), strides=(2,2), scope="dec_layer1", activation=tf.nn.relu)
+            y = conv2d_transpose(y, 512, kernel_size=(3,3), strides=(2,2), scope="dec_layer0", activation=tf.nn.relu)
+            y = conv2d_transpose(y, 512, kernel_size=(3,3), strides=(2,2), scope="dec_layer1", activation=tf.nn.relu)
             y = conv2d_transpose(y, 256, kernel_size=(3,3), strides=(2,2), scope="dec_layer2", activation=tf.nn.relu)
             y = conv2d_transpose(y, 128, kernel_size=(3,3), strides=(2,2), scope="dec_layer3", activation=tf.nn.relu)
             y = conv2d_transpose(y, 64, kernel_size=(3,3), strides=(2,2), scope="dec_layer4", activation=tf.nn.relu)
