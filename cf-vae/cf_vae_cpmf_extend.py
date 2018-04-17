612cf0d7b8849836de480c4e9933e396fd815f92
zakosai <linh.nguyen.1992@gmail.com>
2018-04-17 22:28:11 +0900

    precision, mapk

--- cf-vae/cf_vae_cpmf_extend.py
+++ cf-vae/cf_vae_cpmf_extend.py
@@ -14,6 +14,7 @@ import scipy
 import scipy.io as sio
 from operator import add
 from resnet_model import conv2d_fixed_padding, building_block, block_layer
+import ml_metrics
 
 class params:
     def __init__(self):
@@ -358,19 +359,30 @@ class cf_vae_extend:
         pred_all = list(pred_all)
 
         recall_avgs = []
-        for m in range(50, M, 50):
+        precision_avgs = []
+        mapk_avgs = []
+        for m in range(5, M, 5):
             print "m = " + "{:>10d}".format(m) + "done"
             recall_vals = []
+            precision_vals = []
             for i in range(len(user_all)):
                 top_M = np.argsort(-pred_all[i])[0:m]
                 hits = set(top_M) & set(user_all[i])   # item idex from 0
                 hits_num = len(hits)
                 recall_val = float(hits_num) / float(ground_tr_num[i])
                 recall_vals.append(recall_val)
+                precision = float(hits_num) / float(m)
+                precision_vals.append(precision)
+
             recall_avg = np.mean(np.array(recall_vals))
-            print recall_avg
+            precision_avg = np.mean(np.array(precision_vals))
+            mapk = ml_metrics.mapk([list(np.argsort(-k)) for k in pred_all], user_all, m)
+            print recall_avg, precision_avg
             recall_avgs.append(recall_avg)
-        return recall_avgs
+            precision_avgs.append(precision_avg)
+            mapk_avgs.append(mapk)
+
+        return recall_avgs, precision_avgs, mapk_avgs
 
     def predict_all(self):
         return np.dot(self.U, (self.V.T))
