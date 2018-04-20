4b327c3dc61ac31561095ac2751d0585cb63c153
zakosai <linh.nguyen.1992@gmail.com>
2018-04-20 11:17:14 +0900

    check

--- cf-vae/vae_im.py
+++ cf-vae/vae_im.py
@@ -112,11 +112,12 @@ class vanilla_vae:
             y = conv2d_transpose(y, 64, kernel_size=(3,3), strides=(2,2), scope="dec_layer4", activation=tf.nn.relu)
             y = conv2d_transpose(y, 3, kernel_size=(3,3), strides=(2,2), scope="dec_layer5", activation=tf.nn.relu)
             x_recons = y
-
+        print(Flatten()(x).shape)
+        print(binary_crossentropy(Flatten()(x_), Flatten()(x_recons)).shape())
         loss_recons = tf.reduce_mean(tf.reduce_sum(binary_crossentropy(Flatten()(x_), Flatten()(x_recons)), axis=1))
         loss_kl = 0.5 * tf.reduce_mean(tf.reduce_sum(tf.square(z_mu) + tf.exp(z_log_sigma_sq) - z_log_sigma_sq - 1, 1))
         # loss_kl = 0.5 * tf.reduce_mean(tf.reduce_sum(tf.square(z_mu) + tf.exp(z_log_sigma_sq) - z_log_sigma_sq - 1, 1))
-        loss = loss_recons + loss_kl
+        loss = K.mean(loss_recons + loss_kl)
         # other cases not finished yet
         train_op = tf.train.AdamOptimizer(self.learning_rate).minimize(loss)
 
