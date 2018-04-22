f1fc27fef0cb9b82448c4aead6f32c9a3c6cc540
zakosai <linh.nguyen.1992@gmail.com>
2018-04-22 19:38:58 +0900

    u

--- cf-vae/train_cvae_extend.py
+++ cf-vae/train_cvae_extend.py
@@ -61,8 +61,8 @@ def load_rating(path):
 
 
 params = params()
-params.lambda_u = 0.1
-params.lambda_v = 100
+params.lambda_u = 0.01
+params.lambda_v = 10
 params.lambda_r = 1
 params.C_a = 1
 params.C_b = 0.01
@@ -118,7 +118,7 @@ num_factors = 500
 
 
 model = cf_vae_extend(num_users=8000, num_items=16000, num_factors=num_factors, params=params,
-                      input_dim=8000, encoding_dims=[2000, 1000], z_dim = 500, decoding_dims=[1000, 2000, 8000],
+                      input_dim=8000, encoding_dims=[1000, 200], z_dim = 50, decoding_dims=[200, 1000, 8000],
                       decoding_dims_str=[100,200, 1863], loss_type='cross_entropy', model = model_type, ckpt_folder=ckpt, initial=initial)
 model.fit(data["train_users"], data["train_items"], data["content"],img, data["structure"], params)
 model.save_model(os.path.join(ckpt,"cf_vae_%d.mat"%(model_type)))
