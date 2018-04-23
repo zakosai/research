a14c00f9b8792f39e0b7ca1e71a8326dddd00750
zakosai <linh.nguyen.1992@gmail.com>
2018-04-23 12:36:39 +0900

    r

--- cf-vae/train_cvae_extend.py
+++ cf-vae/train_cvae_extend.py
@@ -63,7 +63,7 @@ def load_rating(path):
 params = params()
 params.lambda_u = 0.1
 params.lambda_v = 10
-params.lambda_r = 0.1
+params.lambda_r = 10
 params.C_a = 1
 params.C_b = 0.01
 params.max_iter_m = 1
