d191ccdb7ea88354fdbcdd3cf1e1ec8411000c0b
zakosai <linh.nguyen.1992@gmail.com>
2018-04-27 21:33:01 +0900

    ave

--- cf-vae/train_cvae_extend.py
+++ cf-vae/train_cvae_extend.py
@@ -97,7 +97,7 @@ for u in [0.01, 0.1, 1]:
             params.lambda_r = r
 
             model = cf_vae_extend(num_users=8000, num_items=16000, num_factors=num_factors, params=params,
-                                  input_dim=8000, encoding_dims=[2000, 1000], z_dim = 500, decoding_dims=[1000, 2000, 8000],
+                                  input_dim=8000, encoding_dims=[1000, 200], z_dim = 50, decoding_dims=[200, 1000, 8000],
                                   decoding_dims_str=[100,200, 1863], loss_type='cross_entropy', model = model_type, ckpt_folder=ckpt, initial=initial)
             model.fit(data["train_users"], data["train_items"], data["content"],img, data["structure"], params)
             model.save_model(os.path.join(ckpt,"cf_vae_%d_%d.mat"%(model_type, i)))
