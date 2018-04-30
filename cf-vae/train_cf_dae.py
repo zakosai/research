2aec4fc6f5edfb061ebc77789b1ebf5e46acae4d
zakosai <linh.nguyen.1992@gmail.com>
2018-04-30 20:12:03 +0900

    dae

--- cf-vae/train_cf_dae.py
+++ cf-vae/train_cf_dae.py
@@ -85,7 +85,7 @@ img = img.astype(np.float32)/255
 num_factors = 500
 model = cf_vae_extend(num_users=8000, num_items=16000, num_factors=num_factors, params=params,
     input_dim=8000, encoding_dims=[2000, 1000], z_dim = 500, decoding_dims=[1000, 2000, 8000],
-    decoding_dims_str=[100,200, 1863], loss_type='cross_entropy')
+    decoding_dims_str=[100,200, 1863], loss_type='cross_entropy', model=model_type)
 model.fit(data["train_users"], data["train_items"], data["content"],img, data["content"], params)
 model.save_model(os.path.join(ckpt,"cf_dae.mat"))
 # model.load_model("cf_vae.mat")
