96f92c48218e91f580cec3870d9d003ed76ad17e
zakosai <linh.nguyen.1992@gmail.com>
2018-05-01 20:03:23 +0900

    retrain

--- cf-vae/train.sh
+++ cf-vae/train.sh
@@ -5,9 +5,9 @@ for i in 20 50 100
 do
     #mkdir grocery/z$i
     #python train_vae.py --ckpt_folder=grocery/z$i --data_dir=data/amazon2/ --zdim=$i
-    python train_cvae_extend.py --model=0 --ckpt_folder=grocery/z$i --data_dir=data/amazon2/ --iter=15 --zdim=$i
+    python train_cvae_extend.py --model=0 --ckpt_folder=grocery/z$i_m --data_dir=data/amazon2/ --iter=15 --zdim=$i
     #python train_img_vae.py --ckpt_folder=grocery/z$i --data_dir=data/amazon2/ --zdim=$i
-    python train_cvae_extend.py --model=1 --ckpt_folder=grocery/z$i --data_dir=data/amazon2/ --iter=15 --zdim=$i
+    python train_cvae_extend.py --model=1 --ckpt_folder=grocery/z$i_m --data_dir=data/amazon2/ --iter=15 --zdim=$i
 done
 
 #
