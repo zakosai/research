8faf70afa4870a526ed261f72f37a5945549d1f8
zakosai <linh.nguyen.1992@gmail.com>
2018-05-01 12:46:04 +0900

    zdim100

--- cf-vae/train_vae.py
+++ cf-vae/train_vae.py
@@ -34,8 +34,8 @@ test_X = data[~idx]
 # train_img = images[idx]
 # test_img = images[~idx]
 
-model = vanilla_vae(input_dim=8000, encoding_dims=[2000, 400], z_dim=100, decoding_dims=[400, 2000, 8000], loss='cross_entropy', ckpt_folder=ckpt)
+model = vanilla_vae(input_dim=8000, encoding_dims=[1000, 200], z_dim=100, decoding_dims=[200, 1000, 8000], loss='cross_entropy', ckpt_folder=ckpt)
 # As there will be an additional layer from 100 to 50 in the encoder. in decoder, we also take this layer
                     # lr=0.01, batch_size=128, print_step=50)
 print('fitting data starts...')
-model.fit(train_X, epochs=10000,learning_rate=0.001, batch_size=500, print_size=50, train=True, scope="text")
+model.fit(train_X, epochs=1000,learning_rate=0.001, batch_size=500, print_size=50, train=True, scope="text")
