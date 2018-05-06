3e1f0673789841fe5b7d652fd25ea9246a83b1b0
zakosai <linh.nguyen.1992@gmail.com>
2018-05-06 20:12:48 +0900

    ave

--- cf-vae/train.sh
+++ cf-vae/train.sh
@@ -1,15 +1,17 @@
 #python train_cvae_extend.py --model=6 --ckpt_folder=grocery/z4_50 --data_dir=data/amazon2/ --iter=70 --zdim=50
-#python train_cvae_extend.py --model=0 --ckpt_folder=grocery/z4_50 --data_dir=data/amazon2/ --iter=70 --zdim=50
-python train_cvae_extend.py --model=1 --ckpt_folder=grocery/z6_50 --data_dir=data/amazon2/ --iter=70 --zdim=50
+python train_cvae_extend.py --model=0 --ckpt_folder=grocery/z4_50 --data_dir=data/amazon2/ --iter=150 --zdim=50
+python train_cvae_extend.py --model=1 --ckpt_folder=grocery/z6_50 --data_dir=data/amazon2/ --iter=150 --zdim=50
 
+python train_vae.py --ckpt_folder=pre2/z6_50 --data_dir=data/movie/ --zdim=50
+python train_img_vae.py --ckpt_folder=pre2/z6_50 --data_dir=data/movie/ --zdim=50
 python train_cvae_extend.py --model=0 --ckpt_folder=pre2/z6_50 --data_dir=data/movie/ --iter=100 --zdim=50
-python train_cvae_extend.py --model=6 --ckpt_folder=pre2/z6_50 --data_dir=data/movie/ --iter=100 --zdim=50
+#python train_cvae_extend.py --model=6 --ckpt_folder=pre2/z6_50 --data_dir=data/movie/ --iter=100 --zdim=50
 python train_cvae_extend.py --model=1 --ckpt_folder=pre2/z6_50 --data_dir=data/movie/ --iter=100 --zdim=50
 
-mkdir pre2/z6_100
-python train_vae.py --ckpt_folder=pre2/z6_100 --data_dir=data/movie/ --zdim=100
-python train_img_vae.py --ckpt_folder=pre2/z6_100 --data_dir=data/movie/ --zdim=100
-python train_cvae_extend.py --model=1 --ckpt_folder=pre2/z6_100 --data_dir=data/movie/ --iter=100 --zdim=100
+#mkdir pre2/z6_100
+#python train_vae.py --ckpt_folder=pre2/z6_100 --data_dir=data/movie/ --zdim=100
+#python train_img_vae.py --ckpt_folder=pre2/z6_100 --data_dir=data/movie/ --zdim=100
+#python train_cvae_extend.py --model=1 --ckpt_folder=pre2/z6_100 --data_dir=data/movie/ --iter=100 --zdim=100
 
 #for i in 50 100 150
 #do
