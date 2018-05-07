4e9cf26c9b0c8a1a7dee3bf49accd5afe313e263
zakosai <linh.nguyen.1992@gmail.com>
2018-05-07 12:41:47 +0900

    ave

--- cf-vae/train_ave.py
+++ cf-vae/train_ave.py
@@ -20,10 +20,10 @@ dir = args.data_dir
 np.random.seed(0)
 tf.set_random_seed(0)
 
-# variables = sio.loadmat("data/citeulike-a/mult_nor.mat")
-# data = variables['X']
-variables = load_npz(os.path.join(dir, "mult_nor.npz"))
-data = variables.toarray()
+variables = sio.loadmat("data/citeulike-a/mult_nor.mat")
+data = variables['X']
+# variables = load_npz(os.path.join(dir, "mult_nor.npz"))
+# data = variables.toarray()
 idx = np.random.rand(data.shape[0]) < 0.8
 train_X = data[idx]
 test_X = data[~idx]
