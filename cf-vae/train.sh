dfa8c6c9123c2bad5ebc470d3a0fd9a5e82b707d
zakosai <linh.nguyen.1992@gmail.com>
2018-05-07 21:22:36 +0900

    ave

--- cf-vae/train.sh
+++ cf-vae/train.sh
@@ -9,15 +9,15 @@
 #python train_cvae_extend.py --model=1 --ckpt_folder=pre2/z6_50 --data_dir=data/movie/ --iter=100 --zdim=50
 
 #python train_ave.py --ckpt_folder=citeu/z1_50 --data_dir=data/citeulike-a/
-python train_cave.py --model=0 --ckpt_folder=citeu/z1_50 --data_dir=data/citeulike-a/ --iter=200 --zdim=50
-mkdir -p citeut/z1_50
-python train_ave.py --ckpt_folder=citeut/z1_50 --data_dir=data/citeulike-t/
-python train_cave.py --model=0 --ckpt_folder=citeut/z1_50 --data_dir=data/citeulike-t/ --iter=200 --zdim=50
+python train_cave.py --model=0 --ckpt_folder=citeu/z2_50 --data_dir=data/citeulike-a/ --iter=200 --zdim=50
+mkdir -p citeut/z2_50
+python train_ave.py --ckpt_folder=citeut/z2_50 --data_dir=data/citeulike-t/
+python train_cave.py --model=0 --ckpt_folder=citeut/z2_50 --data_dir=data/citeulike-t/ --iter=200 --zdim=50
 git pull origin resnet
-mkdir citeu/z2_50
+mkdir citeu/z3_50
 
-python train_ave.py --ckpt_folder=citeu/z1_50 --data_dir=data/citeulike-a/
-python train_cave.py --model=0 --ckpt_folder=citeu/z1_50 --data_dir=data/citeulike-a/ --iter=200 --zdim=50
+python train_ave.py --ckpt_folder=citeu/z3_50 --data_dir=data/citeulike-a/
+python train_cave.py --model=0 --ckpt_folder=citeu/z3_50 --data_dir=data/citeulike-a/ --iter=200 --zdim=50
 #mkdir pre2/z6_100
 #python train_vae.py --ckpt_folder=pre2/z6_100 --data_dir=data/movie/ --zdim=100
 #python train_img_vae.py --ckpt_folder=pre2/z6_100 --data_dir=data/movie/ --zdim=100
