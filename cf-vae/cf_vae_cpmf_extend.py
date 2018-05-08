4cb6dcf37a4f1e506fcde2728d93f9feff641746
zakosai <linh.nguyen.1992@gmail.com>
2018-05-08 14:03:31 +0900

    ave

--- cf-vae/cf_vae_cpmf_extend.py
+++ cf-vae/cf_vae_cpmf_extend.py
@@ -194,8 +194,10 @@ class cf_vae_extend:
 
         if self.loss_type == "cross_entropy":
             if self.model != 6:
-                loss_recons = tf.reduce_mean(tf.reduce_sum(binary_crossentropy(self.x_, x_recons), axis=1))
-                loss_kl = 0.5 * tf.reduce_mean(tf.reduce_sum(tf.square(z_mu) + tf.exp(z_log_sigma_sq) - z_log_sigma_sq - 1, 1))
+                loss_recons = -tf.reduce_mean(tf.reduce_sum(self.x_ * tf.log(tf.maximum(x_recons, 1e-10))
+                + (1-self.x_) * tf.log(tf.maximum(1 - x_recons, 1e-10)),1))
+                loss_kl = 0.5 * tf.reduce_mean(tf.reduce_sum(tf.square(z_mu) + tf.exp(z_log_sigma_sq)
+            - z_log_sigma_sq - 1, 1))
             else:
                 loss_im_recons = self.input_width * self.input_height * metrics.binary_crossentropy(K.flatten(x_im_), K.flatten(x_im_recons))
                 loss_im_kl = 0.5 * tf.reduce_sum(tf.square(z_im_mu) + tf.exp(z_im_log_sigma_sq) - z_im_log_sigma_sq - 1, 1)
