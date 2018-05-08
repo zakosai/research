d91bf559692418f4f2383cb029463a79edc32ad3
zakosai <linh.nguyen.1992@gmail.com>
2018-05-08 10:18:14 +0900

    ave

--- cf-vae/cf_vae_cpmf_extend.py
+++ cf-vae/cf_vae_cpmf_extend.py
@@ -497,7 +497,7 @@ class cf_vae_extend:
         recall_avgs = []
         precision_avgs = []
         mapk_avgs = []
-        for m in range(1, M, 1):
+        for m in range(50, M, 300):
             print "m = " + "{:>10d}".format(m) + "done"
             recall_vals = []
             apk_vals = []
