389448c561a45715e0cd0e41952a974d439b1a69
zakosai <linh.nguyen.1992@gmail.com>
2018-05-08 15:14:18 +0900

    ave

--- cf-vae/vae.py
+++ cf-vae/vae.py
@@ -42,11 +42,16 @@ class vanilla_vae:
             x_ = placeholder((None, self.input_dim))
             x = x_
             depth_inf = len(self.encoding_dims)
+            reg_loss = 0
 
             # noisy_level = 1
             # x = x + noisy_level*tf.random_normal(tf.shape(x))
             for i in range(depth_inf):
                 x = dense(x, self.encoding_dims[i], scope="enc_layer"+"%s" %i, activation=tf.nn.sigmoid)
+                with tf.variable_scope("enc_layer%s"%i) as scope:
+                        scope.reuse_variables()
+                        w = tf.get_variable("weights")
+                    reg_loss += tf.nn.l2_loss(w)
             h_encode = x
             z_mu = dense(h_encode, self.z_dim, scope="mu_layer")
             z_log_sigma_sq = dense(h_encode, self.z_dim, scope = "sigma_layer")
@@ -59,6 +64,10 @@ class vanilla_vae:
 
                 for i in range(depth_gen):
                     y = dense(z, self.decoding_dims[i], scope="dec_layer"+"%s" %i, activation=tf.nn.sigmoid)
+                    with tf.variable_scope("dec_layer%s"%i) as scope:
+                        scope.reuse_variables()
+                        w = tf.get_variable("weights")
+                    reg_loss += tf.nn.l2_loss(w)
                     # if last_layer_nonelinear: depth_gen -1
 
             else:
