2f4aa1a07aae14e69cafd6a586c75ab999c259bf
zakosai <linh.nguyen.1992@gmail.com>
2018-05-13 12:25:59 +0900

    ave

--- cf-vae/train_cave.py
+++ cf-vae/train_cave.py
@@ -105,7 +105,7 @@ if type == 1:
             for r in [0.1, 1, 10]:
                 params.lambda_r = r
                 model = cf_vae_extend(num_users=5551, num_items=16980, num_factors=num_factors, params=params,
-                                      input_dim=8000, encoding_dims=[500, 200], z_dim = 50, decoding_dims=[200, 500, 8000],
+                                      input_dim=8000, encoding_dims=[200], z_dim = 50, decoding_dims=[200, 8000],
                                       decoding_dims_str=[100,200, 1863], loss_type='cross_entropy', model = model_type, ckpt_folder=ckpt, initial=initial)
                 model.fit(data["train_users"], data["train_items"], data["content"],img, data["structure"], params, data["test_users"])
                 model.save_model(os.path.join(ckpt,"cf_vae_%d_%d.mat"%(model_type, i)))
