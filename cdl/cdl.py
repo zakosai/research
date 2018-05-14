f267098bb57fccd060ea907725515ce71c2b02cd
zakosai <linh.nguyen.1992@gmail.com>
2018-05-14 20:38:29 +0900

    ave

--- cdl/cdl.py
+++ cdl/cdl.py
@@ -9,15 +9,15 @@ import os
 
 def load_cvae_data():
   data = {}
-  data_dir = "data/amazon2/"
-  # variables = scipy.io.loadmat(data_dir + "mult_nor-small.mat")
-  # data["content"] = variables['X']
+  data_dir = "data/citeulike-a/"
+  variables = scipy.io.loadmat(data_dir + "mult_nor.mat")
+  data["content"] = variables['X']
   variables = load_npz("data/amazon2/mult_nor-small.npz")
   data["content"] = variables.toarray()
-  data["train_users"] = load_rating(data_dir + "cf-train-1-users-small.dat")
-  data["train_items"] = load_rating(data_dir + "cf-train-1-items-small.dat")
-  data["test_users"] = load_rating(data_dir + "cf-test-1-users-small.dat")
-  data["test_items"] = load_rating(data_dir + "cf-test-1-items-small.dat")
+  data["train_users"] = load_rating(data_dir + "cf-train-1-users.dat")
+  data["train_items"] = load_rating(data_dir + "cf-train-1-items.dat")
+  data["test_users"] = load_rating(data_dir + "cf-test-1-users.dat")
+  data["test_items"] = load_rating(data_dir + "cf-test-1-items.dat")
 
   return data
 
@@ -90,7 +90,7 @@ if __name__ == '__main__':
     lambda_v = 10 # lambda_v in CDL
     K = 50  # no of latent vectors in the compact representation
     p = 10 # used for data-folder name
-    data_dir ="data/amazon2/" # whether to use dummy data
+    data_dir ="data/citeulike-a/" # whether to use dummy data
     num_iter = 200
     batch_size = 512
 
@@ -109,7 +109,7 @@ if __name__ == '__main__':
     # variables = load_npz("data/amazon2/mult_nor-small.npz")
     # X = variables.toarray()
     #
-    R = load_rating2(data_dir + "cf-train-1-users-small.dat")
+    R = load_rating2(data_dir + "cf-train-1-users.dat")
 
     X = data["content"]
     # R = data["train_users"]
@@ -119,40 +119,38 @@ if __name__ == '__main__':
     #    internal_act='relu', output_act='relu')
 
     #mx.cpu() no param needed for cpu.
-    i = 0
-    for lambda_u in [0.01, 0.1, 1]:
-        for lambda_v in [0.1, 1, 10]:
-            ae_model = AutoEncoderModel(mx.gpu(1), [X.shape[1],200,K],
-                pt_dropout=0.2, internal_act='relu', output_act='relu')
 
-            train_X = X
+    ae_model = AutoEncoderModel(mx.gpu(1), [X.shape[1],200,K],
+        pt_dropout=0.2, internal_act='relu', output_act='relu')
 
-            #ae_model.layerwise_pretrain(train_X, 256, 50000, 'sgd', l_rate=0.1, decay=0.0,
-            #                         lr_scheduler=mx.misc.FactorScheduler(20000,0.1))
-            #V = np.zeros((train_X.shape[0],10))
-            V = np.random.rand(train_X.shape[0],K)/10
-            lambda_v_rt = np.ones((train_X.shape[0],K))*sqrt(lv)
-            U, V, theta, BCD_loss = ae_model.finetune(train_X, R, V, lambda_v_rt, lambda_u,
-                    lambda_v, dir_save, batch_size,
-                    num_iter, 'sgd', l_rate=0.1, decay=0.0,
-                    lr_scheduler=mx.misc.FactorScheduler(20000,0.1))
-            #ae_model.save('cdl_pt.arg')
-            np.savetxt(dir_save+'/final-U_%d.dat'%i,U,fmt='%.5f',comments='')
-            np.savetxt(dir_save+'/final-V_%d.dat'%i,V,fmt='%.5f',comments='')
-            np.savetxt(dir_save+'/final-theta_%d.dat'%i,theta,fmt='%.5f',comments='')
+    train_X = X
 
-            f = open("result_dae.txt", "a")
-            f.write("-------%d-------%d--------\n"%(lambda_u, lambda_v))
-            pred_all = predict_all(U, V)
-            predict_val(pred_all, data["train_users"], data["test_users"], f)
-            f.write("\n")
-            f.close()
+    #ae_model.layerwise_pretrain(train_X, 256, 50000, 'sgd', l_rate=0.1, decay=0.0,
+    #                         lr_scheduler=mx.misc.FactorScheduler(20000,0.1))
+    #V = np.zeros((train_X.shape[0],10))
+    V = np.random.rand(train_X.shape[0],K)/10
+    lambda_v_rt = np.ones((train_X.shape[0],K))*sqrt(lv)
+    U, V, theta, BCD_loss = ae_model.finetune(train_X, R, V, lambda_v_rt, lambda_u,
+            lambda_v, dir_save, batch_size,
+            num_iter, 'sgd', l_rate=0.1, decay=0.0,
+            lr_scheduler=mx.misc.FactorScheduler(20000,0.1))
+    #ae_model.save('cdl_pt.arg')
+    np.savetxt(dir_save+'/final-U.dat',U,fmt='%.5f',comments='')
+    np.savetxt(dir_save+'/final-V.dat',V,fmt='%.5f',comments='')
+    np.savetxt(dir_save+'/final-theta.dat',theta,fmt='%.5f',comments='')
 
-            #ae_model.load('cdl_pt.arg')
-            Recon_loss = lambda_v/lv*ae_model.eval(train_X,V,lambda_v_rt)
-            print "Training error: %.3f" % (BCD_loss+Recon_loss)
-            fp = open(dir_save+'/cdl.log','a')
-            fp.write("Training error: %.3f\n" % (BCD_loss+Recon_loss))
-            fp.close()
-            #print "Validation error:", ae_model.eval(val_X)
-            i+=1
+    f = open("result_dae.txt", "a")
+    f.write("-------%d-------%d--------\n"%(lambda_u, lambda_v))
+    pred_all = predict_all(U, V)
+    predict_val(pred_all, data["train_users"], data["test_users"], f)
+    f.write("\n")
+    f.close()
+
+    #ae_model.load('cdl_pt.arg')
+    Recon_loss = lambda_v/lv*ae_model.eval(train_X,V,lambda_v_rt)
+    print "Training error: %.3f" % (BCD_loss+Recon_loss)
+    fp = open(dir_save+'/cdl.log','a')
+    fp.write("Training error: %.3f\n" % (BCD_loss+Recon_loss))
+    fp.close()
+    #print "Validation error:", ae_model.eval(val_X)
+
