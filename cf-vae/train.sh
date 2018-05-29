c62de5008fdddc852bb6f14b9caa83e2b6ece90c
zakosai <linh.nguyen.1992@gmail.com>
2018-05-29 20:03:49 +0900

    grocery again

--- cf-vae/train.sh
+++ cf-vae/train.sh
@@ -1,27 +1,14 @@
-#mkdir citeu/z4_50
-#cp citeu/z2_50/vae_* citeu/z4_50/.
-#python train_cave.py --ckpt_folder=citeu/z4_50 --data_dir=data/citeulike-a/ --iter=15 --zdim=50 --model=0
-#
-#python train_cf_dae.py --ckpt_folder=grocery/dae --data_dir=data/amazon2/ --iter=150  --model=0
+python train_vae.py --ckpt_folder=grocery2/vae --data_dir=data/amazon2/ --zdim=50
+python train_cvae_extend.py --model=0 --ckpt_folder=grocery2/vae --data_dir=data/amazon2/ --iter=100 --zdim=50
+cp -a grocery2/vae/vae* grocery2/vae_im
+python train_img_vae.py --ckpt_folder=grocery2/vae_im --data_dir=data/amazon2/ --zdim=50
+python train_cvae_extend.py --model=1 --ckpt_folder=grocery2/vae_im --data_dir=data/amazon2/ --iter=100 --zdim=50
 
 
-
-
-#for i in 50 100 150
-#do
-#    mkdir pre2/z3_$i
-#    python train_vae.py --ckpt_folder=pre2/z3_$i --data_dir=data/movie/ --zdim=$i
-#    python train_cvae_extend.py --model=0 --ckpt_folder=pre2/z3_$i --data_dir=data/movie/ --iter=100 --zdim=$i
+python train_dae.py --ckpt_folder=grocery2/dae --data_dir=data/amazon2/ --zdim=50
+python train_cf_dae.py --model=0 --ckpt_folder=grocery2/dae --data_dir=data/amazon2/ --iter=100 --zdim=50
+cp -a grocery2/dae/dae* grocery2/dae_im
+python train_im_dae.py --ckpt_folder=grocery2/dae_im --data_dir=data/amazon2/ --zdim=50
+python train_cf_dae.py --model=1 --ckpt_folder=grocery2/dae_im --data_dir=data/amazon2/ --iter=100 --zdim=50
 #    python train_img_vae.py --ckpt_folder=pre2/z3_$i --data_dir=data/movie/ --zdim=$i
 #    python train_cvae_extend.py --model=1 --ckpt_folder=pre2/z3_$i --data_dir=data/movie/ --iter=100 --zdim=$i
-#done
-
-#python train_ave.py --ckpt_folder=citeu/z3_50 --data_dir=data/citeulike-a/
-python train_cave.py --model=0 --ckpt_folder=citeu/z3_50 --data_dir=data/citeulike-a/ --iter=200 --zdim=50 --type=0
-
-python train_cave.py --model=0 --ckpt_folder=citeu/z6_50 --data_dir=data/citeulike-a/ --iter=15 --zdim=50 --type=1
-
-#python train_cf_dae.py --ckpt_folder=grocery/dae_fix --data_dir=data/amazon2/ --iter=15  --model=0
-#python train_cf_dae.py --ckpt_folder=grocery/dae_fix --data_dir=data/amazon2/ --iter=15  --model=1
-#python train_cvae_extend.py --model=0 --ckpt_folder=grocery/z50_fix --data_dir=data/amazon2/ --iter=15 --zdim=50
-
