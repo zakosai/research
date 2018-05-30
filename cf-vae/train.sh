f07aa39157536e7678324def13b06e857142016a
zakosai <linh.nguyen.1992@gmail.com>
2018-05-30 21:22:28 +0900

    sport

--- cf-vae/train.sh
+++ cf-vae/train.sh
@@ -1,12 +1,13 @@
-python train_vae.py --ckpt_folder=sport/vae --data_dir=data/sport/ --zdim=50
-python train_img_vae.py --ckpt_folder=sport/vae --data_dir=data/sport/ --zdim=50
+#python train_vae.py --ckpt_folder=sport/vae --data_dir=data/sport/ --zdim=50
+#python train_img_vae.py --ckpt_folder=sport/vae --data_dir=data/sport/ --zdim=50
 python train_cvae_extend.py --model=3 --ckpt_folder=sport/vae --data_dir=data/sport/ --iter=50 --zdim=50
 python train_cvae_extend.py --model=0 --ckpt_folder=sport/vae --data_dir=data/sport/ --iter=50 --zdim=50
 python train_cvae_extend.py --model=1 --ckpt_folder=sport/vae --data_dir=data/sport/ --iter=50 --zdim=50
 
 
 
-python train_dae.py --ckpt_folder=sport/dae --data_dir=data/sport/ --zdim=50
+#python train_dae.py --ckpt_folder=sport/dae --data_dir=data/sport/
 python train_cf_dae.py --model=0 --ckpt_folder=sport/dae --data_dir=data/sport/ --iter=50
-python train_im_dae.py --ckpt_folder=sport/dae --data_dir=data/sport/ --zdim=50
+#python train_im_dae.py --ckpt_folder=sport/dae --data_dir=data/sport/
 python train_cf_dae.py --model=1 --ckpt_folder=sport/dae--data_dir=data/sport/ --iter=50
+python train_cf_dae.py --model=3 --ckpt_folder=sport/dae --data_dir=data/sport/ --iter=50
