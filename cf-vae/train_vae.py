22f2a2f9753a67871ef033869a7d03652cb5f508
zakosai <linh.nguyen.1992@gmail.com>
2018-05-30 20:32:00 +0900

    sport

--- cf-vae/train_vae.py
+++ cf-vae/train_vae.py
@@ -24,8 +24,9 @@ tf.set_random_seed(0)
 
 # variables = sio.loadmat("data/citeulike-a/mult_nor.mat")
 # data = variables['X']
-variables = load_npz(os.path.join(dir, "mult-nor.npz"))
-data = variables.toarray()
+# variables = load_npz(os.path.join(dir, "mult-nor.npz"))
+# data = variables.toarray()
+data = np.load(os.path.join(dir, "structure.npy"))
 idx = np.random.rand(data.shape[0]) < 0.8
 train_X = data[idx]
 test_X = data[~idx]
@@ -36,8 +37,8 @@ test_X = data[~idx]
 # train_img = images[idx]
 # test_img = images[~idx]
 
-model = vanilla_vae(input_dim=8000, encoding_dims=[200, 100], z_dim=zdim, decoding_dims=[100, 200, 8000], loss='cross_entropy', ckpt_folder=ckpt)
+model = vanilla_vae(input_dim=4562, encoding_dims=[200, 100], z_dim=zdim, decoding_dims=[100, 200, 4562], loss='cross_entropy', ckpt_folder=ckpt)
 # As there will be an additional layer from 100 to 50 in the encoder. in decoder, we also take this layer
                     # lr=0.01, batch_size=128, print_step=50)
 print('fitting data starts...')
-model.fit(train_X, epochs=10000,learning_rate=0.001, batch_size=500, print_size=50, train=True, scope="text")
+model.fit(train_X, epochs=10000,learning_rate=0.001, batch_size=500, print_size=50, train=True, scope="structure")
