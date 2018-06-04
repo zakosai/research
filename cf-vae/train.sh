1e02d69853fbba94da313773f77ecdca6f0b5c34
zakosai <linh.nguyen.1992@gmail.com>
2018-06-04 19:12:52 +0900

    sport

--- cf-vae/train.sh
+++ cf-vae/train.sh
@@ -7,7 +7,7 @@ python train_cvae_extend.py --model=2 --ckpt_folder=sport/vae --data_dir=data/sp
 
 
 #python train_dae.py --ckpt_folder=sport/dae --data_dir=data/sport/
-python train_cf_dae.py --model=0 --ckpt_folder=sport/dae --data_dir=data/sport/ --iter=50
+#python train_cf_dae.py --model=0 --ckpt_folder=sport/dae --data_dir=data/sport/ --iter=50
 #python train_im_dae.py --ckpt_folder=sport/dae --data_dir=data/sport/
-python train_cf_dae.py --model=1 --ckpt_folder=sport/dae--data_dir=data/sport/ --iter=50
+python train_cf_dae.py --model=1 --ckpt_folder=sport/dae --data_dir=data/sport/ --iter=50
 python train_cf_dae.py --model=2 --ckpt_folder=sport/dae --data_dir=data/sport/ --iter=50
