34625d37cb9f63118dba0e6ff45fcbb18367d61b
zakosai <linh.nguyen.1992@gmail.com>
2018-06-08 16:44:48 +0900

    user infor

--- cf-vae/train_vae.py
+++ cf-vae/train_vae.py
@@ -41,4 +41,4 @@ model = vanilla_vae(input_dim=742, encoding_dims=[100], z_dim=zdim, decoding_dim
 # As there will be an additional layer from 100 to 50 in the encoder. in decoder, we also take this layer
                     # lr=0.01, batch_size=128, print_step=50)
 print('fitting data starts...')
-model.fit(train_X, epochs=10000,learning_rate=0.00001, batch_size=500, print_size=50, train=True, scope="user")
+model.fit(train_X, epochs=10000,learning_rate=0.001, batch_size=500, print_size=50, train=True, scope="user")
