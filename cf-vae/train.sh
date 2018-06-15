00e700dd4e0b4245a3b023dd3f411a1617569c7b
zakosai <linh.nguyen.1992@gmail.com>
2018-06-15 10:41:51 +0900

    movielens

--- cf-vae/train.sh
+++ cf-vae/train.sh
@@ -7,14 +7,14 @@ python train_cvae_extend.py --model=0 --ckpt_folder=ml/70p --data_dir=data/ml-1m
 python train_cf_dae.py --model=0 --ckpt_folder=ml/70p --data_dir=data/ml-1m/ --iter=50 --data_type=70p
 
 
-python train_vae.py --ckpt_folder=ml/70p --data_dir=data/ml-1m/ --zdim=50 --data_type=15
+python train_vae.py --ckpt_folder=ml/15 --data_dir=data/ml-1m/ --zdim=50 --data_type=15
 
-python train_cvae_user.py --model=0 --ckpt_folder=ml/70p --data_dir=data/ml-1m/ --iter=50 --zdim=50 --gridsearch=1 --data_type=15
-python train_cvae_extend.py --model=0 --ckpt_folder=ml/70p --data_dir=data/ml-1m/ --iter=50 --zdim=50 --gridsearch=1 --data_type=15
-python train_cf_dae.py --model=0 --ckpt_folder=ml/70p --data_dir=data/ml-1m/ --iter=50 --data_type=15
+python train_cvae_user.py --model=0 --ckpt_folder=ml/15 --data_dir=data/ml-1m/ --iter=50 --zdim=50 --gridsearch=1 --data_type=15
+python train_cvae_extend.py --model=0 --ckpt_folder=ml/15 --data_dir=data/ml-1m/ --iter=50 --zdim=50 --gridsearch=1 --data_type=15
+python train_cf_dae.py --model=0 --ckpt_folder=ml/15 --data_dir=data/ml-1m/ --iter=50 --data_type=15
 
-python train_vae.py --ckpt_folder=ml/70p --data_dir=data/ml-1m/ --zdim=50 --data_type=5
+python train_vae.py --ckpt_folder=ml/5 --data_dir=data/ml-1m/ --zdim=50 --data_type=5
 
-python train_cvae_user.py --model=0 --ckpt_folder=ml/70p --data_dir=data/ml-1m/ --iter=50 --zdim=50 --gridsearch=1 --data_type=5
-python train_cvae_extend.py --model=0 --ckpt_folder=ml/70p --data_dir=data/ml-1m/ --iter=50 --zdim=50 --gridsearch=1 --data_type=5
-python train_cf_dae.py --model=0 --ckpt_folder=ml/70p --data_dir=data/ml-1m/ --iter=50 --data_type=5
+python train_cvae_user.py --model=0 --ckpt_folder=ml/5 --data_dir=data/ml-1m/ --iter=50 --zdim=50 --gridsearch=1 --data_type=5
+python train_cvae_extend.py --model=0 --ckpt_folder=ml/5 --data_dir=data/ml-1m/ --iter=50 --zdim=50 --gridsearch=1 --data_type=5
+python train_cf_dae.py --model=0 --ckpt_folder=ml/5 --data_dir=data/ml-1m/ --iter=50 --data_type=5
