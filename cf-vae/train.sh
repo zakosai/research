835d25a373cbe1835b7ce7e76b028b42491f5c81
zakosai <linh.nguyen.1992@gmail.com>
2018-06-15 10:54:57 +0900

    movielens

--- cf-vae/train.sh
+++ cf-vae/train.sh
@@ -1,5 +1,5 @@
-python train_vae.py --ckpt_folder=ml/70p --data_dir=data/ml-1m/ --zdim=50 --data_type=70p
-python train_dae.py --ckpt_folder=ml/70p --data_dir=data/ml-1m/
+#python train_vae.py --ckpt_folder=ml/70p --data_dir=data/ml-1m/ --zdim=50 --data_type=70p
+#python train_dae.py --ckpt_folder=ml/70p --data_dir=data/ml-1m/
 
 
 python train_cvae_user.py --model=0 --ckpt_folder=ml/70p --data_dir=data/ml-1m/ --iter=50 --zdim=50 --gridsearch=1 --data_type=70p
