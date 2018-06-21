100eb1c2b600a97da33eef95fa3d80ff24742665
zakosai <linh.nguyen.1992@gmail.com>
2018-06-21 11:24:42 +0900

    PMF

---
+++ cf-vae/PMF.py
@@ -0,0 +1,178 @@
+import numpy as np
+from numpy import linalg as LA
+import scipy.io as sio
+
+class PMF:
+    def __init__(self, num_feat=10, epsilon=1, _lambda=0.1, momentum=0.8, maxepoch=20, num_batches=10, batch_size=1000):
+        self.num_feat = num_feat
+        self.epsilon = epsilon
+        self._lambda = _lambda
+        self.momentum = momentum
+        self.maxepoch = maxepoch
+        self.num_batches = num_batches
+        self.batch_size = batch_size
+        
+        self.w_C = None
+        self.w_I = None
+
+        self.err_train = []
+        self.err_val = []
+        
+    def fit(self, train_vec, val_vec, train_users, test_users):
+        # mean subtraction
+        self.mean_inv = np.mean(train_vec[:,2])
+        
+        pairs_tr = train_vec.shape[0]
+        pairs_va = val_vec.shape[0]
+        
+        # 1-p-i, 2-m-c
+        num_inv = int(max(np.amax(train_vec[:,0]), np.amax(val_vec[:,0]))) + 1
+        num_com = int(max(np.amax(train_vec[:,1]), np.amax(val_vec[:,1]))) + 1
+
+        incremental = False
+        if ((not incremental) or (self.w_C is None)):
+            # initialize
+            self.epoch = 0
+            self.w_C = 0.1 * np.random.randn(num_com, self.num_feat)
+            self.w_I = 0.1 * np.random.randn(num_inv, self.num_feat)
+            
+            self.w_C_inc = np.zeros((num_com, self.num_feat))
+            self.w_I_inc = np.zeros((num_inv, self.num_feat))
+        
+        
+        while self.epoch < self.maxepoch:
+            self.epoch += 1
+
+            # Shuffle training truples
+            shuffled_order = np.arange(train_vec.shape[0])
+            np.random.shuffle(shuffled_order)
+
+            # Batch update
+            for batch in range(self.num_batches):
+                # print "epoch %d batch %d" % (self.epoch, batch+1)
+
+                batch_idx = np.mod(np.arange(self.batch_size * batch,
+                                             self.batch_size * (batch+1)),
+                                   shuffled_order.shape[0])
+
+                batch_invID = np.array(train_vec[shuffled_order[batch_idx], 0], dtype='int32')
+                batch_comID = np.array(train_vec[shuffled_order[batch_idx], 1], dtype='int32')
+
+                # Compute Objective Function
+                pred_out = np.sum(np.multiply(self.w_I[batch_invID,:], 
+                                                self.w_C[batch_comID,:]),
+                                axis=1) # mean_inv subtracted
+
+                rawErr = pred_out - train_vec[shuffled_order[batch_idx], 2] + self.mean_inv
+
+                # Compute gradients
+                Ix_C = 2 * np.multiply(rawErr[:, np.newaxis], self.w_I[batch_invID,:]) \
+                        + self._lambda * self.w_C[batch_comID,:]
+                Ix_I = 2 * np.multiply(rawErr[:, np.newaxis], self.w_C[batch_comID,:]) \
+                        + self._lambda * self.w_I[batch_invID,:]
+            
+                dw_C = np.zeros((num_com, self.num_feat))
+                dw_I = np.zeros((num_inv, self.num_feat))
+
+                # loop to aggreate the gradients of the same element
+                for i in range(self.batch_size):
+                    dw_C[batch_comID[i],:] += Ix_C[i,:]
+                    dw_I[batch_invID[i],:] += Ix_I[i,:]
+
+
+                # Update with momentum
+                self.w_C_inc = self.momentum * self.w_C_inc + self.epsilon * dw_C / self.batch_size
+                self.w_I_inc = self.momentum * self.w_I_inc + self.epsilon * dw_I / self.batch_size
+
+
+                self.w_C = self.w_C - self.w_C_inc
+                self.w_I = self.w_I - self.w_I_inc
+
+                # Compute Objective Function after
+                if batch == self.num_batches - 1:
+                    pred_out = np.sum(np.multiply(self.w_I[np.array(train_vec[:,0], dtype='int32'),:],
+                                                    self.w_C[np.array(train_vec[:,1], dtype='int32'),:]),
+                                        axis=1) # mean_inv subtracted
+                    rawErr = pred_out - train_vec[:, 2] + self.mean_inv
+                    obj = LA.norm(rawErr) ** 2 \
+                            + 0.5*self._lambda*(LA.norm(self.w_I) ** 2 + LA.norm(self.w_C) ** 2)
+
+                    self.err_train.append(np.sqrt(obj/pairs_tr))
+
+                # Compute validation error
+                if batch == self.num_batches - 1:
+                    pred_out = np.sum(np.multiply(self.w_I[np.array(val_vec[:,0], dtype='int32'),:],
+                                                    self.w_C[np.array(val_vec[:,1], dtype='int32'),:]),
+                                        axis=1) # mean_inv subtracted
+                    rawErr = pred_out - val_vec[:, 2] + self.mean_inv
+                    self.err_val.append(LA.norm(rawErr)/np.sqrt(pairs_va))
+
+                # Print info
+            self.predict_val(train_users, test_users)
+
+                #if batch == self.num_batches - 1:
+                #    print 'Training RMSE: %f, Test RMSE %f' % (self.err_train[-1], self.err_val[-1])
+    def save_model(self, save_path_pmf):
+        # self.saver.save(self.sess, save_path_weights)
+        sio.savemat(save_path_pmf, {"U":self.w_I, "V":self.w_C, "mean":self.mean_inv})
+        print "all parameters saved"
+    def load_model(self, load_path_pmf):
+        # self.saver.restore(self.sess, load_path_weights)
+        data = sio.loadmat(load_path_pmf)
+        self.w_I = data["U"]
+        self.w_C = data["V"]
+        self.mean_inv = data["mean"]
+        print "model loaded"
+
+    def predict(self, invID): 
+        return np.dot(self.w_C, self.w_I[invID,:]) + self.mean_inv
+
+    def predict_val(self, train_users, test_users, file=None):
+        user_all = test_users
+        ground_tr_num = [len(user) for user in user_all]
+
+
+
+        recall_avgs = []
+        precision_avgs = []
+        mapk_avgs = []
+        for m in [10, 100]:
+            print "m = " + "{:>10d}".format(m) + "done"
+            recall_vals = []
+            for i in range(len(user_all)):
+                train = train_users[i]
+                pred = self.predict(i)
+                top_M = list(np.argsort(-pred)[0:(m +len(train))])
+                for u in train:
+                    if u in top_M:
+                        top_M.remove(u)
+                top_M = top_M[:m]
+                if len(top_M) != m:
+                    print(top_M, train_users[i])
+                hits = set(top_M) & set(user_all[i])   # item idex from 0
+                hits_num = len(hits)
+                try:
+                    recall_val = float(hits_num) / float(ground_tr_num[i])
+                except:
+                    recall_val = 1
+                recall_vals.append(recall_val)
+                # precision = float(hits_num) / float(m)
+                # precision_vals.append(precision)
+
+            recall_avg = np.mean(np.array(recall_vals))
+            # precision_avg = np.mean(np.array(precision_vals))
+            # # mapk = ml_metrics.mapk([list(np.argsort(-pred_all[k])) for k in range(len(pred_all)) if len(user_all[k])!= 0],
+            # #                        [u for u in user_all if len(u)!=0], m)
+            print recall_avg
+            if file != None:
+                file.write("m = %d, recall = %f\t"%(m, recall_avg))
+        
+    def set_params(self, parameters):
+        if isinstance(parameters, dict):
+            self.num_feat = parameters.get("num_feat", 10)
+            self.epsilon = parameters.get("epsilon", 1)
+            self._lambda = parameters.get("_lambda", 0.1)
+            self.momentum = parameters.get("momentum", 0.8)
+            self.maxepoch = parameters.get("maxepoch", 20)
+            self.num_batches = parameters.get("num_batches", 10)
+            self.batch_size = parameters.get("batch_size", 1000)
---
+++ cf-vae/train_pmf.py
@@ -0,0 +1,121 @@
+import numpy as np
+import tensorflow as tf
+import scipy.io
+import matplotlib.pyplot as plt
+from cvae_user import cf_vae_extend, params
+from scipy.sparse import load_npz
+import argparse
+import os
+import csv
+from PMF import PMF
+
+np.random.seed(0)
+tf.set_random_seed(0)
+
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('--model',  type=int, default=0,
+                   help='type of model: 0-only text, 1-text+image, 2-text+image+structure, 3-text+structure')
+parser.add_argument('--ckpt_folder',  type=str, default='pre_model/exp1/',
+                   help='where model is stored')
+parser.add_argument('--initial',  type=bool, default=True,
+                   help='where model is stored')
+parser.add_argument('--iter',  type=int, default=30,
+                   help='where model is stored')
+parser.add_argument('--data_dir',  type=str, default='data/amazon',
+                   help='where model is stored')
+parser.add_argument('--zdim',  type=int, default=50,
+                   help='where model is stored')
+parser.add_argument('--gridsearch',  type=int, default=0,
+                   help='gridsearch or not')
+parser.add_argument('--data_type',  type=str, default='5',
+                   help='gridsearch or not')
+parser.add_argument('--user_dim',  type=int, default=9975,
+                   help='gridsearch or not')
+parser.add_argument('--user_no',  type=int, default=6040,
+                   help='gridsearch or not')
+parser.add_argument('--item_no',  type=int, default=3883,
+                   help='gridsearch or not')
+args = parser.parse_args()
+model_type = args.model
+ckpt = args.ckpt_folder
+initial = args.initial
+iter = args.iter
+data_dir = args.data_dir
+zdim = args.zdim
+gs = args.gridsearch
+data_type = args.data_type
+print(model_type)
+
+def load_cvae_data(data_dir):
+  data = {}
+  # variables = scipy.io.loadmat(data_dir + "mult_nor.mat")
+  # data["content"] = variables['X']
+  variables = load_npz(os.path.join(data_dir,"mult_nor.npz"))
+  data["content"] = variables.toarray()
+  variables = np.load(os.path.join(data_dir, "structure.npy"))
+  data["structure"] = variables
+  user = np.load(os.path.join(data_dir, "user_info_%s4.npy"%data_type))
+  # user = np.delete(user, [7,8,9,10,11], axis=1)
+  data["user"] = user
+  data["train_users"] = load_rating(data_dir + "cf-train-%s-users.dat"%data_type)
+  data["train_items"] = load_rating(data_dir + "cf-train-%s-items.dat"%data_type)
+  data["test_users"] = load_rating(data_dir + "cf-test-%s-users.dat"%data_type)
+  data["test_items"] = load_rating(data_dir + "cf-test-%s-items.dat"%data_type)
+
+  data["train_vec"] = load_rating2(os.path.join(data_dir, "train-%s.csv"%data_type))
+  data["val_vec"] =load_rating2(os.path.join(data_dir, "val-%s.csv"%data_type))
+  return data
+
+def load_rating(path):
+  arr = []
+  for line in open(path):
+    a = line.strip().split()
+    if a[0]==0:
+      l = []
+    else:
+      l = [int(x) for x in a[1:]]
+    arr.append(l)
+  return arr
+
+def load_rating2(path):
+    arr = list(open(path).readlines())
+    arr = arr[1:]
+    arr = [a.strip() for a in arr]
+    arr = [a.split(",") for a in arr]
+    arr = np.array(arr).astype(np.int32)
+    return arr
+
+
+
+params = params()
+params.lambda_u = 1
+params.lambda_v = 10
+params.lambda_r = 0.1
+params.C_a = 1
+params.C_b = 0.01
+params.max_iter_m = 1
+params.EM_iter = args.iter
+params.num_iter = 150
+
+C = [0.1, 1, 10]
+
+# # for updating W and b in vae
+# self.learning_rate = 0.001
+# self.batch_size = 500
+# self.num_iter = 3000
+# self.EM_iter = 100
+
+data = load_cvae_data(data_dir)
+np.random.seed(0)
+tf.set_random_seed(0)
+
+images = np.fromfile(os.path.join(data_dir,"images.bin"), dtype=np.uint8)
+img = images.reshape((13791, 32, 32, 3))
+img = img.astype(np.float32)/255
+num_factors = zdim
+
+model = PMF(num_feat=50, maxepoch=50, num_batches=43)
+model.fit(data["train_vec"], data["val_vec"], train_users=data["train_users"], test_users=data["test_users"])
+
+
