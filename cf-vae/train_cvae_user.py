6dc4289e35790ebeeaff239813550fe001213c97
zakosai <linh.nguyen.1992@gmail.com>
2018-06-22 13:08:01 +0900

    remove character of ml

--- cf-vae/train_cvae_user.py
+++ cf-vae/train_cvae_user.py
@@ -131,10 +131,10 @@ if gs == 1:
                     print(u, v, r)
                 i += 1
 else:
-    model = cf_vae_extend(num_users=6040, num_items=3883, num_factors=num_factors, params=params,
+    model = cf_vae_extend(num_users=args.user_no, num_items=args.item_no, num_factors=num_factors, params=params,
                           input_dim=8000, encoding_dims=[200, 100], z_dim = zdim, decoding_dims=[100, 200, 8000],
                           encoding_dims_str=[500, 200], decoding_dims_str=[200, 500, 4526], loss_type='cross_entropy',
-                          model = model_type, ckpt_folder=ckpt, initial=initial)
+                          model = model_type, ckpt_folder=ckpt, initial=initial, user_dim=args.user_dim)
     model.fit(data["train_users"], data["train_items"], data["content"],img, data["structure"], params, data["test_users"], data["user"])
     model.save_model(os.path.join(ckpt,"cvae_user_%d.mat"%(model_type)))
     # model.load_model("cf_vae.mat")
