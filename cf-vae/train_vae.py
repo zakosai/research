57c8c13b855f2217e5aca2b278b5e1410b1b4e70
zakosai <linh.nguyen.1992@gmail.com>
2018-06-26 22:16:59 +0900

    new dataset

--- cf-vae/train_vae.py
+++ cf-vae/train_vae.py
@@ -29,9 +29,9 @@ tf.set_random_seed(0)
 
 # variables = sio.loadmat("data/citeulike-a/mult_nor.mat")
 # data = variables['X']
-# variables = load_npz(os.path.join(dir, "mult_nor.npz"))
-# data = variables.toarray()
-data = np.load(os.path.join(dir, "user_info_%s.npy"%data_type))
+variables = load_npz(os.path.join(dir, "mult_nor.npz"))
+data = variables.toarray()
+# data = np.load(os.path.join(dir, "user_info_%s.npy"%data_type))
 # data = np.delete(data, [7,8,9,10,11], axis=1)
 idx = np.random.rand(data.shape[0]) < 0.8
 train_X = data[idx]
@@ -43,9 +43,9 @@ test_X = data[~idx]
 # train_img = images[idx]
 # test_img = images[~idx]
 
-model = vanilla_vae(input_dim=args.user_dim, encoding_dims=[100], z_dim=zdim, decoding_dims=[100, args.user_dim], loss='cross_entropy', ckpt_folder=ckpt)
-# model = vanilla_vae(input_dim=8000, encoding_dims=[200, 100], z_dim=zdim, decoding_dims=[100, 200, 8000], loss='cross_entropy', ckpt_folder=ckpt)
+# model = vanilla_vae(input_dim=args.user_dim, encoding_dims=[100], z_dim=zdim, decoding_dims=[100, args.user_dim], loss='cross_entropy', ckpt_folder=ckpt)
+model = vanilla_vae(input_dim=8000, encoding_dims=[200, 100], z_dim=zdim, decoding_dims=[100, 200, 8000], loss='cross_entropy', ckpt_folder=ckpt)
 # As there will be an additional layer from 100 to 50 in the encoder. in decoder, we also take this layer
                     # lr=0.01, batch_size=128, print_step=50)
 print('fitting data starts...')
-model.fit(train_X, epochs=10000,learning_rate=0.001, batch_size=500, print_size=50, train=True, scope="user")
+model.fit(train_X, epochs=10000,learning_rate=0.001, batch_size=500, print_size=50, train=True, scope="text")
