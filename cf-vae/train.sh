72c5c8605730dc1f59a8de10e59624cf27d94dd1
zakosai <linh.nguyen.1992@gmail.com>
2018-06-27 19:32:04 +0900

    cdae_user

--- cf-vae/train.sh
+++ cf-vae/train.sh
@@ -1,34 +1,32 @@
-folders='Outdoor Toy Tool Beauty Electronics TV'
+folders='Toy Tool Beauty Electronics TV'
 rate='20 80'
-#for f in $folders
-#do
-#    mkdir -p $f/20
-#    mkdir $f/80
-#    python train_vae.py --ckpt_folder=$f/20 --data_dir=data/$f/ --zdim=50 --data_type=8 --user_dim=742
-#    cp $f/20/vae* $f/80/
-#
-#done
-
 for f in $folders
 do
-    for r in $rate
-    do
-        dim="$(sed -n '3p' data/$f/info.txt)"
-        user_no="$(sed -n '1p' data/$f/info.txt)"
-        item_no="$(sed -n '2p' data/$f/info.txt)"
-
-        python train_vae.py --ckpt_folder=$f/$r --data_dir=data/$f/ --zdim=50 --data_type=$r --user_dim=$dim
-        python train_cvae_user.py --model=0 --ckpt_folder=$f/$r --data_dir=data/$f/ --iter=50 --zdim=50 --gridsearch=1 --data_type=$r --user_dim=$dim --user_no=$user_no --item_no=$item_no
-
-        python train_cvae_extend.py --model=0 --ckpt_folder=$f/$r --data_dir=data/$f/ --iter=50 --zdim=50 --gridsearch=1 --data_type=$r --user_no=$user_no --item_no=$item_no
-
-        python train_dae.py --ckpt_folder=$f/$r --data_dir=data/$f
-        python train_cf_dae.py --model=0 --ckpt_folder=$f/$r --data_dir=data/$f/ --iter=50 --data_type=$r --user_no=$user_no --item_no=$item_no
-
-
-    done
+    python train_dae.py --ckpt_folder=$f/20 --data_dir=data/$f/ --zdim=50 --data_type=8 --user_dim=742
+    cp $f/20/dae* $f/80/
 
 done
+
+#for f in $folders
+#do
+#    for r in $rate
+#    do
+#        dim="$(sed -n '3p' data/$f/info.txt)"
+#        user_no="$(sed -n '1p' data/$f/info.txt)"
+#        item_no="$(sed -n '2p' data/$f/info.txt)"
+#
+#        python train_vae.py --ckpt_folder=$f/$r --data_dir=data/$f/ --zdim=50 --data_type=$r --user_dim=$dim
+#        python train_cvae_user.py --model=0 --ckpt_folder=$f/$r --data_dir=data/$f/ --iter=50 --zdim=50 --gridsearch=1 --data_type=$r --user_dim=$dim --user_no=$user_no --item_no=$item_no
+#
+#        python train_cvae_extend.py --model=0 --ckpt_folder=$f/$r --data_dir=data/$f/ --iter=50 --zdim=50 --gridsearch=1 --data_type=$r --user_no=$user_no --item_no=$item_no
+#
+#        python train_dae.py --ckpt_folder=$f/$r --data_dir=data/$f
+#        python train_cf_dae.py --model=0 --ckpt_folder=$f/$r --data_dir=data/$f/ --iter=50 --data_type=$r --user_no=$user_no --item_no=$item_no
+#
+#
+#    done
+#
+#done
 
 #python train_vae.py --ckpt_folder=sport/8n --data_dir=data/sport/ --zdim=50 --data_type=8 --user_dim=742
 #python train_cvae_user.py --model=0 --ckpt_folder=sport/8n --data_dir=data/sport/ --iter=50 --zdim=50 --gridsearch=1 --data_type=5 --user_dim=742 --user_no=5584 --item_no=13790
@@ -49,3 +47,6 @@ done
 #
 #python train_vae.py --ckpt_folder=ml/70p --data_dir=data/ml-1m/ --zdim=50 --data_type=70p
 #python train_cvae_user.py --model=0 --ckpt_folder=ml/70p --data_dir=data/ml-1m/ --iter=50 --zdim=50 --gridsearch=1 --data_type=70p
+#python train_cdae_user.py --model=0 --ckpt_folder=Outdoor/20 --data_dir=data/Outdoor/ --iter=50 --zdim=50  --data_type=20 --user_dim=979 --user_no=3205 --item_no=11560
+
+#python train_dae.py --ckpt_folder=Outdoor/20 --data_dir=data/Outdoor/ --zdim=50 --data_type=20 --user_dim=979
