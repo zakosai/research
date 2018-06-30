a0893153cc0d281e80ea034e83d546472c3e3ef7
zakosai <linh.nguyen.1992@gmail.com>
2018-06-30 15:48:43 +0900

    sport new

--- cf-vae/train.sh
+++ cf-vae/train.sh
@@ -1,15 +1,7 @@
 folders='Toy'
-rate='1 8 80'
+rate='20 80'
 #python train_dae.py --ckpt_folder=sport/1nn --data_dir=data/sport/ --zdim=50 --data_type=1 --user_dim=742
 #python train_vae.py --ckpt_folder=sport/1nn --data_dir=data/sport/ --zdim=50 --data_type=1 --user_dim=742
-python train_cf_dae.py --model=0 --ckpt_folder=sport/1nn --data_dir=data/sport/ --iter=50 --zdim=50 --gridsearch=1 --data_type=1 --user_no=5584 --item_no=13790
-
-python train_cvae_extend.py --model=0 --ckpt_folder=sport/1nn --data_dir=data/sport/ --iter=50 --zdim=50 --gridsearch=1 --data_type=1 --user_no=5584 --item_no=13790
-#
-python train_cvae_user.py --model=0 --ckpt_folder=sport/1nn --data_dir=data/sport/ --iter=50 --zdim=50 --gridsearch=1 --data_type=1 --user_dim=742 --user_no=5584 --item_no=13790
-python train_cdae_user.py --model=0 --ckpt_folder=sport/1nn --data_dir=data/sport/ --iter=50 --zdim=50 --gridsearch=1 --data_type=1 --user_dim=742 --user_no=5584 --item_no=13790
-
-#
 
 
 #for f in $folders
@@ -19,29 +11,33 @@ python train_cdae_user.py --model=0 --ckpt_folder=sport/1nn --data_dir=data/spor
 #
 #done
 
-#for f in $folders
-#do
-#    for r in $rate
-#    do
-#        dim="$(sed -n '3p' data/$f/info.txt)"
-#        user_no="$(sed -n '1p' data/$f/info.txt)"
-#        item_no="$(sed -n '2p' data/$f/info.txt)"
-#
-#        python train_cvae_extend.py --model=0 --ckpt_folder=$f/$r --data_dir=data/$f/ --iter=50 --zdim=50 --gridsearch=1 --data_type=$r --user_no=$user_no --item_no=$item_no
-#
-#        python train_vae.py --ckpt_folder=$f/$r --data_dir=data/$f/ --zdim=50 --data_type=$r --user_dim=$dim
-#        python train_cvae_user.py --model=0 --ckpt_folder=$f/$r --data_dir=data/$f/ --iter=50 --zdim=50 --gridsearch=1 --data_type=$r --user_dim=$dim --user_no=$user_no --item_no=$item_no
-#
-#        python train_cf_dae.py --model=0 --ckpt_folder=$f/$r --data_dir=data/$f/ --iter=50 --data_type=$r --user_no=$user_no --item_no=$item_no
-#
-#        python train_dae.py --ckpt_folder=$f/$r --data_dir=data/$f/ --zdim=50 --data_type=$r --user_dim=$dim
-#        python train_cdae_user.py --model=0 --ckpt_folder=$f/$r --data_dir=data/$f/ --iter=50 --zdim=50 --gridsearch=1 --data_type=$r --user_dim=$dim --user_no=$user_no --item_no=$item_no
-#
-#
-#
-#    done
-#
-#done
+for f in $folders
+do
+    for r in $rate
+    do
+        dim="$(sed -n '3p' data2/$f/info.txt)"
+        user_no="$(sed -n '1p' data2/$f/info.txt)"
+        item_no="$(sed -n '2p' data2/$f/info.txt)"
+        python train_vae.py --ckpt_folder=$f/$r --data_dir=data2/$f/ --zdim=50 --data_type=$r --user_dim=$dim --type=text
+        python train_vae.py --ckpt_folder=$f/$r --data_dir=data2/$f/ --zdim=50 --data_type=$r --user_dim=$dim --type=user
+
+        python train_cvae_extend.py --model=0 --ckpt_folder=$f/$r --data_dir=data2/$f/ --iter=50 --zdim=50 --gridsearch=1 --data_type=$r --user_no=$user_no --item_no=$item_no
+
+        #python train_vae.py --ckpt_folder=$f/$r2 --data_dir=data2/$f/ --zdim=50 --data_type=$r --user_dim=$dim
+
+        python train_dae.py --ckpt_folder=$f/$r --data_dir=data2/$f/ --zdim=50 --data_type=$r --user_dim=$dim --type=text
+
+        python train_cf_dae.py --model=0 --ckpt_folder=$f/$r --data_dir=data2/$f/ --iter=50 --data_type=$r --user_no=$user_no --item_no=$item_no
+        python train_cvae_user.py --model=0 --ckpt_folder=$f/$r --data_dir=data2/$f/ --iter=50 --zdim=50 --gridsearch=1 --data_type=$r --user_dim=$dim --user_no=$user_no --item_no=$item_no
+
+        python train_dae.py --ckpt_folder=$f/$r --data_dir=data2/$f/ --zdim=50 --data_type=$r --user_dim=$dim --type=user
+        python train_cdae_user.py --model=0 --ckpt_folder=$f/$r --data_dir=data2/$f/ --iter=50 --zdim=50 --gridsearch=1 --data_type=$r --user_dim=$dim --user_no=$user_no --item_no=$item_no
+
+
+
+    done
+
+done
 
 #python train_vae.py --ckpt_folder=sport/8n --data_dir=data/sport/ --zdim=50 --data_type=8 --user_dim=742
 #python train_cvae_user.py --model=0 --ckpt_folder=sport/8n --data_dir=data/sport/ --iter=50 --zdim=50 --gridsearch=1 --data_type=5 --user_dim=742 --user_no=5584 --item_no=13790
--- cf-vae/train_dae.py
+++ cf-vae/train_dae.py
@@ -18,6 +18,8 @@ parser.add_argument('--data_type',  type=str, default='5',
                    help='where model is stored')
 parser.add_argument('--user_dim',  type=int, default=9975,
                    help='where model is stored')
+parser.add_argument('--type',  type=str, default="text",
+                   help='where model is stored')
 args = parser.parse_args()
 ckpt = args.ckpt_folder
 dir = args.data_dir
@@ -29,9 +31,11 @@ tf.set_random_seed(0)
 
 # variables = sio.loadmat("data/citeulike-a/mult_nor.mat")
 # data = variables['X']
-# variables = load_npz(os.path.join(dir, "mult_nor.npz"))
-# data = variables.toarray()
-data = np.load(os.path.join(dir, "user_info_%s2.npy"%data_type))
+if args.type == "text":
+    variables = load_npz(os.path.join(dir, "mult_nor.npz"))
+    data = variables.toarray()
+else:
+    data = np.load(os.path.join(dir, "user_info_%s.npy"%data_type))
 # data = np.delete(data, [7,8,9,10,11], axis=1)
 idx = np.random.rand(data.shape[0]) < 0.8
 train_X = data[idx]
@@ -43,9 +47,16 @@ test_X = data[~idx]
 # train_img = images[idx]
 # test_img = images[~idx]
 
-model = vanilla_vae(input_dim=args.user_dim, encoding_dims=[100], z_dim=zdim, decoding_dims=[100, args.user_dim], loss='cross_entropy', ckpt_folder=ckpt)
-# model = vanilla_vae(input_dim=8000, encoding_dims=[200, 100], z_dim=zdim, decoding_dims=[100, 200, 8000], loss='cross_entropy', ckpt_folder=ckpt)
+# model = vanilla_vae(input_dim=args.user_dim, encoding_dims=[100], z_dim=zdim, decoding_dims=[100, args.user_dim], loss='cross_entropy', ckpt_folder=ckpt)
+if args.type == "text":
+    model = vanilla_vae(input_dim=8000, encoding_dims=[200, 100], z_dim=zdim, decoding_dims=[100, 200, 8000], loss='cross_entropy', ckpt_folder=ckpt)
 # As there will be an additional layer from 100 to 50 in the encoder. in decoder, we also take this layer
                     # lr=0.01, batch_size=128, print_step=50)
-print('fitting data starts...')
-model.fit(train_X, epochs=10000,learning_rate=0.001, batch_size=500, print_size=50, train=True, scope="user")
+    print('fitting data starts...')
+    model.fit(train_X, epochs=10000,learning_rate=0.001, batch_size=500, print_size=50, train=True, scope="text")
+
+else:
+    model = vanilla_vae(input_dim=args.user_dim, encoding_dims=[100], z_dim=zdim, decoding_dims=[100, args.user_dim], loss='cross_entropy', ckpt_folder=ckpt)
+    print('fitting data starts...')
+    model.fit(train_X, epochs=10000,learning_rate=0.001, batch_size=500, print_size=50, train=True, scope="user")
+
