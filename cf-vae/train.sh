ba7a970a87aa68fe67f3f47be5585ecd9421a79b
zakosai <linh.nguyen.1992@gmail.com>
2018-07-03 10:19:32 +0900

    1 8

--- cf-vae/train.sh
+++ cf-vae/train.sh
@@ -1,5 +1,5 @@
 folders='Outdoor'
-rate='20 80'
+rate='1 8'
 #python train_dae.py --ckpt_folder=sport/1nn --data_dir=data/sport/ --zdim=50 --data_type=1 --user_dim=742
 #python train_vae.py --ckpt_folder=sport/1nn --data_dir=data/sport/ --zdim=50 --data_type=1 --user_dim=742
 
