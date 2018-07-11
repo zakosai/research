5d8ff978cb81d7b9c7953f41f88afbb698b0ac45
zakosai <linh.nguyen.1992@gmail.com>
2018-07-11 14:41:24 +0900

    PMF

--- cf-vae/PMF.py
+++ cf-vae/PMF.py
@@ -113,7 +113,7 @@ class PMF:
             #     m = recall
             #     e_m = self.epoch
 
-        print("max recall: %f in epoch %d"%(m, e_m))
+        # print("max recall: %f in epoch %d"%(m, e_m))
                 # Print info
 
                 #if batch == self.num_batches - 1:
@@ -133,21 +133,24 @@ class PMF:
     def predict(self, invID): 
         return np.dot(self.w_C, self.w_I[invID,:]) + self.mean_inv
 
-    def predict_val(self, train_users, test_users, file=None):
+    def predict_val(self, pred_all, train_users, test_users, file=None):
         user_all = test_users
         ground_tr_num = [len(user) for user in user_all]
 
 
+        pred_all = list(pred_all)
+
         recall_avgs = []
         precision_avgs = []
         mapk_avgs = []
-        for m in [100, 10]:
+        for m in [10]:
             print "m = " + "{:>10d}".format(m) + "done"
             recall_vals = []
+            ndcg = []
+            hit = 0
             for i in range(len(user_all)):
                 train = train_users[i]
-                pred = self.predict(i)
-                top_M = list(np.argsort(-pred)[0:(m +len(train))])
+                top_M = list(np.argsort(-pred_all[i])[0:(m +len(train))])
                 for u in train:
                     if u in top_M:
                         top_M.remove(u)
@@ -156,22 +159,62 @@ class PMF:
                     print(top_M, train_users[i])
                 hits = set(top_M) & set(user_all[i])   # item idex from 0
                 hits_num = len(hits)
+                if hits_num > 0:
+                    hit += 1
                 try:
                     recall_val = float(hits_num) / float(ground_tr_num[i])
                 except:
                     recall_val = 1
                 recall_vals.append(recall_val)
+                pred = np.array(pred_all[i])
+                score = []
+                for k in range(m):
+                    if top_M[k] in hits:
+                        score.append(1)
+                    else:
+                        score.append(0)
+                actual = self.dcg_score(score, pred[top_M], m)
+                best = self.dcg_score(score, score, m)
+                if best ==0:
+                    ndcg.append(0)
+                else:
+                    ndcg.append(float(actual)/best)
                 # precision = float(hits_num) / float(m)
                 # precision_vals.append(precision)
 
             recall_avg = np.mean(np.array(recall_vals))
             # precision_avg = np.mean(np.array(precision_vals))
-            # # mapk = ml_metrics.mapk([list(np.argsort(-pred_all[k])) for k in range(len(pred_all)) if len(user_all[k])!= 0],
-            # #                        [u for u in user_all if len(u)!=0], m)
-            print recall_avg
+            # mapk = ml_metrics.mapk([list(np.argsort(-pred_all[k])) for k in range(len(pred_all)) if len(user_all[k])!= 0],
+            #                        [u for u in user_all if len(u)!=0], m)
+
+            print("recall %f, hit: %f, NDCG: %f"%(recall_avg, float(hit)/len(user_all), np.mean(ndcg)))
+            #print recall_avg
             if file != None:
                 file.write("m = %d, recall = %f\t"%(m, recall_avg))
-        return recall_avg
+            # precision_avgs.append(precision_avg)
+    def dcg_score(self, y_true, y_score, k=5):
+        """Discounted cumulative gain (DCG) at rank K.
+
+        Parameters
+        ----------
+        y_true : array, shape = [n_samples]
+            Ground truth (true relevance labels).
+        y_score : array, shape = [n_samples, n_classes]
+            Predicted scores.
+        k : int
+            Rank.
+
+        Returns
+        -------
+        score : float
+        """
+        order = np.argsort(y_score)[::-1]
+        y_true = np.take(y_true, order[:k])
+
+        gain = 2 ** y_true - 1
+
+        discounts = np.log2(np.arange(len(y_true)) + 2)
+        return np.sum(gain / discounts)
         
     def set_params(self, parameters):
         if isinstance(parameters, dict):
