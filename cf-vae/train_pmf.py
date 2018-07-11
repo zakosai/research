eb25495254927dfd6090dbf79fc54d85d6ca4a19
zakosai <linh.nguyen.1992@gmail.com>
2018-07-11 12:03:15 +0900

    PMF

--- cf-vae/train_pmf.py
+++ cf-vae/train_pmf.py
@@ -111,27 +111,27 @@ tf.set_random_seed(0)
 
 num_factors = zdim
 i = 0
-e = 1
+e = 3
 l = 0.01
 m = 0.1
-for e in [0.1, 0.3, 1, 3, 10 ]:
-    for l in [0.01, 0.03, 0.1, 1, 3, 10]:
-        for m in [0.01, 0.03, 0.1, 1, 3, 10]:
-            model = PMF(epsilon=e, _lambda=l, momentum=m, num_feat=50, maxepoch=50, num_batches=43)
-            model.fit(data["train_vec"], data["val_vec"], train_users=data["train_users"], test_users=data["test_users"])
-            model.save_model(os.path.join(ckpt,"pmf_%d.mat"%(i)))
-                    # model.load_model("cf_vae.mat")
-            f = open(os.path.join(ckpt, "result_pmf_%d.txt"%model_type), 'a')
-            f.write("-----------%f----------%f----------%f\n"%(e,l,m))
-            model.predict_val(data["train_users"], data["test_users"], f)
-            f.write("\n")
-            print(e, l, m)
-            f.close()
-            i += 1
+# for e in [0.1, 0.3, 1, 3, 10 ]:
+#     for l in [0.01, 0.03, 0.1, 1, 3, 10]:
+#         for m in [0.01, 0.03, 0.1, 1, 3, 10]:
+#             model = PMF(epsilon=e, _lambda=l, momentum=m, num_feat=50, maxepoch=50, num_batches=43)
+#             model.fit(data["train_vec"], data["val_vec"], train_users=data["train_users"], test_users=data["test_users"])
+#             model.save_model(os.path.join(ckpt,"pmf_%d.mat"%(i)))
+#                     # model.load_model("cf_vae.mat")
+#             f = open(os.path.join(ckpt, "result_pmf_%d.txt"%model_type), 'a')
+#             f.write("-----------%f----------%f----------%f\n"%(e,l,m))
+#             model.predict_val(data["train_users"], data["test_users"], f)
+#             f.write("\n")
+#             print(e, l, m)
+#             f.close()
+#             i += 1
 
 
-# model = PMF(epsilon=e, _lambda=l, momentum=m, num_feat=50, maxepoch=50, num_batches=43)
-# model.fit(data["train_vec"], data["val_vec"], train_users=data["train_users"], test_users=data["test_users"])
-# model.predict_val(data["train_users"], data["test_users"])
-# model.save_model(os.path.join(ckpt,"pmf.mat"))
+model = PMF(epsilon=e, _lambda=l, momentum=m, num_feat=50, maxepoch=50, num_batches=43)
+model.fit(data["train_vec"], data["val_vec"], train_users=data["train_users"], test_users=data["test_users"])
+model.predict_val(data["train_users"], data["test_users"])
+model.save_model(os.path.join(ckpt,"pmf.mat"))
 
