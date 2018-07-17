065a360109e80eb2b5b51efe8a06537e91d62cec
zakosai <linh.nguyen.1992@gmail.com>
2018-07-17 12:39:12 +0900

    relu

--- cf-vae/train.sh
+++ cf-vae/train.sh
@@ -58,8 +58,8 @@ user_no=2118
 item_no=7780
 
 
-        #python train_vae.py --ckpt_folder=$f/0_layer --data_dir=data2/$f/ --zdim=50 --data_type=$r --user_dim=$dim --type=text
-        #python train_vae.py --ckpt_folder=$f/0_layer --data_dir=data2/$f/ --zdim=50 --data_type=$r --user_dim=$dim --type=user
+        python train_vae.py --ckpt_folder=$f/0_layer --data_dir=data2/$f/ --zdim=50 --data_type=$r --user_dim=$dim --type=text
+        python train_vae.py --ckpt_folder=$f/0_layer --data_dir=data2/$f/ --zdim=50 --data_type=$r --user_dim=$dim --type=user
 
 
         #python train_vae.py --ckpt_folder=$f/$r2 --data_dir=data2/$f/ --zdim=50 --data_type=$r --user_dim=$dim
@@ -67,5 +67,5 @@ item_no=7780
         #python train_dae.py --ckpt_folder=$f/0_layer --data_dir=data2/$f/ --zdim=50 --data_type=$r --user_dim=$dim --type=text
 
         #python train_cf_dae.py --model=0 --ckpt_folder=$f/0_layer --data_dir=data2/$f/ --iter=50 --data_type=$r --user_no=$user_no --item_no=$item_no --gridsearch=1 --zdim=50
-        python train_cvae_user.py --model=0 --ckpt_folder=$f/0_layer --data_dir=data2/$f/ --iter=50 --zdim=50 --gridsearch=1 --data_type=$r --user_dim=$dim --user_no=$user_no --item_no=$item_no
+        python train_cvae_user.py --model=0 --ckpt_folder=$f/0_layer --data_dir=data2/$f/ --iter=50 --zdim=50 --data_type=$r --user_dim=$dim --user_no=$user_no --item_no=$item_no
         #python train_cvae_extend.py --model=0 --ckpt_folder=$f/0_layer --data_dir=data2/$f/ --iter=50 --zdim=50 --gridsearch=1 --data_type=$r --user_no=$user_no --item_no=$item_no
