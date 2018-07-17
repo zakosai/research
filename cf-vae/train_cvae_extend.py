2f33c69866015899ab69188111f4024e2deee8a8
zakosai <linh.nguyen.1992@gmail.com>
2018-07-17 16:08:44 +0900

    1 layer

--- cf-vae/train_cvae_extend.py
+++ cf-vae/train_cvae_extend.py
@@ -73,9 +73,9 @@ def load_rating(path):
 
 
 params = params()
-params.lambda_u = 1
+params.lambda_u = 10
 params.lambda_v = 1
-params.lambda_r = 0.1
+params.lambda_r = 10
 params.C_a = 1
 params.C_b = 0.01
 params.max_iter_m = 1
@@ -110,7 +110,7 @@ if gs == 1:
                 params.lambda_r = r
                 if i > -1:
                     model = cf_vae_extend(num_users=args.user_no, num_items=args.item_no, num_factors=num_factors, params=params,
-                                          input_dim=8000, encoding_dims=[], z_dim = 50, decoding_dims=[8000],
+                                          input_dim=8000, encoding_dims=[200], z_dim = 50, decoding_dims=[200,8000],
                                           encoding_dims_str=[200], decoding_dims_str=[200, 4526], loss_type='cross_entropy',
                                           model = model_type, ckpt_folder=ckpt, initial=initial)
                     model.fit(data["train_users"], data["train_items"], data["content"], params, data["test_users"])
@@ -126,7 +126,7 @@ if gs == 1:
                 i += 1
 else:
     model = cf_vae_extend(num_users=7981, num_items=19184, num_factors=num_factors, params=params,
-                          input_dim=8000, encoding_dims=[200, 100], z_dim = zdim, decoding_dims=[100, 200, 8000],
+                          input_dim=8000, encoding_dims=[200], z_dim = zdim, decoding_dims=[200, 8000],
                           encoding_dims_str=[500, 200], decoding_dims_str=[200, 500, 4526], loss_type='cross_entropy',
                           model = model_type, ckpt_folder=ckpt, initial=initial)
     model.fit(data["train_users"], data["train_items"], data["content"],params, data["test_users"])
--- cf-vae/train_cvae_user.py
+++ cf-vae/train_cvae_user.py
@@ -134,7 +134,7 @@ if gs == 1:
                 i += 1
 else:
     model = cf_vae_extend(num_users=args.user_no, num_items=args.item_no, num_factors=num_factors, params=params,
-                          input_dim=8000, encoding_dims=[200, 100], z_dim = zdim, decoding_dims=[100, 200, 8000],
+                          input_dim=8000, encoding_dims=[200], z_dim = zdim, decoding_dims=[200, 8000],
                           encoding_dims_str=[500, 200], decoding_dims_str=[200, 500, 4526], loss_type='cross_entropy',
                           model = model_type, ckpt_folder=ckpt, initial=initial, user_dim=args.user_dim)
     model.fit(data["train_users"], data["train_items"], data["content"], params, data["test_users"], data["user"])
