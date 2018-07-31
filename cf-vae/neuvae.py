a223b6d4354bceb0bc1d6dc9370b16a8ba1cde3b
zakosai <linh.nguyen.1992@gmail.com>
2018-07-31 18:13:07 +0900

    change loss function

--- cf-vae/neuvae.py
+++ cf-vae/neuvae.py
@@ -191,7 +191,6 @@ class neuVAE:
                     x_batch = x_data[items[i * self.params.batch_size:idx]]
 
                     r = self.sess.run(rating_, feed_dict={self.x_: x_batch, self.x_u_: u_batch})
-                    print(len(r))
 
                     rat += r.tolist()
 
