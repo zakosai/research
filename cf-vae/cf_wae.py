c8c77955568a84ed32c0527606d93ab1ea755294
zakosai <linh.nguyen.1992@gmail.com>
2018-08-18 17:16:14 +0900

    wae

--- cf-vae/cf_wae.py
+++ cf-vae/cf_wae.py
@@ -114,9 +114,9 @@ class cf_vae_extend:
         decoder_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='text/decode')
         ae_vars = encoder_vars + decoder_vars
 
-        ae_opt = tf.train.AdamOptimizer(self.learning_rate).minimize(loss=self.wae_objective,
+        ae_opt = tf.train.AdamOptimizer(self.params.learning_rate).minimize(loss=self.wae_objective,
                                    var_list=encoder_vars + decoder_vars)
-        z_adv_opt = tf.train.AdamOptimizer(self.learning_rate).minimize(
+        z_adv_opt = tf.train.AdamOptimizer(self.params.learning_rate).minimize(
             loss=self.loss_gan[0], var_list=z_adv_vars)
 
         self.sess = tf.Session()
