314ee183c044c6d322982697a1cd9f27d4637156
zakosai <linh.nguyen.1992@gmail.com>
2018-08-19 15:01:57 +0900

    VDCNN

--- DeepCoNN/model/XceptionNet.py
+++ DeepCoNN/model/XceptionNet.py
@@ -260,26 +260,26 @@ class DeepCoNN(object):
 
         # Convolutional Block 64
         for _ in range(num_conv_blocks[0] - 1):
-            out = identity_block(out, filters=64, kernel_size=3, use_bias=use_bias, shortcut=shortcut)
-        out = conv_block(out, filters=64, kernel_size=3, use_bias=use_bias, shortcut=shortcut,
+            out = self.identity_block(out, filters=64, kernel_size=3, use_bias=use_bias, shortcut=shortcut)
+        out = self.conv_block(out, filters=64, kernel_size=3, use_bias=use_bias, shortcut=shortcut,
                          pool_type=pool_type, sorted=sorted, stage=1)
 
         # Convolutional Block 128
         for _ in range(num_conv_blocks[1] - 1):
-            out = identity_block(out, filters=128, kernel_size=3, use_bias=use_bias, shortcut=shortcut)
-        out = conv_block(out, filters=128, kernel_size=3, use_bias=use_bias, shortcut=shortcut,
+            out = self.dentity_block(out, filters=128, kernel_size=3, use_bias=use_bias, shortcut=shortcut)
+        out = self.conv_block(out, filters=128, kernel_size=3, use_bias=use_bias, shortcut=shortcut,
                          pool_type=pool_type, sorted=sorted, stage=2)
 
         # Convolutional Block 256
         for _ in range(num_conv_blocks[2] - 1):
-            out = identity_block(out, filters=256, kernel_size=3, use_bias=use_bias, shortcut=shortcut)
-        out = conv_block(out, filters=256, kernel_size=3, use_bias=use_bias, shortcut=shortcut,
+            out = self.identity_block(out, filters=256, kernel_size=3, use_bias=use_bias, shortcut=shortcut)
+        out = self.conv_block(out, filters=256, kernel_size=3, use_bias=use_bias, shortcut=shortcut,
                          pool_type=pool_type, sorted=sorted, stage=3)
 
         # Convolutional Block 512
         for _ in range(num_conv_blocks[3] - 1):
-            out = identity_block(out, filters=512, kernel_size=3, use_bias=use_bias, shortcut=shortcut)
-        out = conv_block(out, filters=512, kernel_size=3, use_bias=use_bias, shortcut=False,
+            out = self.identity_block(out, filters=512, kernel_size=3, use_bias=use_bias, shortcut=shortcut)
+        out = self.conv_block(out, filters=512, kernel_size=3, use_bias=use_bias, shortcut=False,
                          pool_type=None, stage=4)
 
         # k-max pooling with k = 8
