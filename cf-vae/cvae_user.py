762b395de76aa9b35ce36beda205b3ca08591c16
zakosai <linh.nguyen.1992@gmail.com>
2018-08-20 20:01:57 +0900

    check

--- cf-vae/cvae_user.py
+++ cf-vae/cvae_user.py
@@ -416,7 +416,7 @@ class cf_vae_extend:
             # update V
             ids = np.array([len(x) for x in users]) > 0
             u = self.U[ids]
-            XX = np.dot(u.T, u) * params.C_b 
+            XX = np.dot(u.T, u) * params.C_b
             for j in xrange(self.num_items):
                 user_ids = items[j]
                 m = len(user_ids)
@@ -429,6 +429,7 @@ class cf_vae_extend:
                         x = params.C_a * np.sum(self.U[user_ids, :], axis=0) + params.lambda_v * (self.exp_z[j,:] + self.exp_z_im[j,:])
                     elif self.model != 6:
                         x = params.C_a * np.sum(self.U[user_ids, :], axis=0) + params.lambda_v * self.exp_z[j, :]
+                        print(A.shape, x.shape)
                         #x = params.C_a * np.sum(self.U[user_ids, :], axis=0)
                     else:
                         x = params.C_a * np.sum(self.U[user_ids, :], axis=0) + params.lambda_v * self.exp_z_im[j,:]
