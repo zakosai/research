8159634f0c7e397c82850527e3a10ebf6f25de22
zakosai <linh.nguyen.1992@gmail.com>
2018-08-21 17:28:03 +0900

    Outdoor sport Toy

--- cf-vae/train.sh
+++ cf-vae/train.sh
@@ -1,32 +1,23 @@
-#folders='Toy Tool Beauty Electronics TV'
-#rate='1'
-#
-#
-#for f in $folders
-#do
-#    for r in $rate
-#    do
-#        dim="$(sed -n '3p' data2/$f/info.txt)"
-#        user_no="$(sed -n '1p' data2/$f/info.txt)"
-#        item_no="$(sed -n '2p' data2/$f/info.txt)"
-#        python train_vae.py --ckpt_folder=$f/$r --data_dir=data2/$f/ --zdim=50 --data_type=$r --user_dim=$dim --type=text
-#        python train_vae.py --ckpt_folder=$f/$r --data_dir=data2/$f/ --zdim=50 --data_type=$r --user_dim=$dim --type=user
-#
-#
-#        #python train_vae.py --ckpt_folder=$f/$r2 --data_dir=data2/$f/ --zdim=50 --data_type=$r --user_dim=$dim
-#
-#        python train_dae.py --ckpt_folder=$f/$r --data_dir=data2/$f/ --zdim=50 --data_type=$r --user_dim=$dim --type=text
-#
-#        python train_cf_dae.py --model=0 --ckpt_folder=$f/$r --data_dir=data2/$f/ --iter=50 --data_type=$r --user_no=$user_no --item_no=$item_no --gridsearch=1 --zdim=50
-#        python train_cvae_user.py --model=0 --ckpt_folder=$f/$r --data_dir=data2/$f/ --iter=50 --zdim=50 --gridsearch=1 --data_type=$r --user_dim=$dim --user_no=$user_no --item_no=$item_no
-#        python train_cvae_extend.py --model=0 --ckpt_folder=$f/$r --data_dir=data2/$f/ --iter=50 --zdim=50 --gridsearch=1 --data_type=$r --user_no=$user_no --item_no=$item_no
-#
-#        #python train_dae.py --ckpt_folder=$f/$r --data_dir=data2/$f/ --zdim=50 --data_type=$r --user_dim=$dim --type=user
-#        #python train_cdae_user.py --model=0 --ckpt_folder=$f/$r --data_dir=data2/$f/ --iter=50 --zdim=50 --gridsearch=1 --data_type=$r --user_dim=$dim --user_no=$user_no --item_no=$item_no
-#
-#
-#
-#    done
+folders='Outdoor sport Toy'
+rate='8 80'
+
+
+for f in $folders
+do
+    for r in $rate
+    do
+        dim="$(sed -n '3p' data2/$f/info.txt)"
+        user_no="$(sed -n '1p' data2/$f/info.txt)"
+        item_no="$(sed -n '2p' data2/$f/info.txt)"
+        python wae.py --ckpt_folder=$f/$r_wae --data_dir=data2/$f/ --zdim=100 --data_type=$r --user_dim=$dim --type=text
+        python train_cf_wae.py --model=0 --ckpt_folder=$f/$r_wae --data_dir=data2/$f/ --iter=50 --data_type=$r --user_no=$user_no --item_no=$item_no --gridsearch=1 --zdim=100
+
+        #python train_dae.py --ckpt_folder=$f/$r --data_dir=data2/$f/ --zdim=50 --data_type=$r --user_dim=$dim --type=user
+        #python train_cdae_user.py --model=0 --ckpt_folder=$f/$r --data_dir=data2/$f/ --iter=50 --zdim=50 --gridsearch=1 --data_type=$r --user_dim=$dim --user_no=$user_no --item_no=$item_no
+
+
+
+    done
 
 
 #done
