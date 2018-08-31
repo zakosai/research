49383e53a9fb56832ea4f5949d1a9c69f4302964
zakosai <linh.nguyen.1992@gmail.com>
2018-08-31 15:12:11 +0900

    train total again

--- cf-vae/train.sh
+++ cf-vae/train.sh
@@ -1,6 +1,6 @@
 folders='Health Toy TV CD Tool Beauty Kitchen Office Grocery Baby Clothing Kindle Phone Video Pet Music Instrument
 Automotive Garden Electronics Books'
-rate='80'
+rate='1 8'
 
 
 for f in $folders
@@ -11,13 +11,13 @@ do
         #dim="$(sed -n '3p' data2/$f/info.txt)"
         user_no="$(sed -n '1p' data2/$f/info.txt)"
         item_no="$(sed -n '2p' data2/$f/info.txt)"
-        python wae.py --ckpt_folder=$f/80 --data_dir=data2/$f/ --zdim=100 --data_type=$r --type=text
-        python train_cf_wae.py --model=0 --ckpt_folder=$f/$r --data_dir=data2/$f/ --iter=50 --data_type=$r --user_no=$user_no --item_no=$item_no --gridsearch=1 --zdim=100
-        python train_vae.py --ckpt_folder=$f/80 --data_dir=data2/$f/ --zdim=100 --data_type=$r --type=text
-        python train_cvae_extend.py --model=0 --ckpt_folder=$f/80 --data_dir=data2/$f/ --iter=50 --data_type=$r \
+        python wae.py --ckpt_folder=$f/sum_$r --data_dir=data2/$f/ --zdim=100 --data_type=$r --type=text
+        python train_cf_wae.py --model=0 --ckpt_folder=$f/sum_$r --data_dir=data2/$f/ --iter=50 --data_type=$r --user_no=$user_no --item_no=$item_no --gridsearch=1 --zdim=100
+        python train_vae.py --ckpt_folder=$f/sum_$r --data_dir=data2/$f/ --zdim=100 --data_type=$r --type=text
+        python train_cvae_extend.py --model=0 --ckpt_folder=$f/sum_$r --data_dir=data2/$f/ --iter=50 --data_type=$r \
         --user_no=$user_no --item_no=$item_no --gridsearch=1 --zdim=100
-        python train_dae.py --ckpt_folder=$f/80 --data_dir=data2/$f/ --zdim=100 --data_type=$r --type=text
-        python train_cf_dae.py --model=0 --ckpt_folder=$f/$r --data_dir=data2/$f/ --iter=50 --data_type=$r \
+        python train_dae.py --ckpt_folder=$f/sum_$r --data_dir=data2/$f/ --zdim=100 --data_type=$r --type=text
+        python train_cf_dae.py --model=0 --ckpt_folder=$f/sum_$r --data_dir=data2/$f/ --iter=50 --data_type=$r \
         --user_no=$user_no --item_no=$item_no --gridsearch=1 --zdim=100
 
         #python train_dae.py --ckpt_folder=$f/$r --data_dir=data2/$f/ --zdim=50 --data_type=$r --user_dim=$dim --type=user
