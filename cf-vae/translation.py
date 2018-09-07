5a6a4fdb7a9513ef0bcc0daa572ed306104cfcd2
zakosai <linh.nguyen.1992@gmail.com>
2018-09-07 15:50:46 +0900

    translation

--- cf-vae/translation.py
+++ cf-vae/translation.py
@@ -176,7 +176,7 @@ def one_hot_vector(A, num_product):
     return one_hot_A
 
 def calc_recall(pred, test):
-    pred_ab = np.argsort(pred)[:, -10:][::-1]
+    pred_ab = np.argsort(pred)[:, -100:][::-1]
     recall = []
     for i in range(len(pred_ab)):
         hits = set(test[i]) & set(pred_ab[i])
@@ -185,7 +185,7 @@ def calc_recall(pred, test):
     return np.mean(np.array(recall))
 
 def main():
-    iter = 200
+    iter = 100
     batch_size= 500
     clothing_num = 18226
     health_num = 16069
