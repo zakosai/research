147867ee1a117dbe762e1ff3a87b35d94f3afc4a
zakosai <linh.nguyen.1992@gmail.com>
2018-09-12 15:58:31 +0900

    translation new

--- cf-vae/translation.py
+++ cf-vae/translation.py
@@ -207,7 +207,7 @@ def one_hot_vector2(A, num_product):
     return one_hot
 
 def calc_recall(pred, test):
-    pred_ab = np.argsort(pred)[::-1][:, 10:]
+    pred_ab = np.argsort(pred)[::-1][:, :10]
     recall = []
     for i in range(len(pred_ab)):
         hits = set(test[i]) & set(pred_ab[i])
