66d61a23a5e21d7d8e15df229e4810607073e7d0
zakosai <linh.nguyen.1992@gmail.com>
2018-09-13 15:08:30 +0900

    add dropout

--- cf-vae/translation.py
+++ cf-vae/translation.py
@@ -224,7 +224,7 @@ def calc_rmse(pred, test):
     return np.sqrt(np.mean((test-pred)**2))
 
 def main():
-    iter = 1000
+    iter = 100
     batch_size= 500
     clothing_num = 8364
     health_num = 15084
