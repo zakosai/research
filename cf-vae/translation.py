6c04a81b2c69da0b3a5cf894693589ea613191a4
zakosai <linh.nguyen.1992@gmail.com>
2018-10-17 20:17:56 +0900

    keep encode dropout, revert other

--- cf-vae/translation.py
+++ cf-vae/translation.py
@@ -57,7 +57,7 @@ class Translation:
     def dec(self, x, scope, decode_dim, reuse=False):
         x_ = x
         if self.train:
-            x_ = tf.nn.dropout(x_, 0.3)
+            x_ = tf.nn.dropout(x_, 0.5)
         with tf.variable_scope(scope, reuse=reuse):
             for i in range(len(decode_dim)):
                 x_ = fully_connected(x_, decode_dim[i], self.active_function, scope="dec_%d" % i)
@@ -68,7 +68,7 @@ class Translation:
 
         with tf.variable_scope(scope, reuse=reuse):
             if self.train:
-                x_ = tf.nn.dropout(x_, 0.3)
+                x_ = tf.nn.dropout(x_, 0.5)
             for i in range(len(adv_dim)):
                 x_ = fully_connected(x_, adv_dim[i], self.active_function, scope="adv_%d" % i)
         return x_
