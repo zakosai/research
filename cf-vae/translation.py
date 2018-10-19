03a7b5fab65fc00e93cf04b93c1c5ae05a121144
zakosai <linh.nguyen.1992@gmail.com>
2018-10-19 17:56:55 +0900

    decrease wide

--- cf-vae/translation.py
+++ cf-vae/translation.py
@@ -240,10 +240,10 @@ def main():
     clothing_num = 18226
     health_num = 16079
     encoding_dim_A = [1000, 500]
-    encoding_dim_B = [1500, 500]
+    encoding_dim_B = [1000, 500]
     share_dim = [100]
     decoding_dim_A = [500, 1000, health_num]
-    decoding_dim_B = [500, 1500, clothing_num]
+    decoding_dim_B = [500, 1000, clothing_num]
     z_dim = 50
     adv_dim_A = adv_dim_B = [200, 100, 1]
     checkpoint_dir = "translation/Health_Clothing/"
@@ -322,8 +322,8 @@ def main():
                 print("recall B: %f" % (calc_recall(y_ab, dense_B_test)))
                 print("recall A: %f" % (calc_recall(y_ba, dense_A_test)))
                 model.train = True
-        if i%100 == 0:
-            model.learning_rate /= 10
+        if i%50 == 0:
+            model.learning_rate /= 2
 
             # pred = np.array(y_ab).flatten()
             # test = np.array(user_B_val).flatten()
