927e89ff6ca571e4b7171e5d9ec0242e74e1dd12
zakosai <linh.nguyen.1992@gmail.com>
2018-10-19 23:59:50 +0900

    train

--- cf-vae/train.sh
+++ cf-vae/train.sh
@@ -1,9 +1,9 @@
 
 python train_cvae_extend.py --model=0 --ckpt_folder=translation/Health_Clothing/ --data_dir=data/Health_Clothing/ \
---iter=50 --data_type=70 --user_no=6557 --item_no=34295 --gridsearch=1 --zdim=100
+--iter=50 --data_type=70 --user_no=6557 --item_no=34296 --gridsearch=1 --zdim=100
 
 python train_cf_dae.py --model=0 --ckpt_folder=translation/Health_Clothing/ --data_dir=data/Health_Clothing/ \
---iter=50 --data_type=70 --user_no=6557 --item_no=34295 --gridsearch=1 --zdim=100
+--iter=50 --data_type=70 --user_no=6557 --item_no=34296 --gridsearch=1 --zdim=100
 
 #python train_cvae_extend.py --model=0 --ckpt_folder=translation/Grocery_Health/ --data_dir=data/Grocery_Health/ \
 #--iter=50 --data_type=70 --user_no=6848 --item_no=23448 --gridsearch=1 --zdim=100
