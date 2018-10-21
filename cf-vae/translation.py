9fe2e491598d864e84c5d458b63f5d4bfbb6ae9e
zakosai <linh.nguyen.1992@gmail.com>
2018-10-21 17:31:32 +0900

    new loss function

--- cf-vae/translation.py
+++ cf-vae/translation.py
@@ -161,12 +161,12 @@ class Translation:
         self.loss_d= loss_d_A + loss_d_B
 
         # Loss cycle - consistency (CC)
-        # loss_CC_A = self.lambda_3 * self.loss_kl(z_mu_A, z_sigma_A) + self.lambda_3 * self.loss_kl(z_mu_ABA, z_sigma_ABA)\
-        #             + self.lambda_4 * self.loss_reconstruct(x_A, y_ABA)
-        # loss_CC_B = self.lambda_3 * self.loss_kl(z_mu_B, z_sigma_B) + self.lambda_3 * self.loss_kl(z_mu_BAB, z_sigma_BAB)\
-        #             + self.loss_reconstruct(x_B, y_BAB)
-        loss_CC_A = self.lambda_4*self.loss_reconstruct(x_A, y_ABA)
-        loss_CC_B = self.lambda_4 * self.loss_reconstruct(x_B, y_BAB)
+        loss_CC_A = self.lambda_3 * self.loss_kl(z_mu_A, z_sigma_A) + self.lambda_3 * self.loss_kl(z_mu_ABA, z_sigma_ABA)\
+                    + self.lambda_4 * self.loss_reconstruct(x_A, y_ABA)
+        loss_CC_B = self.lambda_3 * self.loss_kl(z_mu_B, z_sigma_B) + self.lambda_3 * self.loss_kl(z_mu_BAB, z_sigma_BAB)\
+                    + self.loss_reconstruct(x_B, y_BAB)
+        # loss_CC_A = self.lambda_4 * self.loss_reconstruct(x_A, y_ABA)
+        # loss_CC_B = self.lambda_4 * self.loss_reconstruct(x_B, y_BAB)
 
         self.loss_CC = loss_CC_A + loss_CC_B
 
