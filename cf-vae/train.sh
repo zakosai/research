80952c79bbcfac740084021f019c18af7fee6ff9
zakosai <linh.nguyen.1992@gmail.com>
2018-10-21 19:13:01 +0900

    train sh

--- cf-vae/train.sh
+++ cf-vae/train.sh
@@ -1,14 +1,3 @@
-mkdir translation/Health_Beauty
-python train_cvae_extend.py --model=0 --ckpt_folder=translation/Health_Beauty/ --data_dir=data/Health_Beauty/ \
---iter=50 --data_type=70 --user_no=7026 --item_no=26248 --gridsearch=1 --zdim=100
-python train_dae.py --ckpt_folder=translation/Health_Beauty/ --data_dir=data/Health_Beauty/ --zdim=100 \
---data_type=70 --type=text
-python train_cf_dae.py --model=0 --ckpt_folder=translation/Health_Beauty/ --data_dir=data/Health_Beauty/ \
---iter=50 --data_type=70 --user_no=7026 --item_no=26248 --gridsearch=1 --zdim=100
-
-mkdir translation/Health_Electronics
-python train_vae.py --ckpt_folder=translation/Health_Electronics/ --data_dir=data/Health_Electronics/ --zdim=100 \
---data_type=70 --type=text
 python train_cvae_extend.py --model=0 --ckpt_folder=translation/Health_Electronics/ --data_dir=data/Health_Electronics/ \
 --iter=50 --data_type=70 --user_no=14433 --item_no=61410 --gridsearch=1 --zdim=100
 python train_dae.py --ckpt_folder=translation/Health_Electronics/ --data_dir=data/Health_Electronics/ --zdim=100 \
