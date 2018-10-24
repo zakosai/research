103e949f212e20e5715b7359562afd27466e7dac
zakosai <linh.nguyen.1992@gmail.com>
2018-10-24 21:07:09 +0900

    shape

--- cf-vae/translation.py
+++ cf-vae/translation.py
@@ -52,8 +52,7 @@ class Translation:
             for i in range(len(encode_dim)):
                 x_ = fully_connected(x_, encode_dim[i], scope="enc_%d"%i,
                                      weights_regularizer=self.regularizer)
-                x_ = tf.nn.leaky_relu(x_)
-                x_ = batch_norm(x_, decay=0.995)
+                # x_ = tf.nn.leaky_relu(x_)
         return x_
 
     def dec(self, x, scope, decode_dim, reuse=False):
@@ -88,8 +87,8 @@ class Translation:
 
     def gen_z(self, h, scope, reuse=False):
         with tf.variable_scope(scope, reuse=reuse):
-            z_mu = fully_connected(h, self.z_dim, self.active_function, scope="z_mu")
-            z_sigma = fully_connected(h, self.z_dim, self.active_function, scope="z_sigma")
+            z_mu = fully_connected(h, self.z_dim, scope="z_mu")
+            z_sigma = fully_connected(h, self.z_dim,  scope="z_sigma")
             e = tf.random_normal(tf.shape(z_mu))
             z = z_mu + tf.sqrt(tf.maximum(tf.exp(z_sigma), self.eps)) * e
         return z, z_mu, z_sigma
