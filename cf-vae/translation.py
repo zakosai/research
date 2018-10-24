08ed6d42d7a7e6a1d749a09d5e975469371834df
zakosai <linh.nguyen.1992@gmail.com>
2018-10-24 20:51:29 +0900

    shape

--- cf-vae/translation.py
+++ cf-vae/translation.py
@@ -261,10 +261,10 @@ def main():
     clothing_num = 8364
     encoding_dim_A = [500]
     encoding_dim_B = [500]
-    share_dim = [100]
+    share_dim = [200]
     decoding_dim_A = [500, health_num]
     decoding_dim_B = [500, clothing_num]
-    z_dim = 50
+    z_dim = 100
     adv_dim_A = adv_dim_B = [200, 100, 1]
     checkpoint_dir = "translation/%s_%s/"%(A,B)
     user_A, user_B, dense_A, dense_B = create_dataset(health_num, clothing_num, A, B)
