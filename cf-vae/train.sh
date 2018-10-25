a3fc985764cc1a7edfc724fc093991284ff49c1a
zakosai <linh.nguyen.1992@gmail.com>
2018-10-25 23:14:25 +0900

    multi-VAE

--- cf-vae/train.sh
+++ cf-vae/train.sh
@@ -1,9 +1,11 @@
-python train_cvae_extend.py --model=0 --ckpt_folder=translation/Health_Electronics/ --data_dir=data/Health_Electronics/ \
---iter=50 --data_type=70 --user_no=14433 --item_no=61410 --gridsearch=1 --zdim=100
-python train_dae.py --ckpt_folder=translation/Health_Electronics/ --data_dir=data/Health_Electronics/ --zdim=100 \
+python train_vae.py --ckpt_folder=translation/Drama_Horror/ --data_dir=data/Drama_Horror/ --zdim=100 \
 --data_type=70 --type=text
-python train_cf_dae.py --model=0 --ckpt_folder=translation/Health_Electronics/ --data_dir=data/Health_Electronics/ \
---iter=50 --data_type=70 --user_no=14433 --item_no=61410 --gridsearch=1 --zdim=100
+python train_cvae_extend.py --model=0 --ckpt_folder=translation/Drama_Horror/ --data_dir=data/Drama_Horror/ \
+--iter=50 --data_type=70 --user_no=6037 --item_no=3206 --gridsearch=1 --zdim=100
+python train_dae.py --ckpt_folder=translation/Drama_Horror/ --data_dir=data/Drama_Horror/ --zdim=100 \
+--data_type=70 --type=text
+python train_cf_dae.py --model=0 --ckpt_folder=translation/Drama_Horror/ --data_dir=data/Drama_Horror/ \
+--iter=50 --data_type=70 --user_no=6037 --item_no=3206 --gridsearch=1 --zdim=100
 
 #python train_cvae_extend.py --model=0 --ckpt_folder=translation/Grocery_Health/ --data_dir=data/Grocery_Health/ \
 #--iter=50 --data_type=70 --user_no=6848 --item_no=23448 --gridsearch=1 --zdim=100
