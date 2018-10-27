188f5ce6a2e4059bb84e7896c59c08722c74fc07
zakosai <linh.nguyen.1992@gmail.com>
2018-10-27 22:30:23 +0900

    train

--- cf-vae/train.sh
+++ cf-vae/train.sh
@@ -1,46 +1,35 @@
-mkdir translation2
-mkdir translation2/Health_Clothing
-python train_vae.py --ckpt_folder=translation2/Health_Clothing/ --data_dir=data/Health_Clothing/ --zdim=50 \
+mkdir translation/Drama_Comedy
+python train_vae.py --ckpt_folder=translation/Drama_Comedy/ --data_dir=data/Drama_Comedy/ --zdim=50 \
 --data_type=70 --type=text
-python train_cvae_extend.py --model=0 --ckpt_folder=translation2/Health_Clothing/ --data_dir=data/Health_Clothing/ \
---iter=50 --data_type=70 --user_no=6557 --item_no=34296 --gridsearch=1 --zdim=50
-python train_dae.py --ckpt_folder=translation2/Health_Clothing/ --data_dir=data/Health_Clothing/ --zdim=50 \
+python train_cvae_extend.py --model=0 --ckpt_folder=translation/Drama_Comedy/ --data_dir=data/Drama_Comedy/ \
+--iter=50 --data_type=70 --user_no=6023 --item_no=2577 --gridsearch=1 --zdim=50
+python train_dae.py --ckpt_folder=translation/Drama_Comedy/ --data_dir=data/Drama_Comedy/ --zdim=50 \
 --data_type=70 --type=text
-python train_cf_dae.py --model=0 --ckpt_folder=translation2/Health_Clothing/ --data_dir=data/Health_Clothing/ \
---iter=50 --data_type=70 --user_no=6557 --item_no=34296 --gridsearch=1 --zdim=50
+python train_cf_dae.py --model=0 --ckpt_folder=translation/Drama_Comedy/ --data_dir=data/Drama_Comedy/ \
+--iter=50 --data_type=70 --user_no=6023 --item_no=2577 --gridsearch=1 --zdim=50
 
-mkdir translation2/Video_TV
+mkdir translation/Romance_Thriller
 
-python train_vae.py --ckpt_folder=translation2/Video_TV/ --data_dir=data/Video_TV/ --zdim=50 \
+python train_vae.py --ckpt_folder=translation/Romance_Thriller/ --data_dir=data/Romance_Thriller/ --zdim=50 \
 --data_type=70 --type=text
-python train_cvae_extend.py --model=0 --ckpt_folder=translation2/Video_TV/ --data_dir=data/Video_TV/ \
---iter=50 --data_type=70 --user_no=5459 --item_no=28650 --gridsearch=1 --zdim=50
-python train_dae.py --ckpt_folder=translation2/Video_TV/ --data_dir=data/Video_TV/ --zdim=50 \
+python train_cvae_extend.py --model=0 --ckpt_folder=translation/Romance_Thriller/ --data_dir=data/Romance_Thriller/ \
+--iter=50 --data_type=70 --user_no=5875 --item_no=930 --gridsearch=1 --zdim=50
+python train_dae.py --ckpt_folder=translation/Romance_Thriller/ --data_dir=data/Romance_Thriller/ --zdim=50 \
 --data_type=70 --type=text
-python train_cf_dae.py --model=0 --ckpt_folder=translation2/Video_TV/ --data_dir=data/Video_TV/ \
---iter=50 --data_type=70 --user_no=5459 --item_no=28650 --gridsearch=1 --zdim=50
+python train_cf_dae.py --model=0 --ckpt_folder=translation/Romance_Thriller/ --data_dir=data/Romance_Thriller/ \
+--iter=50 --data_type=70 --user_no=5875 --item_no=930 --gridsearch=1 --zdim=50
 
-mkdir translation2/Drama_Horror
 
-python train_vae.py --ckpt_folder=translation2/Drama_Horror/ --data_dir=data/Drama_Horror/ --zdim=50 \
---data_type=70 --type=text
-python train_cvae_extend.py --model=0 --ckpt_folder=translation2/Drama_Horror/ --data_dir=data/Drama_Horror/ \
---iter=50 --data_type=70 --user_no=6037 --item_no=3206 --gridsearch=1 --zdim=50
-python train_dae.py --ckpt_folder=translation2/Drama_Horror/ --data_dir=data/Drama_Horror/ --zdim=50 \
---data_type=70 --type=text
-python train_cf_dae.py --model=0 --ckpt_folder=translation2/Drama_Horror/ --data_dir=data/Drama_Horror/ \
---iter=50 --data_type=70 --user_no=6037 --item_no=3206 --gridsearch=1 --zdim=50
-
-mkdir translation2/Health_Grocery
-
-python train_vae.py --ckpt_folder=translation2/Health_Grocery/ --data_dir=data/Health_Grocery/ --zdim=50 \
---data_type=70 --type=text
-python train_cvae_extend.py --model=0 --ckpt_folder=translation2/Health_Grocery/ --data_dir=data/Health_Grocery/ \
---iter=50 --data_type=70 --user_no=6848 --item_no=23448 --gridsearch=1 --zdim=50
-python train_dae.py --ckpt_folder=translation2/Health_Grocery/ --data_dir=data/Health_Grocery/ --zdim=50 \
---data_type=70 --type=text
-python train_cf_dae.py --model=0 --ckpt_folder=translation2/Health_Grocery/ --data_dir=data/Health_Grocery/ \
---iter=50 --data_type=70 --user_no=6848 --item_no=23448 --gridsearch=1 --zdim=50
+#mkdir translation2/Health_Grocery
+#
+#python train_vae.py --ckpt_folder=translation2/Health_Grocery/ --data_dir=data/Health_Grocery/ --zdim=50 \
+#--data_type=70 --type=text
+#python train_cvae_extend.py --model=0 --ckpt_folder=translation2/Health_Grocery/ --data_dir=data/Health_Grocery/ \
+#--iter=50 --data_type=70 --user_no=6848 --item_no=23448 --gridsearch=1 --zdim=50
+#python train_dae.py --ckpt_folder=translation2/Health_Grocery/ --data_dir=data/Health_Grocery/ --zdim=50 \
+#--data_type=70 --type=text
+#python train_cf_dae.py --model=0 --ckpt_folder=translation2/Health_Grocery/ --data_dir=data/Health_Grocery/ \
+#--iter=50 --data_type=70 --user_no=6848 --item_no=23448 --gridsearch=1 --zdim=50
 
 mkdir translation2/Health_Beauty
 
