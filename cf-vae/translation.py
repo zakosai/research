bd95df6b507436bd1ca5b57204b0d5627791b20e
zakosai <linh.nguyen.1992@gmail.com>
2018-10-28 15:20:57 +0900

    movielens

--- cf-vae/translation.py
+++ cf-vae/translation.py
@@ -350,8 +350,8 @@ def main():
     # test_B = [t - train_size - val_size for t in test_B]
 
     model = Translation(batch_size, num_A, num_B, encoding_dim_A, decoding_dim_A, encoding_dim_B,
-                        decoding_dim_B, adv_dim_A, adv_dim_B, z_dim, share_dim, learning_rate=1e-3, lambda_2=1,
-                        lambda_4=1)
+                        decoding_dim_B, adv_dim_A, adv_dim_B, z_dim, share_dim, learning_rate=1e-4, lambda_2=100,
+                        lambda_4=100)
     model.build_model()
 
     sess = tf.Session()
