ce3d155d5ca58cc56041697a13a6d4c632c55bf2
zakosai <linh.nguyen.1992@gmail.com>
2018-10-28 13:51:42 +0900

    movielens

--- cf-vae/translation.py
+++ cf-vae/translation.py
@@ -50,11 +50,11 @@ class Translation:
         # x_ = tf.reshape(x_, (-1, 10000))
 
         # if self.train:
-        x_ = tf.nn.dropout(x_, 0.5)
         with tf.variable_scope(scope, reuse=reuse):
             for i in range(len(encode_dim)):
                 x_ = fully_connected(x_, encode_dim[i], self.active_function, scope="enc_%d"%i,
                                      weights_regularizer=self.regularizer)
+                x_ = tf.nn.dropout(x_, 0.5)
         return x_
 
     def dec(self, x, scope, decode_dim, reuse=False):
