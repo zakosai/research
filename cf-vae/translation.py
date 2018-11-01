de6803cb1964090a3ba2907304747352ff2b2aba
zakosai <linh.nguyen.1992@gmail.com>
2018-11-01 18:48:55 +0900

    translation

--- cf-vae/translation.py
+++ cf-vae/translation.py
@@ -398,7 +398,7 @@ def main():
                     model.x_B: x_B}
 
             if i <100:
-                _, loss_vae = sess.run([model.train_op_VAE, model.loss_VAE], feed_dict=feed)
+                _, loss_vae = sess.run([model.train_op_VAE_A, model.loss_VAE], feed_dict=feed)
                 _, loss_vae = sess.run([model.train_op_VAE_B, model.loss_VAE], feed_dict=feed)
                 loss_gen = loss_dis = loss_cc = 0
             # elif i>=50 and i < 100:
