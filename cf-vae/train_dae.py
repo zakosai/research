fbf8c32a2ae13576eda2643674aa26e1bd0daf5e
zakosai <linh.nguyen.1992@gmail.com>
2018-11-13 20:14:03 +0900

    clothing

--- cf-vae/train_dae.py
+++ cf-vae/train_dae.py
@@ -48,8 +48,10 @@ test_X = data[~idx]
 # test_img = images[~idx]
 
 # model = vanilla_vae(input_dim=args.user_dim, encoding_dims=[100], z_dim=zdim, decoding_dims=[100, args.user_dim], loss='cross_entropy', ckpt_folder=ckpt)
+dim = train_X.shape[1]
+
 if args.type == "text":
-    model = vanilla_vae(input_dim=8000, encoding_dims=[400, 200], z_dim=zdim, decoding_dims=[200, 400, 8000],
+    model = vanilla_vae(input_dim=dim, encoding_dims=[400, 200], z_dim=zdim, decoding_dims=[200, 400, dim],
                         loss='cross_entropy', ckpt_folder=ckpt)
 # As there will be an additional layer from 100 to 50 in the encoder. in decoder, we also take this layer
                     # lr=0.01, batch_size=128, print_step=50)
