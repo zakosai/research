230c61335fe8f2d2f3a99701d84f447ec9f64b9b
zakosai <linh.nguyen.1992@gmail.com>
2018-11-20 14:55:47 +0900

    multi-vae-cml

---
+++ cf-vae/multi-VAE_CML.py
@@ -0,0 +1,301 @@
+import tensorflow as tf
+from tensorflow.contrib.layers import fully_connected, flatten, batch_norm
+from tensorflow import sigmoid
+import tensorflow.keras.backend as K
+from tensorflow.contrib.framework import argsort
+import numpy as np
+import os
+import argparse
+
+
+class Translation:
+    def __init__(self, batch_size, dim, encode_dim, decode_dim, z_dim, eps=1e-10,
+                 lambda_0=10, lambda_1=0.1, lambda_2=100,
+                 lambda_3=0.1,
+                 lambda_4=100, learning_rate=1e-4):
+        self.batch_size = batch_size
+        self.dim = dim
+        self.encode_dim = encode_dim
+        self.decode_dim = decode_dim
+        self.z_dim = z_dim
+        self.eps = eps
+        self.lambda_0 = lambda_0
+        self.lambda_1 = lambda_1
+        self.lambda_2 = lambda_2
+        self.lambda_3 = lambda_3
+        self.lambda_4 = lambda_4
+        self.learning_rate = learning_rate
+        self.active_function = tf.nn.tanh
+        # self.z_A = z_A
+        # self.z_B = z_B
+        self.train = True
+        self.regularizer = tf.contrib.layers.l2_regularizer(scale=0.1)
+
+
+    def enc(self, x, scope, encode_dim, reuse=False):
+        x_ = x
+
+        # if self.train:
+        #     x_ = tf.nn.dropout(x_, 0.3)
+        with tf.variable_scope(scope, reuse=reuse):
+            for i in range(len(encode_dim)):
+                x_ = fully_connected(x_, encode_dim[i], self.active_function, scope="enc_%d"%i,
+                                     weights_regularizer=self.regularizer)
+                x_ = batch_norm(x_, decay=0.995)
+        return x_
+
+    def dec(self, x, scope, decode_dim, reuse=False):
+        x_ = x
+        # if self.train:
+        #     x_ = tf.nn.dropout(x_, 0.3)
+        with tf.variable_scope(scope, reuse=reuse):
+            for i in range(len(decode_dim)):
+                x_ = fully_connected(x_, decode_dim[i], self.active_function, scope="dec_%d" % i,
+                                     weights_regularizer=self.regularizer)
+        return x_
+
+    def gen_z(self, h, scope, reuse=False):
+        with tf.variable_scope(scope, reuse=reuse):
+            z_mu = fully_connected(h, self.z_dim, self.active_function, scope="z_mu")
+            z_sigma = fully_connected(h, self.z_dim, self.active_function, scope="z_sigma")
+            e = tf.random_normal(tf.shape(z_mu))
+            z = z_mu + tf.sqrt(tf.maximum(tf.exp(z_sigma), self.eps)) * e
+        return z, z_mu, z_sigma
+
+    def encode(self, x, dim):
+        h = self.enc(x, "encode", dim)
+        z, z_mu, z_sigma = self.gen_z(h, "VAE")
+        return z, z_mu, z_sigma
+
+    def decode(self, x, dim):
+        y = self.dec(x, "decode", dim)
+        return y
+
+    def loss_kl(self, mu, sigma):
+        return 0.5 * tf.reduce_mean(tf.reduce_sum(tf.square(mu) + tf.exp(sigma) - sigma - 1, 1))
+
+    def loss_reconstruct(self, x, x_recon):
+        log_softmax_var = tf.nn.log_softmax(x_recon)
+
+        neg_ll = -tf.reduce_mean(tf.reduce_sum(
+            log_softmax_var * x,
+            axis=-1))
+        # return tf.reduce_mean(tf.abs(x - x_recon))
+        return neg_ll
+
+    def build_model(self):
+        self.x = tf.placeholder(tf.float32, [None, self.dim], name='input')
+
+        x = self.x
+
+        # VAE for domain A
+        z, z_mu, z_sigma = self.encode(x, self.encode_dim)
+        x_recon = self.decode(z, self.decode_dim)
+        self.x_recon = x_recon
+
+        # Loss VAE
+        self.loss = self.lambda_1 * self.loss_kl(z_mu, z_sigma) + self.lambda_2 * self.loss_reconstruct(x,
+                                                                                                        x_recon) + \
+                    tf.losses.get_regularization_loss()
+
+        self.train_op = tf.train.AdamOptimizer(self.learning_rate).minimize(self.loss)
+
+
+def create_dataset(dataset="Tool", type=1, num_p = 7780):
+    dense_train = read_data("data2/%s/cf-train-%dp-users.dat"%(dataset, type))
+    train = one_hot_vector(dense_train, num_p)
+
+    dense_test = read_data("data2/%s/cf-test-%dp-users.dat"%(dataset, type))
+
+
+    return train, dense_train, dense_test
+
+def read_data(filename):
+    f = list(open(filename).readlines())
+    f = [i.split(" ") for i in f]
+    f = [[int(j) for j in i] for i in f]
+    f = [i[1:] for i in f]
+    return f
+
+def read_data2(filename):
+    data = list(open(filename).readlines())
+    data = data[1:]
+    n_data = len(data)
+    print(len(data))
+    data = [d.strip() for d in data]
+    data = [d.split(", ") for d in data]
+    data = [d[:3] for d in data]
+    data = np.array(data).reshape(n_data, 3).astype(np.int32)
+    return data
+
+def one_hot_vector(A, num_product):
+    one_hot_A = np.zeros((len(A), num_product))
+
+    for i, row in enumerate(A):
+        for j in row:
+            if j!= num_product:
+                one_hot_A[i,j] = 1
+    return one_hot_A
+
+def one_hot_vector2(A, num_product):
+    one_hot = np.zeros((6557, num_product))
+    for i in A:
+        one_hot[i[0], i[1]] = i[2]
+    return one_hot
+
+def calc_recall(pred, train, test, k=10, type=None):
+    pred_ab = np.argsort(-pred)
+    recall = []
+    ndcg = []
+    for i in range(len(pred_ab)):
+        p = pred_ab[i, :k+len(train[i])]
+        for u in train[i]:
+            if u in p:
+                p.remove(u)
+        p = p[:k]
+        hits = set(test[i]) & set(p)
+
+        #recall
+        recall_val = float(len(hits)) / len(test[i])
+        recall.append(recall_val)
+
+        #ncdg
+        score = []
+        for j in range(k):
+            if p[j] in hits:
+                score.append(1)
+            else:
+                score.append(0)
+        actual = dcg_score(score, pred[i, p], k)
+        best = dcg_score(score, score, k)
+        if best == 0:
+            ndcg.append(0)
+        else:
+            ndcg.append(float(actual) / best)
+
+        print("k= %d, recall %s: %f, ndcg: %f"%(k, type, np.mean(recall), np.mean(ndcg)))
+
+
+    return np.mean(np.array(recall))
+
+def dcg_score(y_true, y_score, k=50):
+    """Discounted cumulative gain (DCG) at rank K.
+
+    Parameters
+    ----------
+    y_true : array, shape = [n_samples]
+        Ground truth (true relevance labels).
+    y_score : array, shape = [n_samples, n_classes]
+        Predicted scores.
+    k : int
+        Rank.
+
+    Returns
+    -------
+    score : float
+    """
+    order = np.argsort(y_score)[::-1]
+    y_true = np.take(y_true, order[:k])
+
+    gain = 2 ** y_true - 1
+
+    discounts = np.log2(np.arange(len(y_true)) + 2)
+    return np.sum(gain / discounts)
+
+def calc_rmse(pred, test):
+    idx = np.where(test != 0)
+    pred = pred[idx]
+    test = test[idx]
+    return np.sqrt(np.mean((test-pred)**2))
+
+def main():
+    iter = 3000
+    batch_size= 500
+    args = parser.parse_args()
+    dataset = args.data
+    type = args.type
+    num_p = args.num_p
+    checkpoint_dir = "%s/%d/" % (dataset, type)
+    train, dense_train, dense_test = create_dataset(dataset, type, num_p)
+    num_u = len(dense_train)
+
+    encoding_dim = [600, 200]
+    decoding_dim = [200, 600, num_p]
+
+
+
+    z_dim = 50
+
+
+
+
+
+    # dense_A_test = np.array(dense_A)[test_A]
+    # dense_B_test = np.array(dense_B)[test_B]
+    # test_A = [t - train_size - val_size for t in test_A]
+    # test_B = [t - train_size - val_size for t in test_B]
+
+    model = Translation(batch_size, num_p, encoding_dim, decoding_dim, z_dim)
+    model.build_model()
+
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
+    saver = tf.train.Saver(max_to_keep=20)
+    max_recall = 0
+    dense_val = dense_test[:100]
+    user_val = train[:100]
+
+    for i in range(1, iter):
+        shuffle_idx = np.random.permutation(num_u)
+        train_cost = 0
+        for j in range(int(num_u/batch_size)):
+            list_idx = shuffle_idx[j*batch_size:(j+1)*batch_size]
+            x = num_u[list_idx]
+
+            feed = {model.x: x}
+
+            _, loss = sess.run([model.train_op, model.loss], feed_dict=feed)
+
+
+        # print("Loss last batch: loss gen %f, loss dis %f, loss vae %f, loss gan %f, loss cc %f"%(loss_gen, loss_dis,
+        #                                                                         loss_vae, loss_gan, loss_cc))
+
+        # Validation Process
+        if i%10 == 0:
+            model.train = False
+            loss_val, y_val = sess.run([model.loss, model.x_recon],
+                                              feed_dict={model.x:user_val})
+
+            print(len(y_val[0]))
+            recall = calc_recall(y_val, dense_test[:100], dense_val)
+            print("Loss val: %f, recall %f" % (loss_val, recall))
+            if recall > max_recall:
+                max_recall = recall
+                saver.save(sess, os.path.join(checkpoint_dir, 'multi-VAE-model'), i)
+                loss_test, y= sess.run([model.loss, model.x_recon], feed_dict={model.x: train})
+                print("Loss test: %f" % (loss_test))
+
+                # y_ab = y_ab[test_B]
+                # y_ba = y_ba[test_A]
+                calc_recall(y, dense_train, dense_test)
+            model.train = True
+        if i%100 == 0:
+            model.learning_rate /= 2
+            print("decrease lr to %f"%model.learning_rate)
+
+
+    print(max_recall)
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('--data',  type=str, default="Tool",
+                   help='dataset name')
+parser.add_argument('--type',  type=str, default='1',
+                   help='1p or 8p')
+parser.add_argument('--num_p', type=int, default=7780, help='number of product')
+
+
+if __name__ == '__main__':
+    main()
+
+
+
