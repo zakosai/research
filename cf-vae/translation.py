95eeb200c129735a5b1114de37d48b8b12d14a58
zakosai <linh.nguyen.1992@gmail.com>
2018-11-22 19:45:37 +0900

    no GAN

--- cf-vae/translation.py
+++ cf-vae/translation.py
@@ -239,7 +239,7 @@ class Translation:
         # self.loss_gen =  loss_CC_A + loss_CC_B + 0.1 * tf.losses.get_regularization_loss() +\
         #                 self.loss_generator(y_AB) + self.loss_generator(y_ABA) + self.loss_generator(y_BAB) +\
         #                 self.loss_generator(y_BA) + self.loss_reconstruct(x_A, y_BA) + self.loss_reconstruct(x_B, y_AB)
-        self.loss_gen = self.loss_VAE + 0.1 * tf.losses.get_regularization_loss() + self.loss_CC
+        self.loss_gen = self.loss_VAE + 0.1 * tf.losses.get_regularization_loss() 
         loss_gen_A = loss_VAE_A + loss_CC_A + tf.losses.get_regularization_loss()
         loss_gen_B = loss_VAE_B + loss_CC_B + tf.losses.get_regularization_loss()
 
@@ -417,11 +417,11 @@ def main():
 
     print(k)
 
-    encoding_dim_A = [600]
-    encoding_dim_B = [600]
-    share_dim = [200]
-    decoding_dim_A = [600, num_A]
-    decoding_dim_B = [600, num_B]
+    encoding_dim_A = [600, 200]
+    encoding_dim_B = [600, 200]
+    share_dim = []
+    decoding_dim_A = [200, 600, num_A]
+    decoding_dim_B = [200, 600, num_B]
 
 
     assert len(user_A) == len(user_B)
