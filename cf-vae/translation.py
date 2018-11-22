33f4f48007716e02e859168f0831ae92bfbca530
zakosai <linh.nguyen.1992@gmail.com>
2018-11-22 19:53:29 +0900

    no GAN

--- cf-vae/translation.py
+++ cf-vae/translation.py
@@ -239,7 +239,9 @@ class Translation:
         # self.loss_gen =  loss_CC_A + loss_CC_B + 0.1 * tf.losses.get_regularization_loss() +\
         #                 self.loss_generator(y_AB) + self.loss_generator(y_ABA) + self.loss_generator(y_BAB) +\
         #                 self.loss_generator(y_BA) + self.loss_reconstruct(x_A, y_BA) + self.loss_reconstruct(x_B, y_AB)
-        self.loss_gen = self.loss_VAE + 0.1 * tf.losses.get_regularization_loss()
+        self.loss_gen = self.loss_VAE + 0.1 * tf.losses.get_regularization_loss() + self.loss_CC + \
+                        self.loss_generator(y_AB) + self.loss_generator(y_ABA) + self.loss_generator(y_BAB) + \
+                      self.loss_generator(y_BA) + self.loss_reconstruct(x_A, y_BA) + self.loss_reconstruct(x_B, y_AB)
         loss_gen_A = loss_VAE_A + loss_CC_A + tf.losses.get_regularization_loss()
         loss_gen_B = loss_VAE_B + loss_CC_B + tf.losses.get_regularization_loss()
 
@@ -417,11 +419,11 @@ def main():
 
     print(k)
 
-    encoding_dim_A = [600, 200]
-    encoding_dim_B = [600, 200]
-    share_dim = []
-    decoding_dim_A = [200, 600, num_A]
-    decoding_dim_B = [200, 600, num_B]
+    encoding_dim_A = [600]
+    encoding_dim_B = [600]
+    share_dim = [200]
+    decoding_dim_A = [600, num_A]
+    decoding_dim_B = [600, num_B]
 
 
     assert len(user_A) == len(user_B)
@@ -476,7 +478,7 @@ def main():
             feed = {model.x_A: x_A,
                     model.x_B: x_B}
 
-            if i <0:
+            if i <20:
                 _, loss_vae = sess.run([model.train_op_VAE_A, model.loss_VAE], feed_dict=feed)
                 _, loss_vae = sess.run([model.train_op_VAE_B, model.loss_VAE], feed_dict=feed)
                 loss_gen = loss_dis = loss_cc = 0
@@ -488,10 +490,10 @@ def main():
                 _, loss_gen, loss_vae, loss_cc = sess.run([model.train_op_gen, model.loss_gen, model.loss_VAE,
                                                         model.loss_CC], feed_dict=feed)
 
-                # sess.run([model.train_op_dis_A],feed_dict=feed)
-                # # _, loss_gen, loss_vae, loss_cc = sess.run([model.train_op_gen_B, model.loss_gen, model.loss_VAE,
-                # #                                            model.loss_CC], feed_dict=feed)
-                # sess.run([model.train_op_dis_B], feed_dict=feed)
+                sess.run([model.train_op_dis_A],feed_dict=feed)
+                # _, loss_gen, loss_vae, loss_cc = sess.run([model.train_op_gen_B, model.loss_gen, model.loss_VAE,
+                #                                            model.loss_CC], feed_dict=feed)
+                sess.run([model.train_op_dis_B], feed_dict=feed)
                 loss_dis = 0
             # print(adv_AA, adv_AB)
             # _, loss_dis = sess.run([model.train_op_dis, model.loss_dis], feed_dict=feed)
