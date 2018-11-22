c0bf81bcebdf2bf9d31946670b4ab3a2ad5ab027
zakosai <linh.nguyen.1992@gmail.com>
2018-11-22 19:19:58 +0900

    no GAN

--- cf-vae/translation.py
+++ cf-vae/translation.py
@@ -31,7 +31,7 @@ class Translation:
         self.lambda_3 = lambda_3
         self.lambda_4 = lambda_4
         self.learning_rate = learning_rate
-        self.active_function = tf.nn.relu
+        self.active_function = tf.nn.tanh
         # self.z_A = z_A
         # self.z_B = z_B
         self.train = True
@@ -487,10 +487,10 @@ def main():
                 _, loss_gen, loss_vae, loss_cc = sess.run([model.train_op_gen, model.loss_gen, model.loss_VAE,
                                                         model.loss_CC], feed_dict=feed)
 
-                sess.run([model.train_op_dis_A],feed_dict=feed)
-                # _, loss_gen, loss_vae, loss_cc = sess.run([model.train_op_gen_B, model.loss_gen, model.loss_VAE,
-                #                                            model.loss_CC], feed_dict=feed)
-                sess.run([model.train_op_dis_B], feed_dict=feed)
+                # sess.run([model.train_op_dis_A],feed_dict=feed)
+                # # _, loss_gen, loss_vae, loss_cc = sess.run([model.train_op_gen_B, model.loss_gen, model.loss_VAE,
+                # #                                            model.loss_CC], feed_dict=feed)
+                # sess.run([model.train_op_dis_B], feed_dict=feed)
                 loss_dis = 0
             # print(adv_AA, adv_AB)
             # _, loss_dis = sess.run([model.train_op_dis, model.loss_dis], feed_dict=feed)
