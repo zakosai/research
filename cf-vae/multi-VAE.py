644735de01d31778ef33916d31ffe58bc134451e
zakosai <linh.nguyen.1992@gmail.com>
2018-11-27 17:50:56 +0900

    final

--- cf-vae/multi-VAE.py
+++ cf-vae/multi-VAE.py
@@ -216,7 +216,7 @@ def calc_rmse(pred, test):
     return np.sqrt(np.mean((test-pred)**2))
 
 def main():
-    iter = 3000
+    iter = 300
     batch_size= 500
     args = parser.parse_args()
     A = args.A
