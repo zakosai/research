d4bfbeaefa161d5927b92ac90ecf2f1741286d25
zakosai <linh.nguyen.1992@gmail.com>
2018-11-27 18:04:23 +0900

    final

--- cf-vae/multi-VAE.py
+++ cf-vae/multi-VAE.py
@@ -35,8 +35,8 @@ class Translation:
     def enc(self, x, scope, encode_dim, reuse=False):
         x_ = x
 
-        if self.train:
-            x_ = tf.nn.dropout(x_, 0.3)
+        # if self.train:
+        #     x_ = tf.nn.dropout(x_, 0.7)
         with tf.variable_scope(scope, reuse=reuse):
             for i in range(len(encode_dim)):
                 x_ = fully_connected(x_, encode_dim[i], self.active_function, scope="enc_%d"%i,
@@ -46,8 +46,8 @@ class Translation:
 
     def dec(self, x, scope, decode_dim, reuse=False):
         x_ = x
-        if self.train:
-            x_ = tf.nn.dropout(x_, 0.3)
+        # if self.train:
+        #     x_ = tf.nn.dropout(x_, 0.3)
         with tf.variable_scope(scope, reuse=reuse):
             for i in range(len(decode_dim)):
                 x_ = fully_connected(x_, decode_dim[i], self.active_function, scope="dec_%d" % i,
