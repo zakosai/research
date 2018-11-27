1e71651483bc4bb8988d0abef9dee82daf88b62d
zakosai <linh.nguyen.1992@gmail.com>
2018-11-27 15:00:01 +0900

    final

--- cf-vae/translation.py
+++ cf-vae/translation.py
@@ -240,12 +240,12 @@ class Translation:
         self.y_BA = y_BA
         self.y_AB = y_AB
 
-        # self.loss_gen =  loss_CC_A + loss_CC_B + 0.1 * tf.losses.get_regularization_loss() +\
-        #                 self.loss_generator(y_AB) + self.loss_generator(y_ABA) + self.loss_generator(y_BAB) +\
-        #                 self.loss_generator(y_BA) + self.loss_reconstruct(x_A, y_BA) + self.loss_reconstruct(x_B, y_AB)
-        self.loss_gen = self.loss_VAE + 0.1 * tf.losses.get_regularization_loss() + self.loss_CC +\
-                        self.loss_generator(y_AB) + self.loss_generator(y_ABA) + self.loss_generator(y_BAB) + \
-                      self.loss_generator(y_BA) + self.loss_reconstruct(x_A, y_BA) + self.loss_reconstruct(x_B, y_AB)
+        self.loss_gen =  loss_CC_A + loss_CC_B + 0.1 * tf.losses.get_regularization_loss() +\
+                        self.loss_generator(y_AB) + self.loss_generator(y_ABA) + self.loss_generator(y_BAB) +\
+                        self.loss_generator(y_BA) + self.loss_reconstruct(x_A, y_BA) + self.loss_reconstruct(x_B, y_AB)
+        # self.loss_gen = self.loss_VAE + 0.1 * tf.losses.get_regularization_loss() + self.loss_CC +\
+        #                 self.loss_generator(y_AB) + self.loss_generator(y_ABA) + self.loss_generator(y_BAB) + \
+        #               self.loss_generator(y_BA) + self.loss_reconstruct(x_A, y_BA) + self.loss_reconstruct(x_B, y_AB)
         loss_gen_A = loss_VAE_A + loss_CC_A + tf.losses.get_regularization_loss()
         loss_gen_B = loss_VAE_B + loss_CC_B + tf.losses.get_regularization_loss()
 
@@ -490,7 +490,7 @@ def main():
             #     _, loss_vae = sess.run([model.train_op_VAE_B, model.loss_VAE], feed_dict=feed)
             #     loss_gen = loss_dis = loss_cc = 0
             else:
-                # model.freeze = False
+                model.freeze = False
                 _, loss_gen, loss_vae, loss_cc = sess.run([model.train_op_gen, model.loss_gen, model.loss_VAE,
                                                         model.loss_CC], feed_dict=feed)
 
