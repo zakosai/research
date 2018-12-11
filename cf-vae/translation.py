024799e624c3ee9af2204aa4607877d70042450d
zakosai <linh.nguyen.1992@gmail.com>
2018-12-11 17:25:14 +0900

    check

--- cf-vae/translation.py
+++ cf-vae/translation.py
@@ -336,7 +336,7 @@ def calc_recall(pred, test, m=[100], type=None):
 
             #recall
             recall_val = float(len(hits)) / len(test[i])
-            if recall_val > 0.7:
+            if recall_val > 0.5:
                 print(i, p)
             recall.append(recall_val)
 
