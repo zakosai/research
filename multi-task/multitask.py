b9d83446431029fa17fa438e36aa6d51f6c3189b
zakosai <linh.nguyen.1992@gmail.com>
2018-12-14 22:13:34 +0900

    fix bug index

--- multi-task/multitask.py
+++ multi-task/multitask.py
@@ -370,8 +370,8 @@ def main():
 
     encode_user = encode_item = encode_tag = [200]
     share_dim = [100]
-    decode_user = [200, dataset['user_no']]
-    decode_item = [200, dataset['item_no']]
+    decode_user = [200, dataset['item_no']]
+    decode_item = [200, dataset['user_no']]
     decode_tag = [200, dataset['tag_no']]
 
     batch_size = 500
