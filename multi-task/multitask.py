9d65b7449988b4c59910e0cee5d43af89d2f2913
zakosai <linh.nguyen.1992@gmail.com>
2018-12-15 19:39:17 +0900

    lecky relu 0.7

--- multi-task/multitask.py
+++ multi-task/multitask.py
@@ -48,7 +48,7 @@ class MultiTask:
                                      weights_regularizer=self.regularizer, trainable=self.freeze)
                 # y = maxout(x_, encode_dim[i])
                 # x_ = tf.reshape(y, x_.shape)
-                x_ = tf.nn.leaky_relu(x_, alpha=0.5)
+                x_ = tf.nn.leaky_relu(x_, alpha=0.7)
                 # x_ = self.active_function(x_)
         return x_
 
@@ -60,7 +60,7 @@ class MultiTask:
             for i in range(len(layer)):
                 x_ = fully_connected(x_, layer[i], scope="dec_%d" % i,
                                      weights_regularizer=self.regularizer, trainable=self.freeze)
-                x_ = tf.nn.leaky_relu(x_, alpha=1)
+                x_ = tf.nn.leaky_relu(x_, alpha=0.7)
                 # x_ = self.active_function(x_)
             # x_ = fully_connected(x_, layer[-1], scope="last_dec",
             #                  weights_regularizer=self.regularizer, trainable=self.freeze)
@@ -74,7 +74,7 @@ class MultiTask:
             # x_ = tf.nn.dropout(x_, 0.7)
             for i in range(len(layer)):
                 x_ = fully_connected(x_, layer[i], scope="adv_%d" % i)
-                x_ = tf.nn.leaky_relu(x_, alpha=1)
+                x_ = tf.nn.leaky_relu(x_, alpha=0.7)
                 # x_ = self.active_function(x_)
             # x_ = fully_connected(x_, layer[-1], scope="adv_last")
         return x_
@@ -89,7 +89,7 @@ class MultiTask:
                                      weights_regularizer=self.regularizer)
                 # y = maxout(x_, dim[i])
                 # x_ = tf.reshape(y, x_.shape)
-                x_ = tf.nn.leaky_relu(x_, alpha=1)
+                x_ = tf.nn.leaky_relu(x_, alpha=0.7)
                 # x_ = self.active_function(x_)
 
         return x_
@@ -101,7 +101,7 @@ class MultiTask:
         with tf.variable_scope(scope, reuse=reuse):
             for i in range(len(layer)):
                 x_ = fully_connected(x_, layer[i], scope="%s_%d"%(scope, i), weights_regularizer=self.regularizer)
-                x_ = tf.nn.leaky_relu(x_, alpha=1)
+                x_ = tf.nn.leaky_relu(x_, alpha=0.7)
                 # x_ = self.active_function(x_)
         return x_
 
