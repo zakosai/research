d4cc6cd0ed256945333e0c24aa1f369822068a8f
zakosai <linh.nguyen.1992@gmail.com>
2018-12-15 13:10:07 +0900

    multi-VAE

--- multi-task/multi-VAE.py
+++ multi-task/multi-VAE.py
@@ -228,7 +228,7 @@ def calc_rmse(pred, test):
     return np.sqrt(np.mean((test-pred)**2))
 
 def main():
-    iter = 600
+    iter = 3000
     batch_size= 500
     args = parser.parse_args()
     dataset = args.data
