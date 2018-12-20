9aa259b4e2a471b2078dfa32b0d52e4e13e43472
zakosai <linh.nguyen.1992@gmail.com>
2018-12-20 17:56:00 +0900

    other dataset

--- multi-task/vae-unet.py
+++ multi-task/vae-unet.py
@@ -254,18 +254,18 @@ def main():
 
     z_dim = 50
     max_item = max(np.sum(dataset['user_onehot'], axis=1))
-    x_dim = z_dim * num_p
+    x_dim = z_dim * max_item
     user_item = np.zeros((num_u,x_dim))
-    # for i in range(num_u):
-    #     idx = np.where(dataset['user_onehot'][i] == 1)
-    #     u_c = content[idx]
-    #     u_c = u_c.flatten()
-    #     user_item[i, :len(u_c)] = u_c
-
     for i in range(num_u):
-        u_c = np.multiply(dataset['user_onehot'][i], content.T)
-        u_c = u_c.T.flatten()
-        user_item[i] = u_c
+        idx = np.where(dataset['user_onehot'][i] == 1)
+        u_c = content[idx]
+        u_c = u_c.flatten()
+        user_item[i, :len(u_c)] = u_c
+
+    # for i in range(num_u):
+    #     u_c = np.multiply(dataset['user_onehot'][i], content.T)
+    #     u_c = u_c.T.flatten()
+    #     user_item[i] = u_c
 
 
 
