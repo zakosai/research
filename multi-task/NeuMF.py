9bf141c3a8fd2e01e9ea773c06c14be0088bf63d
zakosai <linh.nguyen.1992@gmail.com>
2018-12-20 21:06:11 +0900

    neuMF

--- multi-task/NeuMF.py
+++ multi-task/NeuMF.py
@@ -279,9 +279,8 @@ if __name__ == '__main__':
                 user = [i]*num_items
                 item = list(range(num_items))
                 predict = model.predict([np.array(user), np.array(item)], batch_size=1000, verbose=0)
-                print(predict)
+                predict = [item for sublist in predict for item in sublist]
                 pred.append(predict)
-            print(pred)
             recall, _ = calc_recall(np.array(pred), dataset['user_item_test'].values(), [50])
             if recall > max_recall:
                 max_recall = recall
