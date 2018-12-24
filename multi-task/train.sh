9e1eba94c8a3a77dfacee4e9afbece07a1536eb8
zakosai <linh.nguyen.1992@gmail.com>
2018-12-24 13:37:11 +0900

    remove tag

--- multi-task/train.sh
+++ multi-task/train.sh
@@ -8,15 +8,17 @@ for d in $dir
 do
 #python vae.py  --ckpt_folder=experiment/$d/ --data_dir=data/$d/
 #python train_cvae_extend.py --ckpt_folder=experiment/$d/ --data_dir=data/$d/
-#python NeuMF.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
-#python FM.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
-#python MLP.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
-#python GMF.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
-#python NeuMF.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/ --mf_pretrain=experiment/$d/GMF.h5 \
-#--mlp_pretrain=experiment/$d/MLP.h5
-#python multi-VAE.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
 python vae_item.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
 python vae-unet.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
+python NeuMF.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
+python FM.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
+python MLP.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
+python GMF.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
+python NeuMF.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/ --mf_pretrain=experiment/$d/GMF.h5 \
+--mlp_pretrain=experiment/$d/MLP.h5
+python multi-VAE.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
+
+
 
 done
 
