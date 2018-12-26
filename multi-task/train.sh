d0b22fc45d8b6ccac4536729ddcd7d0afdaa2332
zakosai <linh.nguyen.1992@gmail.com>
2018-12-26 10:54:25 +0900

    outdoor

--- multi-task/train.sh
+++ multi-task/train.sh
@@ -1,24 +1,25 @@
 #!/usr/bin/env bash
 
+python vae.py  --ckpt_folder=experiment/outdoor/ --data_dir=data/outdoor/
+python train_cvae_extend.py --ckpt_folder=experiment/outdoor/ --data_dir=data/outdoor/
 
 
-
-dir='tool outdoor grocery'
-for d in $dir
-do
-#python vae.py  --ckpt_folder=experiment/$d/ --data_dir=data/$d/
-#python train_cvae_extend.py --ckpt_folder=experiment/$d/ --data_dir=data/$d/
-python vae_item.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
-python vae-unet.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
-python NeuMF.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
-python FM.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
-python MLP.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
-python GMF.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
-python NeuMF.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/ --mf_pretrain=experiment/$d/GMF.h5 \
---mlp_pretrain=experiment/$d/MLP.h5
-python multi-VAE.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
-
-
-
-done
+#dir='tool outdoor grocery'
+#for d in $dir
+#do
+##python vae.py  --ckpt_folder=experiment/$d/ --data_dir=data/$d/
+##python train_cvae_extend.py --ckpt_folder=experiment/$d/ --data_dir=data/$d/
+#python vae_item.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
+#python vae-unet.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
+#python NeuMF.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
+#python FM.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
+#python MLP.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
+#python GMF.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
+#python NeuMF.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/ --mf_pretrain=experiment/$d/GMF.h5 \
+#--mlp_pretrain=experiment/$d/MLP.h5
+#python multi-VAE.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
+#
+#
+#
+#done
 
