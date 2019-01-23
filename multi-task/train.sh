92a4f2261b437dab2c48c9848936c44adf7ef007
zakosai <linh.nguyen.1992@gmail.com>
2019-01-23 22:22:20 +0900

    tag

--- multi-task/train.sh
+++ multi-task/train.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
-python vae.py  --ckpt_folder=experiment/lastfm/ --data_dir=data/lastfm/
-python train_cvae_extend.py --ckpt_folder=experiment/lastfm/ --data_dir=data/lastfm/
+#python vae.py  --ckpt_folder=experiment/lastfm/ --data_dir=data/lastfm/
+#python train_cvae_extend.py --ckpt_folder=experiment/lastfm/ --data_dir=data/lastfm/
 
 
 
-dir='outdoor tool lastfm delicious'
+dir='lastfm'
 for d in $dir
 do
 #python vae.py  --ckpt_folder=experiment/$d/ --data_dir=data/$d/
 #python train_cvae_extend.py --ckpt_folder=experiment/$d/ --data_dir=data/$d/
-python vae_item.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
-python vae-unet.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
-#python NeuMF.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
-#python FM.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
-#python MLP.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
-#python GMF.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
-#python NeuMF.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/ --mf_pretrain=experiment/$d/GMF.h5 \
-#--mlp_pretrain=experiment/$d/MLP.h5
+#python vae_item.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
+#python vae-unet.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
+python NeuMF.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
+python FM.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
+python MLP.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
+python GMF.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
+python NeuMF.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/ --mf_pretrain=experiment/$d/GMF.h5 \
+--mlp_pretrain=experiment/$d/MLP.h5
 #python multi-VAE.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
 ##
 ##
