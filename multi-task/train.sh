1712964972148ec0fb97d2450ea5a77fe4f90fd7
zakosai <linh.nguyen.1992@gmail.com>
2019-01-25 21:37:54 +0900

    new delicious

--- multi-task/train.sh
+++ multi-task/train.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-python vae_item.py --data=data/lastfm/dataset.pkl --ckpt=experiment/lastfm/
-python vae-unet.py --data=data/lastfm/dataset.pkl --ckpt=experiment/lastfm/
-python vae-unet.py --data=data/delicious/dataset.pkl --ckpt=experiment/delicious/
+#python vae_item.py --data=data/lastfm/dataset.pkl --ckpt=experiment/lastfm/
+#python vae-unet.py --data=data/lastfm/dataset.pkl --ckpt=experiment/lastfm/
+#python vae-unet.py --data=data/delicious/dataset.pkl --ckpt=experiment/delicious/
 
 
 #python vae.py  --ckpt_folder=experiment/delicious/ --data_dir=data/delicious/
@@ -9,24 +9,26 @@ python vae-unet.py --data=data/delicious/dataset.pkl --ckpt=experiment/delicious
 
 
 
-#dir='lastfm'
-#for d in $dir
-#do
-##python vae.py  --ckpt_folder=experiment/$d/ --data_dir=data/$d/
-##python train_cvae_extend.py --ckpt_folder=experiment/$d/ --data_dir=data/$d/
-##python vae_item.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
-##python vae-unet.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
-#python NeuMF.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
-#python FM.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
-#python MLP.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
-#python GMF.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
-#python NeuMF.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/ --mf_pretrain=experiment/$d/GMF.h5 \
-#--mlp_pretrain=experiment/$d/MLP.h5
-##python multi-VAE.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
-###
-###
-###
-#done
+dir='delicious'
+for d in $dir
+do
+python dae.py  --ckpt_folder=experiment/$d/ --data_dir=data/$d/
+python train_cf_dae.py --ckpt_folder=experiment/$d/ --data_dir=data/$d/ --gridsearch=1
+python vae.py  --ckpt_folder=experiment/$d/ --data_dir=data/$d/
+python train_cvae_extend.py --ckpt_folder=experiment/$d/ --data_dir=data/$d/
+python vae_item.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
+python vae-unet.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
+python NeuMF.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
+python FM.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
+python MLP.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
+python GMF.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
+python NeuMF.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/ --mf_pretrain=experiment/$d/GMF.h5 \
+--mlp_pretrain=experiment/$d/MLP.h5
+python multi-VAE.py --data=data/$d/dataset.pkl --ckpt=experiment/$d/
+##
+##
+##
+done
 #python vae_item.py --data=data/grocery/dataset.pkl --ckpt=experiment/grocery/
 #python vae-unet.py --data=data/grocery/dataset.pkl --ckpt=experiment/grocery/
 #python vae.py  --ckpt_folder=experiment/outdoor/ --data_dir=data/outdoor/
