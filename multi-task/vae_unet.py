ff9bd42458bd41ad058d5cda6b87f44ddafdbcc2
zakosai <linh.nguyen.1992@gmail.com>
2019-01-27 13:54:07 +0900

    re train

--- multi-task/vae_unet.py
+++ multi-task/vae_unet.py
@@ -12,7 +12,7 @@ from scipy.sparse import load_npz
 
 class Translation:
     def __init__(self, batch_size, x_dim, y_dim, encode_dim, decode_dim, z_dim, eps=1e-10,
-                 lambda_0=10, lambda_1=0.1, lambda_2=100,
+                 lambda_0=10, lambda_1=0.1, lambda_2=10,
                  lambda_3=0.1,
                  lambda_4=100, learning_rate=1e-4):
         self.batch_size = batch_size
