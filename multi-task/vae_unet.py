f63ea1da6d4c78150276246ba56d48e1f8e492db
zakosai <linh.nguyen.1992@gmail.com>
2019-01-30 18:35:15 +0900

    Implicit

--- multi-task/vae_unet.py
+++ multi-task/vae_unet.py
@@ -92,7 +92,7 @@ class Translation:
         #     axis=-1))
         # return neg_ll
         print(x.shape, x_recon.shape, log_softmax_var.shape)
-        return losses.sparse_categorical_crossentropy(x, log_softmax_var)
+        return losses.hinge(x, log_softmax_var)
 
         # return tf.reduce_mean(tf.abs(x - x_recon))
 
