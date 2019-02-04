a9bfa01e0c2932fec7b14bb9e27e592beef004f9
zakosai <linh.nguyen.1992@gmail.com>
2019-02-04 23:48:05 +0900

    retrain office

--- multi-task/train.sh
+++ multi-task/train.sh
@@ -33,7 +33,12 @@
 #python vae-unet.py --data=data/grocery/dataset.pkl --ckpt=experiment/grocery/
 #python vae.py  --ckpt_folder=experiment/outdoor/ --data_dir=data/outdoor/
 #python train_cvae_extend.py --ckpt_folder=experiment/outdoor/ --data_dir=data/outdoor/
+
+python NeuMF.py --data=data/ml-1m/dataset.pkl --ckpt=experiment/ml-1m/
+python FM.py --data=data/ml-1m/dataset.pkl --ckpt=experiment/ml-1m/
+python NeuMF.py --data=data/delicious/dataset.pkl --ckpt=experiment/delicious/
+python FM.py --data=data/delicious/dataset.pkl --ckpt=experiment/delicious/
 python NeuMF.py --data=data/lastfm/dataset_notag.pkl --ckpt=experiment/lastfm/
 python FM.py --data=data/lastfm/dataset_notag.pkl --ckpt=experiment/lastfm/
 python NeuMF.py --data=data/office/dataset.pkl --ckpt=experiment/office/
-python FM.py --data=data/office/dataset.pkl --ckpt=experiment/officeg/
+python FM.py --data=data/office/dataset.pkl --ckpt=experiment/office/
