ed5965f18556477e1dbc5703e5a916e49396d953
zakosai <linh.nguyen.1992@gmail.com>
2019-02-13 16:30:22 +0900

    fix dataset

--- review_based/cnn.py
+++ review_based/cnn.py
@@ -256,6 +256,7 @@ def main():
                              model.y_review_ids: y_review,
                              model.y_rating: y_rating}
                 p = sess.run(model.X, feed_dict=feed_dict)
+                print(p, y_rating)
                 if j == 0:
                     error = p - y_rating
                 else:
