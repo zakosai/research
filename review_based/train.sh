faa0825c6e57054095155769ad2f851336d366ce
zakosai <linh.nguyen.1992@gmail.com>
2019-02-25 11:21:44 +0900

    add clip

--- review_based/train.sh
+++ review_based/train.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 
-folder="Kindle CD Health TV Beauty Tool Toy Outdoor Video Clothing Baby Office Electronics Books"
-for f in $folder;
-do
-    mkdir data/$f
-    python dataset.py --data=../cf-vae/data/$f/reviews.json.gz --output_folder=data/$f/
-done
+#folder="Kindle CD Health TV Beauty Tool Toy Outdoor Video Clothing Baby Office Electronics Books"
+#for f in $folder;
+#do
+#    mkdir data/$f
+#    python dataset.py --data=../cf-vae/data/$f/reviews.json.gz --output_folder=data/$f/
+#done
 
 folder="Kitchen AmzVideo Instrument Music Automotive Pet App Phone Kindle CD Health TV Beauty Tool Toy Outdoor Video
 Clothing Baby Office Electronics Books"
