5b6fef541c20f5fc370d258d8f6f45a7aa711552
zakosai <linh.nguyen.1992@gmail.com>
2019-03-01 19:09:37 +0900

    description

--- review_based/dataset.py
+++ review_based/dataset.py
@@ -10,11 +10,10 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from keras.preprocessing.sequence import pad_sequences
 import numpy as np
 
-
 class Dataset(object):
     def __init__(self, data, cnn=False, max_sequence_length=1000, max_nb_words=20000, folder="data/",
                  embedding_dim=300):
-        f = open(data, "rb")
+        f = open(os.path.join(data, "dataset.pkl"), "rb")
         self.data = pickle.load(f)
         f.close()
         if cnn:
@@ -47,7 +46,7 @@ class Dataset(object):
         else:
             text = self.data['train'][2]
             tfidf = TfidfVectorizer(stop_words='english', max_features=20000)
-            tfidf.fit(text.astype('U'), self.data['train'][3])
+            self.y_review = tfidf.fit_transform(text.astype('U'), self.data['train'][3])
             text = []
             for u in range(self.data['user_no']):
                 user = self.data['train_user'][u]
@@ -61,6 +60,14 @@ class Dataset(object):
                 except:
                     text.append(' ')
             self.X_item = tfidf.transform(text).toarray()
+
+            self.user_onehot = np.zeros((self.data['user_no'], self.data['item_no']))
+            for user in self.data['train_user']:
+                d = self.data['train_user'][user]
+                for i, j in enumerate(d[0]):
+                    self.user_onehot[user, j] = d[2][i]
+            self.description = np.load(os.path.join(data, "description.npy"))
+            print(self.description.shape)
 
 
     def create_batch(self, idx, k=2, type='train'):
@@ -170,13 +177,56 @@ class Dataset(object):
         y_rating = np.array(data_b[3])
         X_user = self.X_user[data_b[0]]
         X_item = self.X_item[data_b[1]]
+        user_onehot = self.user_onehot[data_b[0]]
+        item_onehot = self.user_onehot.T[data_b[1]]
+        description = self.description[data_b[1], :]
+        # user_onehot = np.concatenate((X_user, user_onehot), axis=1)
+        # item_onehot = np.concatenate((X_item, item_onehot), axis=1)
+        if type=='train':
+            y_review = self.y_review[idx].toarray()
+        else:
+            y_review = data_b[2]
+        # X_user = np.concatenate((X_user, user_onehot), axis=1)
+        # X_item = np.concatenate((X_item, item_onehot), axis=1)
+
+        # Create X
+        # sequences_user = np.zeros((len(idx), self.data['item_no']))
+        # sequences_item = np.zeros((len(idx)))
+        # for i, d in data_b.iterrows():
+        #     user = self.data['train_user'][d[0]]
+        #     sequences_user.append(' '.join(user[1]))
+        #
+        #     try:
+        #         item = self.data['train_item'][d[1]]
+        #         sequences_item.append(' '.join(item[1]))
+        #     except:
+        #         sequences_item.append(' ')
+        # X_user = self.tfidf.transform(sequences_user).toarray()
+        # X_item = self.tfidf.transform(sequences_item).toarray()
+
+        return X_user, X_item, user_onehot, item_onehot, y_rating, y_review, description
+
+    def create_tfidf_neg(self, idx, k=2, type='train'):
+        data_b = self.data[type].iloc[idx]
+        y_rating = np.array(data_b[3])
+        X_user = self.X_user[data_b[0]]
+        X_item = self.X_item[data_b[1]]
 
         # Create X
         # sequences_user = []
         # sequences_item = []
-        # for i, d in data_b.iterrows():
-        #     user = self.data['train_user'][d[0]]
-        #     sequences_user.append(' '.join(user[1]))
+        neg_items = []
+        for i, d in data_b.iterrows():
+            user = self.data['train_user'][d[0]]
+            r = np.random.randint(self.data['item_no'])
+            while r in user[0]:
+                r = np.random.randint(self.data['item_no'])
+            neg_items.append(r)
+
+        y_rating = np.concatenate((y_rating, np.zeros(len(neg_items))))
+        X_user = np.concatenate((X_user, X_user), axis=0)
+        X_item = np.concatenate((X_item, self.X_item[neg_items]), axis=0)
+
         #
         #     try:
         #         item = self.data['train_item'][d[1]]
@@ -310,10 +360,28 @@ def amz_to_pkl(args):
     pickle.dump(dataset, f)
     print("Finish writing data to %s"%(os.path.join(folder, "dataset.pkl")))
 
+def description(args):
+    item = list(open(os.path.join("data", args.data, "item_id.txt")))
+    item = [i.strip() for i in item]
+
+    data = getDF(os.path.join("../cf-vae/data", args.data, "meta.json.gz"))
+    text = [0] * len(item)
+    count = 0
+    for _, i in data.iterrows():
+        try:
+            idx = item.index(i.asin)
+            text[idx] = str(i.description) + str(i.title)
+            count += 1
+        except:
+            continue
+
+    tfidf = TfidfVectorizer(stop_words='english', max_features=8000)
+    description = tfidf.fit_transform(text).toarray()
+    np.save(os.path.join("data", args.data, "description"), description)
+
 def main():
     args = parse_args()
-    amz_to_pkl(args)
-
+    description(args)
 
 if __name__ == '__main__':
     main()
--- review_based/train.sh
+++ review_based/train.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env bash
 
 
-#folder="Kindle CD Health TV Beauty Tool Toy Outdoor Video Clothing Baby Office Electronics Books"
-#for f in $folder;
-#do
-#    mkdir data/$f
-#    python dataset.py --data=../cf-vae/data/$f/reviews.json.gz --output_folder=data/$f/
-#done
-multi=(1.220 0.997 0.923 0.970 0.861 1.328 1.494 1.413 0.775 1.005 1.238 1.144 1.387 1.096 0.973 0.980 1.257 1.187 1.304 0.779 1.350)
-folder="Kitchen AmzVideo Instrument Music Automotive Pet App Phone Kindle CD Health TV Beauty Tool Toy Outdoor
-Video Clothing Baby Office Electronics"
-i=0
+folder="Kitchen AmzVideo Instrument Music Automotive Pet App Phone Kindle CD Health TV Beauty Tool Toy Outdoor Video
+Clothing Baby Office Electronics Books"
 for f in $folder;
 do
-
-    python tf_idf.py --data=data/$f/dataset.pkl --deep=True --output=data/$f/ --multi=${multi[($i)]}
-    i=$((i+1))
+    mkdir data/$f
+    python dataset.py --data=$f
 done
+#multi=(1.220 0.997 0.923 0.970 0.861 1.328 1.494 1.413 0.775 1.005 1.238 1.144 1.387 1.096 0.973 0.980 1.257 1.187 1.304 0.779 1.350)
+#folder="Kitchen AmzVideo Instrument Music Automotive Pet App Phone Kindle CD Health TV Beauty Tool Toy Outdoor
+#Video Clothing Baby Office Electronics"
+#i=0
+#for f in $folder;
+#do
+#
+#    python tf_idf.py --data=data/$f/dataset.pkl --deep=True --output=data/$f/ --multi=${multi[($i)]}
+#    i=$((i+1))
+#done
 
