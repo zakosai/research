d76580eac95e84b2b75e9182801c26fb8832a513
zakosai <linh.nguyen.1992@gmail.com>
2019-05-13 19:17:23 +0900

    change window size

--- seq2seq/seq2seq.py
+++ seq2seq/seq2seq.py
@@ -166,6 +166,7 @@ def main():
     model = Seq2seq()
     # model.p_dim = data.n_user
     model.w_size = data.w_size = args.w_size
+    print(model.w_size, data.w_size, args.w_size)
     model.p_dim = text.shape[1]+ data.n_user
     model.build_model()
 
