0114e2efa0117b242b2f63ff32a306c868c8bc4a
zakosai <linh.nguyen.1992@gmail.com>
2019-05-14 21:14:27 +0900

    retrain

--- seq2seq/train.sh
+++ seq2seq/train.sh
@@ -3,3 +3,5 @@
 python cf_vae.py --ckpt_folder=experiment/ml-1m/cvae_100 --data_dir=data/ml-1m --zdim=100
 source ../.env3/bin/activate
 python hybrid_seq2seq.py --data=ml-1m --num_p=3706
+python seq2seq.py --data=ml-1m --num_p=3706
+
