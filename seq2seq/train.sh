b7b78508340747f3afedc4c2e309ecef29accc86
zakosai <linh.nguyen.1992@gmail.com>
2019-05-17 17:57:32 +0900

    train outdoor, garden, ml-1m

--- seq2seq/train.sh
+++ seq2seq/train.sh
@@ -2,6 +2,21 @@
 
 #python cf_vae.py --ckpt_folder=experiment/ml-1m/cvae_100 --data_dir=data/ml-1m --zdim=100
 #source ../.env3/bin/activate
+
+python seq2seq.py --data=ml-1m --w_size=15 --bilstm=False --n_layers=1
+python seq2seq.py --data=ml-1m --w_size=15
+#python seq2seq.py --data=ml-1m --w_size=15 --cat=True
+python seq2seq.py --data=ml-1m --w_size=15 --cat=True --time=True
+#python seq2seq.py --data=ml-1m --w_size=15 --time=True
+
+python user_seq2seq.py --data=ml-1m --w_size=15 --cat=True
+python user_seq2seq.py --data=ml-1m --w_size=15 --cat=True --time=True
+python user_seq2seq.py --data=ml-1m --w_size=15 --time=True
+python user_seq2seq.py --data=ml-1m --w_size=15
+
+python hybrid_seq2seq.py --data=ml-1m --w_size=15
+python hybrid_seq2seq.py --data=ml-1m --w_size=15 --cat=True --time=True
+
 type="Garden Outdoor"
 for t in $type; do
     python seq2seq.py --data=$t --w_size=4 --bilstm=False --n_layers=1
@@ -19,17 +34,5 @@ for t in $type; do
     python hybrid_seq2seq.py --data=$t --w_size=4 --cat=True --time=True
 done
 
-python seq2seq.py --data=ml-1m --w_size=15 --bilstm=False --n_layers=1
-python seq2seq.py --data=ml-1m --w_size=15
-#python seq2seq.py --data=ml-1m --w_size=15 --cat=True
-python seq2seq.py --data=ml-1m --w_size=15 --cat=True --time=True
-#python seq2seq.py --data=ml-1m --w_size=15 --time=True
 
-python user_seq2seq.py --data=ml-1m --w_size=15 --cat=True
-python user_seq2seq.py --data=ml-1m --w_size=15 --cat=True --time=True
-python user_seq2seq.py --data=ml-1m --w_size=15 --time=True
-python user_seq2seq.py --data=ml-1m --w_size=15
-
-python hybrid_seq2seq.py --data=ml-1m --w_size=15
-python hybrid_seq2seq.py --data=ml-1m --w_size=15 --cat=True --time=True
 
