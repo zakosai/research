0eb7c7bd1a86ebc27dca892eb8e6563d1ff8e28d
zakosai <linh.nguyen.1992@gmail.com>
2019-05-28 15:45:07 +0900

    change active function

--- seq2seq/user_seq2seq.py
+++ seq2seq/user_seq2seq.py
@@ -113,6 +113,7 @@ class Seq2seq(object):
     def prediction(self, x, y, cat=None, y_cat=None, reuse=False):
         with tf.variable_scope("last_layer", reuse=reuse):
             out = layers.fully_connected(x, self.n_products)
+            out = tf.nn.leaky_relu(out, alpha=0.2)
             # loss = tf.reduce_mean(tf.nn.weighted_cross_entropy_with_logits(y, out, 100))
 
             if cat !=None:
