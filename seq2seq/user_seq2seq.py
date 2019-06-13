e6e07061216ceb35dbe32e7d8f39499dce352917
zakosai <linh.nguyen.1992@gmail.com>
2019-06-13 10:23:58 +0900

    change loss

--- seq2seq/user_seq2seq.py
+++ seq2seq/user_seq2seq.py
@@ -164,13 +164,13 @@ class Seq2seq(object):
         last_state = tf.concat([last_state, last_state_cat], axis=1)
 
         self.loss, self.predict = self.prediction(last_state, tf.reshape(self.y[:, -1, :], (-1, self.n_products)))
-        # self.loss *=10
-        # for i in range(self.w_size-1):
-        #     x = tf.reshape(outputs[:, i, :], (-1, self.n_hidden * 2**self.n_layers))
-        #     x = tf.concat([x, last_state_cat], axis = 1)
-        #     y = tf.reshape(self.y[:, i, :], (-1, self.n_products))
-        #     loss, _ = self.prediction(x, y, reuse=True)
-        #     self.loss += loss
+        self.loss *=10
+        for i in range(self.w_size-1):
+            x = tf.reshape(outputs[:, i, :], (-1, self.n_hidden/2))
+            x = tf.concat([x, last_state_cat], axis=1)
+            y = tf.reshape(self.y[:, i, :], (-1, self.n_products))
+            loss, _ = self.prediction(x, y, reuse=True)
+            self.loss += loss
 
         # self.loss = tf.reduce_mean(tf.nn.weighted_cross_entropy_with_logits(tf.reshape(self.y[:, -1, :], (-1, self.n_products)), self.predict, 100))
 
