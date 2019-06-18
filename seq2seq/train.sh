51a6fac58c24f1bc7781dc81745d9f2ecf86f8c4
zakosai <linh.nguyen.1992@gmail.com>
2019-06-18 12:04:35 +0900

    train all

--- seq2seq/train.sh
+++ seq2seq/train.sh
@@ -36,19 +36,19 @@
 
 #python preprocessing.py
 
-dataset="book"
+dataset="Automotive Baby Beauty book CD Grocery Kitchen Outdoor Tool Toy"
 wsize="10"
 for data in $dataset; do
-    mkdir experiment/$data
-    mkdir experiment/$data/cvae
+#    mkdir experiment/$data
+#    mkdir experiment/$data/cvae
 #    python seq2seq.py --data=$data/ratings --bilstm=False --n_layers=1
 #    python seq2seq.py --data=$data/ratings --cat=True --time=True
     for w in $wsize; do
         python user_seq2seq.py --data=$data --time=True --w_size=$w
     done
-    python vae.py --data_dir=data/$data --ckpt_folder=experiment/$data/cvae
-    python vae.py --data_dir=data/$data --ckpt_folder=experiment/$data/cvae --type=user
-    python cvae_user.py --data_dir=data/$data --ckpt_folder=experiment/$data/cvae
+#    python vae.py --data_dir=data/$data --ckpt_folder=experiment/$data/cvae
+#    python vae.py --data_dir=data/$data --ckpt_folder=experiment/$data/cvae --type=user
+#    python cvae_user.py --data_dir=data/$data --ckpt_folder=experiment/$data/cvae
 #    python user_seq2seq.py --data=$data --cat=True --time=True --w_size=15 --batch_size=200
 done
 
