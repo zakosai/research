33e4c73975b49e64375ded8129cf3022f91327ee
zakosai <linh.nguyen.1992@gmail.com>
2019-06-24 15:33:20 +0900

    retrain

--- d2dseq/train.sh
+++ d2dseq/train.sh
@@ -1,7 +1,10 @@
-#!/usr/bin/env bash
-dataset="Health_Grocery Health_Kitchen Health_Garden Grocery_Garden Grocery_Kitchen Kitchen_Garden"
+##!/usr/bin/env bash
+dataset="Health_Kitchen Health_Garden Grocery_Garden Grocery_Kitchen Kitchen_Garden"
 for data in $dataset; do
     echo $data
-    mkdir experiment/$data
     python d2d_seq.py --data=$data --batch_size=100 --iter=50
+    python d2d_seq.py --data=$data --batch_size=100 --iter=50 --swap=True
 done
+
+
+
