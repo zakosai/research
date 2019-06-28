77918f7a527b91cb1212d085da6b36bb5799f0f7
zakosai <linh.nguyen.1992@gmail.com>
2019-06-28 14:29:20 +0900

    add multi-vae into user_seq

--- seq2seq/user_seq2seq.py
+++ seq2seq/user_seq2seq.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from tensorflow.contrib import rnn, layers
+from tensorflow.contrib.layers import fully_connected, flatten, batch_norm
 import numpy as np
 import argparse
 from dataset import Dataset, calc_recall
@@ -69,6 +70,55 @@ class Seq2seq(object):
 
         return outputs, last_state
 
+    def enc(self, x, scope, encode_dim, reuse=False):
+        x_ = x
+
+        # if self.train:
+        #     x_ = tf.nn.dropout(x_, 0.3)
+        with tf.variable_scope(scope, reuse=reuse):
+            for i in range(len(encode_dim)):
+                x_ = fully_connected(x_, encode_dim[i], self.active_function, scope="enc_%d"%i,
+                                     weights_regularizer=self.regularizer)
+        return x_
+
+    def dec(self, x, scope, decode_dim, reuse=False):
+        x_ = x
+        # if self.train:
+        #     x_ = tf.nn.dropout(x_, 0.3)
+        with tf.variable_scope(scope, reuse=reuse):
+            for i in range(len(decode_dim)):
+                x_ = fully_connected(x_, decode_dim[i], self.active_function, scope="dec_%d" % i,
+                                     weights_regularizer=self.regularizer)
+        return x_
+
+    def gen_z(self, h, scope, reuse=False):
+        with tf.variable_scope(scope, reuse=reuse):
+            z_mu = fully_connected(h, self.z_dim, self.active_function, scope="z_mu")
+            z_sigma = fully_connected(h, self.z_dim, self.active_function, scope="z_sigma")
+            e = tf.random_normal(tf.shape(z_mu))
+            z = z_mu + tf.sqrt(tf.maximum(tf.exp(z_sigma), self.eps)) * e
+        return z, z_mu, z_sigma
+
+    def encode(self, x, dim):
+        h = self.enc(x, "encode", dim)
+        z, z_mu, z_sigma = self.gen_z(h, "VAE")
+        return z, z_mu, z_sigma
+
+    def decode(self, x, dim):
+        y = self.dec(x, "decode", dim)
+        return y
+
+    def loss_kl(self, mu, sigma):
+        return 0.5 * tf.reduce_mean(tf.reduce_sum(tf.square(mu) + tf.exp(sigma) - sigma - 1, 1))
+
+    def loss_reconstruct(self, x, x_recon):
+        log_softmax_var = tf.nn.log_softmax(x_recon)
+
+        neg_ll = -tf.reduce_mean(tf.reduce_sum(
+            log_softmax_var * x,
+            axis=-1))
+        # return tf.reduce_mean(tf.abs(x - x_recon))
+        return neg_ll
 
 
     def attention(self, inputs):
@@ -163,11 +213,16 @@ class Seq2seq(object):
         # out_cat, _ = self.encoder_BiLSTM(self.X_cat, "cat2", self.n_hidden*2)
         # print(out_cat.shape)
         # last_state_cat = tf.reshape(out_cat[:, -1, :], (-1, self.n_hidden*4))
-        last_state_cat = self.mlp(self.X_cat)
-        last_state_cat = tf.reshape(last_state_cat, (tf.shape(self.X)[0], self.layers[-1]))
-        last_state = tf.concat([last_state, last_state_cat], axis=1)
+        z, z_mu, z_sigma = self.encode(self.X_cat, [600, 200])
+        x_recon = self.decode(z, [600])
+        last_state = tf.concat([last_state, z], axis=1)
+        # last_state_cat = self.mlp(self.X_cat)
+        # last_state_cat = tf.reshape(last_state_cat, (tf.shape(self.X)[0], self.layers[-1]))
+        # last_state = tf.concat([last_state, last_state_cat], axis=1)
 
         self.loss, self.predict = self.prediction(last_state, tf.reshape(self.y[:, -1, :], (-1, self.n_products)))
+        self.loss += 0.1 * self.loss_kl(z_mu, z_sigma) + 100 * self.loss_reconstruct(x,x_recon) + \
+                    0.1* tf.losses.get_regularization_loss()
         # self.loss *=10
         # for i in range(self.w_size-1):
         #     x = tf.reshape(outputs[:, i, :], (-1, self.n_hidden/2))
