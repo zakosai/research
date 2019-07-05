3ed45396384115fe5f847eef6c9585703e804393
zakosai <linh.nguyen.1992@gmail.com>
2019-07-06 08:07:05 +0900

    retrain recall 20

--- seq2seq/train.sh
+++ seq2seq/train.sh
@@ -5,8 +5,8 @@ for data in $dataset; do
     for w in $wsize; do
         python seq2seq.py --data=$data --bilstm=False --n_layers=1 --w_size=$w
         python user_seq2seq.py --data=$data --cat=True --time=True --w_size=$w
-        python multi-VAE.py --data=$data
     done
+    python multi-VAE.py --data=$data
 done
     python cvae_user.py --data_dir=data/Phone --ckpt_folder=experiment/Phone/cvae
 
