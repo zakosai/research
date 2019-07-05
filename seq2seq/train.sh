f2cb2198370e944de24fd00b284585223c4dfe34
zakosai <linh.nguyen.1992@gmail.com>
2019-07-06 08:06:02 +0900

    retrain recall 20

--- seq2seq/train.sh
+++ seq2seq/train.sh
@@ -1,19 +1,12 @@
 #!/usr/bin/env bash
-dataset="Garden Automotive Beauty Grocery Outdoor Tool Toy Office"
+dataset="Garden Automotive Beauty Grocery Outdoor Tool Toy Office Pet "
 wsize="5 10"
 for data in $dataset; do
-    mkdir experiment/$data
-    mkdir experiment/$data/cvae
-    python preprocessing.py --data=$data
-#    python seq2seq.py --data=$data/ratings --bilstm=False --n_layers=1
-#    python seq2seq.py --data=$data/ratings --cat=True --time=True
     for w in $wsize; do
         python seq2seq.py --data=$data --bilstm=False --n_layers=1 --w_size=$w
         python user_seq2seq.py --data=$data --cat=True --time=True --w_size=$w
     done
-    python vae.py --data_dir=data/$data --ckpt_folder=experiment/$data/cvae
-    python vae.py --data_dir=data/$data --ckpt_folder=experiment/$data/cvae --type=user
-    python cvae_user.py --data_dir=data/$data --ckpt_folder=experiment/$data/cvae
-#    python user_seq2seq.py --data=$data --cat=True --time=True --w_size=15 --batch_size=200
 done
+    python cvae_user.py --data_dir=data/Phone --ckpt_folder=experiment/Phone/cvae
+
 
