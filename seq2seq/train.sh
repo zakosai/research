9d89c9b017945dd7e9fb24ee0e18ee062846e462
zakosai <linh.nguyen.1992@gmail.com>
2019-07-09 13:45:13 +0900

    retrain

--- seq2seq/train.sh
+++ seq2seq/train.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/env bash
-dataset="Pet Clothing Phone"
+dataset="Pet Phone"
 for data in $dataset; do
-    python user_seq2seq.py --data=$data --cat=True --time=True --w_size=4
+    python user_seq2seq.py --data=$data --cat=True --time=True --w_size=7 --batch_size=500
 done
 
-dataset="TV CD Kitchen Kindle Health App Electronics Books"
+dataset="TV CD Kitchen Kindle Health Electronics"
 wsize="5 10"
 for data in $dataset; do
-    mkdir experiment/$data
-    mkdir experiment/$data/cvae
-    python preprocessing.py --data=$data
     for w in $wsize; do
         python seq2seq.py --data=$data --bilstm=False --n_layers=1 --w_size=$w
-        python user_seq2seq.py --data=$data --cat=True --time=True --w_size=$w
+        python user_seq2seq.py --data=$data --cat=True --time=True --w_size=$w --batch_size=500
     done
     python multi-VAE.py --data=$data
+    python vae.py --data_dir=data/$data --ckpt_folder=experiment/$data/cvae
+    python vae.py --data_dir=data/$data --ckpt_folder=experiment/$data/cvae --type=user
+    python cvae_user.py --data_dir=data/$data --ckpt_folder=experiment/$data/cvae
 done
-    python cvae_user.py --data_dir=data/Phone --ckpt_folder=experiment/Phone/cvae
 
 
