12b85417148cfc455ebf65537b60ef2b88a17ae0
zakosai <linh.nguyen.1992@gmail.com>
2019-07-26 20:54:49 +0900

    retrain

--- seq2seq/train.sh
+++ seq2seq/train.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-dataset="Garden Automotive Instrument Grocery"
+dataset="Grocery Outdoor Tool Toy Office Phone Video"
 wsize="5"
 for data in $dataset; do
     python user_seq2seq.py --data=$data --cat=True  --w_size=5 --n_layers=2 --model_type=bilstm
