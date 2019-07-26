cad481d141fb75c6e5ab6bf16a973fde3d8db537
zakosai <linh.nguyen.1992@gmail.com>
2019-07-26 19:50:00 +0900

    retrain

--- seq2seq/train.sh
+++ seq2seq/train.sh
@@ -2,8 +2,8 @@
 dataset="Garden Automotive Instrument Grocery"
 wsize="5"
 for data in $dataset; do
-    python user_seq2seq.py --data=$data --cat=True --time=False --w_size=5 --n_layers=2 --model_type=bilstm
-    python user_seq2seq.py --data=$data --cat=False --time=True --w_size=5 --n_layers=2 --model_type=bilstm
+    python user_seq2seq.py --data=$data --cat=True  --w_size=5 --n_layers=2 --model_type=bilstm
+    python user_seq2seq.py --data=$data  --time=True --w_size=5 --n_layers=2 --model_type=bilstm
 done
 
 
