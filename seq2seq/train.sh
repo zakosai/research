ed91fc7d01887a2fed2f2554e6efe120aefc5e5f
zakosai <linh.nguyen.1992@gmail.com>
2019-07-26 17:54:45 +0900

    comparison

--- seq2seq/train.sh
+++ seq2seq/train.sh
@@ -1,32 +1,9 @@
 #!/usr/bin/env bash
-dataset="Music Instrument Clothing Video Phone Grocery Outdoor Tool Toy Office Pet"
+dataset="Garden Automotive Instrument Grocery"
 wsize="5"
 for data in $dataset; do
-    for w in $wsize; do
-        python user_seq2seq.py --data=$data --cat=True --time=True --w_size=$w --n_layers=2 --model_type=bilstm
-        python user_seq2seq.py --data=$data --cat=True --time=True --w_size=$w --n_layers=1 --model_type=bilstm
-        python user_seq2seq.py --data=$data --cat=True --time=True --w_size=$w --model_type=bigru --n_layers=2
-        python user_seq2seq.py --data=$data --cat=True --time=True --w_size=$w --model_type=bigru --n_layers=1
-        python user_seq2seq.py --data=$data --cat=True --time=True --w_size=$w --model_type=gru --n_layers=1
-        python seq2seq.py --data=$data --n_layers=1 --w_size=$w
-    done
-done
-python user_seq2seq.py --data=Beauty --cat=True --time=True --w_size=5 --n_layers=2 --model_type=bilstm
-python user_seq2seq.py --data=Beauty --cat=True --time=True --w_size=5 --n_layers=1 --model_type=bilstm
-python user_seq2seq.py --data=Beauty --cat=True --time=True --w_size=10 --n_layers=2 --model_type=bilstm
-python user_seq2seq.py --data=Beauty --cat=True --time=True --w_size=10 --n_layers=1 --model_type=bilstm
-
-dataset="Baby TV CD Kitchen Kindle Health Electronics"
-wsize="5 10"
-for data in $dataset; do
-    for w in $wsize; do
-        python seq2seq.py --data=$data --bilstm=False --n_layers=1 --w_size=$w
-        python user_seq2seq.py --data=$data --cat=True --time=True --w_size=$w --batch_size=500
-    done
-    python multi-VAE.py --data=$data
-    python vae.py --data_dir=data/$data --ckpt_folder=experiment/$data/cvae
-    python vae.py --data_dir=data/$data --ckpt_folder=experiment/$data/cvae --type=user
-    python cvae_user.py --data_dir=data/$data --ckpt_folder=experiment/$data/cvae
+    python user_seq2seq.py --data=$data --cat=True --time=False --w_size=5 --n_layers=2 --model_type=bilstm
+    python user_seq2seq.py --data=$data --cat=False --time=True --w_size=5 --n_layers=2 --model_type=bilstm
 done
 
 
