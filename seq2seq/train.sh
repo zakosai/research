11a8aa3a3024ce840a608c4d141d2918c603ed6d
zakosai <linh.nguyen.1992@gmail.com>
2019-07-29 16:53:48 +0900

    retrain

--- seq2seq/train.sh
+++ seq2seq/train.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
-dataset="Grocery Outdoor Tool Toy Office Phone Video"
+dataset="Garden Automotive Beauty Grocery Outdoor Tool Toy Office Phone Video"
 wsize="5"
 for data in $dataset; do
+    python user_seq2seq.py --data=$data --cat=True  --time=True --w_size=5 --n_layers=2 --model_type=bilstm
+    python user_seq2seq.py --data=$data --cat=True  --time=True --w_size=5 --n_layers=1 --model_type=bilstm
+    python user_seq2seq.py --data=$data --cat=True  --time=True --w_size=5 --n_layers=2 --model_type=bigru
+    python user_seq2seq.py --data=$data --cat=True  --time=True --w_size=5 --n_layers=1 --model_type=bigru
+    python user_seq2seq.py --data=$data --cat=True  --time=True --w_size=5 --n_layers=1 --model_type=bigru
     python user_seq2seq.py --data=$data --cat=True  --w_size=5 --n_layers=2 --model_type=bilstm
     python user_seq2seq.py --data=$data  --time=True --w_size=5 --n_layers=2 --model_type=bilstm
 done
