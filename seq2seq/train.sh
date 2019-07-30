cf8f3117460ed9cd920a24bc3e92bca6888431e9
zakosai <linh.nguyen.1992@gmail.com>
2019-07-30 13:26:26 +0900

    network component

--- seq2seq/train.sh
+++ seq2seq/train.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
 #dataset="Garden Automotive Beauty Grocery Outdoor Tool Toy Office Phone Video"
-dataset="Garden Automotive Beauty Grocery Instruments"
+dataset="Garden Automotive Beauty Grocery"
 wsize="5"
-#for data in $dataset; do
-#    python user_seq2seq.py --data=$data --cat=True  --time=True --w_size=5 --n_layers=2 --model_type=bilstm
-#    python user_seq2seq.py --data=$data --cat=True  --time=True --w_size=5 --n_layers=1 --model_type=bilstm
-#    python user_seq2seq.py --data=$data --cat=True  --time=True --w_size=5 --n_layers=2 --model_type=bigru
-#    python user_seq2seq.py --data=$data --cat=True  --time=True --w_size=5 --n_layers=1 --model_type=bigru
-#    python user_seq2seq.py --data=$data --cat=True  --time=True --w_size=5 --n_layers=1 --model_type=gru
-#    python user_seq2seq.py --data=$data --cat=True  --w_size=5 --n_layers=2 --model_type=bilstm
-#    python user_seq2seq.py --data=$data  --time=True --w_size=5 --n_layers=2 --model_type=bilstm
-#done
-
 for data in $dataset; do
-    python user_seq2seq.py --data=$data --cat=True  --time=True --w_size=5 --n_layers=2 --model_type=bilstm
+    python user_seq2seq.py --data=$data --cat=True  --time=True --w_size=5 --n_layers=1 --model_type=lstm
+    python user_seq2seq.py --data=$data --cat=True  --time=True --w_size=5 --n_layers=2 --model_type=lstm
+    python user_seq2seq.py --data=$data --cat=True  --time=True --w_size=5 --n_layers=2 --model_type=gru
+    python user_seq2seq.py --data=$data --cat=True  --time=True --w_size=5 --n_layers=1 --model_type=gru
+    python seq2seq.py --data=$data --cat=True --time=True --w_size=5 --n_layers=2 --model_type=bilstm
+    python seq2seq.py --data=$data --cat=True --time=True --w_size=5 --n_layers=2 --model_type=bigru
 done
+
