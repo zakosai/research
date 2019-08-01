281312c61eacde571c6e9d6ceb6ce26076ca28ad
zakosai <linh.nguyen.1992@gmail.com>
2019-08-01 16:15:32 +0900

    test

--- seq2seq/train.sh
+++ seq2seq/train.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #dataset="Garden Automotive Beauty Grocery Outdoor Tool Toy Office Phone Video"
-dataset="Automotive"
+dataset="Office"
 wsize="5"
 for data in $dataset; do
     python user_seq2seq.py --data=$data --cat=True  --time=True --w_size=5 --n_layers=1 --model_type=lstm --iter=500
