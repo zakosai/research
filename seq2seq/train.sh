a75de7b601f39fb1e16081ded01d7fcf316e8537
zakosai <linh.nguyen.1992@gmail.com>
2019-08-19 14:06:29 +0900

    item embedding

--- seq2seq/train.sh
+++ seq2seq/train.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
-dataset="Tool Toy Office Phone Video"
-#dataset="Office"
-wsize="5"
-for data in $dataset; do
-    python user_seq2seq.py --data=$data --cat=True  --time=True --w_size=5 --n_layers=1 --model_type=lstm --iter=500
-    python user_seq2seq.py --data=$data --cat=True  --time=True --w_size=5 --n_layers=2 --model_type=lstm --iter=500
-    python user_seq2seq.py --data=$data --cat=True  --time=True --w_size=5 --n_layers=2 --model_type=gru --iter=500
-    python user_seq2seq.py --data=$data --cat=True  --time=True --w_size=5 --n_layers=1 --model_type=gru --iter=500
-    python user_seq2seq.py --data=$data --cat=True  --time=True --w_size=5 --n_layers=1 --model_type=bilstm --iter=500
-    python user_seq2seq.py --data=$data --cat=True  --time=True --w_size=5 --n_layers=2 --model_type=bilstm --iter=500
-    python user_seq2seq.py --data=$data --cat=True  --time=True --w_size=5 --n_layers=2 --model_type=bigru --iter=500
-    python user_seq2seq.py --data=$data --cat=True  --time=True --w_size=5 --n_layers=1 --model_type=bigru --iter=500
-#    python seq2seq.py --data=$data --cat=True --time=True --w_size=5 --n_layers=2 --model_type=bilstm
-#    python seq2seq.py --data=$data --cat=True --time=True --w_size=5 --n_layers=2 --model_type=bigru
-done
+#dataset="Tool Toy Office Phone Video"
+##dataset="Office"
+#wsize="5"
+#for data in $dataset; do
+#    python user_seq2seq.py --data=$data --cat=True  --time=True --w_size=5 --n_layers=1 --model_type=lstm --iter=500
+#    python user_seq2seq.py --data=$data --cat=True  --time=True --w_size=5 --n_layers=2 --model_type=lstm --iter=500
+#    python user_seq2seq.py --data=$data --cat=True  --time=True --w_size=5 --n_layers=2 --model_type=gru --iter=500
+#    python user_seq2seq.py --data=$data --cat=True  --time=True --w_size=5 --n_layers=1 --model_type=gru --iter=500
+#    python user_seq2seq.py --data=$data --cat=True  --time=True --w_size=5 --n_layers=1 --model_type=bilstm --iter=500
+#    python user_seq2seq.py --data=$data --cat=True  --time=True --w_size=5 --n_layers=2 --model_type=bilstm --iter=500
+#    python user_seq2seq.py --data=$data --cat=True  --time=True --w_size=5 --n_layers=2 --model_type=bigru --iter=500
+#    python user_seq2seq.py --data=$data --cat=True  --time=True --w_size=5 --n_layers=1 --model_type=bigru --iter=500
+##    python seq2seq.py --data=$data --cat=True --time=True --w_size=5 --n_layers=2 --model_type=bilstm
+##    python seq2seq.py --data=$data --cat=True --time=True --w_size=5 --n_layers=2 --model_type=bigru
+#done
+#
+##    python user_seq2seq.py --data=Garden --cat=True  --time=True --w_size=5 --n_layers=2 --model_type=bigru --iter=500
 
-#    python user_seq2seq.py --data=Garden --cat=True  --time=True --w_size=5 --n_layers=2 --model_type=bigru --iter=500
+python vae.py --data_folder=data/Tool --ckpt_folder=experiment/Tool
+mv experiment/Tool/text.npy data/Tool/
+python python user_seq2seq.py --data=Tool --cat=True  --time=True --w_size=5 --n_layers=1 --model_type=bigru --iter=500
