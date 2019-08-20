4d3bdd11c3030b5df32e23884c3799d960ab9573
zakosai <linh.nguyen.1992@gmail.com>
2019-08-20 10:05:05 +0900

    larger dimension

--- seq2seq/train.sh
+++ seq2seq/train.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-dataset="Automotive Grocery Toy Office Phone Video"
-#dataset="Office"
+#dataset="Automotive Grocery Toy Office Phone Video"
+dataset="Office Grocery Tool"
 wsize="5"
 for data in $dataset; do
-    python user_seq2seq.py --data=$data --cat=True  --time=True --w_size=5 --n_layers=1 --model_type=bigru --iter=500 --global_only=True
-    python vae.py --data_dir=data/Tool --ckpt_folder=experiment/Tool
+#    python user_seq2seq.py --data=$data --cat=True  --time=True --w_size=5 --n_layers=1 --model_type=bigru --iter=500 --global_only=True
+    python vae.py --data_dir=data/Tool --ckpt_folder=experiment/Tool --zdim=100
     mv experiment/$data/text.npy data/$data/
     python seq_emb.py --data=$data --cat=True  --time=True --w_size=5 --n_layers=1 --model_type=lstm --iter=500
     python seq_emb.py --data=$data --cat=True  --time=True --w_size=5 --n_layers=2 --model_type=lstm --iter=500
