93a59f1a94d33a04be6c5048a294b70adce74675
zakosai <linh.nguyen.1992@gmail.com>
2019-11-11 14:30:24 +0900

    fix bug

--- cf-vae/vae_gan_new.py
+++ cf-vae/vae_gan_new.py
@@ -68,9 +68,8 @@ def build_model(d2d):
     # Loss GAN
     # d2d.loss_gen = loss_gen(av_A) + loss_gen(av_B)
     # d2d.loss_dis = loss_discriminator(av_A, av_BA) + loss_discriminator(av_B, av_AB)
-    print(z_mu_A.shape, z_mu_AB.shape)
-    loss_kl_mu_A = tf.reduce_mean(tf.reduce_sum((z_mu_A-z_mu_BA)**2), axis=1)
-    loss_kl_mu_B = tf.reduce_mean(tf.reduce_sum((z_mu_B-z_mu_AB)**2), axis=1)
+    loss_kl_mu_A = tf.reduce_mean(tf.reduce_sum((z_mu_A-z_mu_BA)**2, axis=1))
+    loss_kl_mu_B = tf.reduce_mean(tf.reduce_sum((z_mu_B-z_mu_AB)**2, axis=1))
 
     adv_vars_A = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope="adv")
     # adv_vars_B = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope="adv_B")
