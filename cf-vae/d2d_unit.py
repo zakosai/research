b2f30f099a71d7baa7d6f6b18370f5a9a649152f
zakosai <linh.nguyen.1992@gmail.com>
2019-12-06 14:22:41 +0900

    sparsemax

--- cf-vae/d2d_unit.py
+++ cf-vae/d2d_unit.py
@@ -526,6 +526,7 @@ def main(args):
                     [model.loss_val_a, model.loss_val_b, model.y_AB, model.y_BA],
                  feed_dict={model.x_A: user_A_test, model.x_B: user_B_test})
                 print("Loss test a: %f, Loss test b: %f" % (loss_test_a, loss_test_b))
+                np.savez(os.path.join(checkpoint_dir, "pred.npz"), y_ab=y_ab, y_ba=y_ba)
 
                 # y_ab = y_ab[test_B]
                 # y_ba = y_ba[test_A]
