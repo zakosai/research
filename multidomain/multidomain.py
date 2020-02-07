4ebd4f842c6035c50dc920585db6a2eb7b7c9df8
Linh Nguyen AI <linh@hblab.vn>
2020-02-07 16:54:46 +0900

    change network

--- multidomain/multidomain.py
+++ multidomain/multidomain.py
@@ -82,7 +82,7 @@ def main():
     batch_size = 500
     dataset = Dataset(["Health", "Clothing", "Grocery"])
     device = torch.device("cuda:0" if (torch.cuda.is_available()) else "cpu")
-    model = MultiDomain(dataset.input_size_list, [600, 200, 100], 3).to(device)
+    model = MultiDomain(dataset.input_size_list, [600, 200, 50], 3).to(device)
     loss_func = nn.LogSoftmax(dim=-1)
     result = [[0, 0], [0, 0]]
 
