a4633e16b65c31f7dbb83cc0e2ce14567a43294c
Linh Nguyen AI <linh@hblab.vn>
2020-03-12 14:51:13 +0900

    add embedding

--- new_cf/cf_dae.py
+++ new_cf/cf_dae.py
@@ -117,7 +117,7 @@ def test(data, model, device):
         item_info = torch.from_numpy(data[1]).float().to(device)
         user_recon, z_user = model['user'](user_info)
         item_recon, z_item = model['item'](item_info)
-        print(model['neuCF'].embeddings_user(0))
+        print(model['neuCF'].embeddings_user(torch.tensor([0,1], device=device)))
 
         predict = []
         for i in range(len(user_info)):
