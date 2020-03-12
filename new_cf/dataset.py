6bd1ae71c2a29f8b98b1c26d4f766d0de9122206
Linh Nguyen AI <linh@hblab.vn>
2020-03-12 16:24:09 +0900

    change info

--- new_cf/dataset.py
+++ new_cf/dataset.py
@@ -14,14 +14,14 @@ class Dataset:
         self.no_user, self.user_size = self.user_info.shape
 
         cf_data = self.gen_cf_matrix()
-        self.item_info = cf_data.T
-        self.user_info = cf_data
-        self.item_size = self.item_info.shape[1]
-        self.user_size = self.user_info.shape[1]
-        # self.item_info = np.concatenate((self.item_info, cf_data.T), axis=1)
-        # self.user_info = np.concatenate((self.user_info, cf_data), axis=1)
-        # self.item_size += self.no_user
-        # self.user_size += self.no_item
+        # self.item_info = cf_data.T
+        # self.user_info = cf_data
+        # self.item_size = self.item_info.shape[1]
+        # self.user_size = self.user_info.shape[1]
+        self.item_info = np.concatenate((self.item_info, cf_data.T), axis=1)
+        self.user_info = np.concatenate((self.user_info, cf_data), axis=1)
+        self.item_size += self.no_user
+        self.user_size += self.no_item
 
     def load_cvae_data(self, data_dir, data_type):
         data = {}
