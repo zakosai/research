feb1c4ae6846a162f83d355e2ecd409e5bb76c6d
Linh Nguyen AI <linh@hblab.vn>
2020-03-17 20:48:11 +0900

    train

--- new_cf/train.sh
+++ new_cf/train.sh
@@ -1,8 +1,9 @@
-python multiVAE.py --data_dir=../cf-vae/data/Tool/ --data_type=8p --iter=200 >> result/tool_8.txt
-python multiVAE.py --data_dir=../cf-vae/data/Outdoor/ --data_type=8p --iter=200 >> result/outdoor_8.txt
-python multiVAE.py --data_dir=../cf-vae/data/Outdoor/ --data_type=1p --iter=200 >> result/outdoor_1.txt
-python multiVAE.py --data_dir=../cf-vae/data/Health/ --data_type=1 --iter=200 >> result/health_1.txt
-python multiVAE.py --data_dir=../cf-vae/data/Health/ --data_type=80p --iter=200 >> result/health_80.txt
-python multiVAE.py --data_dir=../cf-vae/data/kitchen/ --data_type=1 --iter=200 >> result/kitchen_1.txt
-python multiVAE.py --data_dir=../cf-vae/data/kitchen/ --data_type=8 --iter=200 >> result/kitchen_8.txt
+python multiVAE.py --data_dir=../cf-vae/data/Tool/ --data_type=1p --iter=200 >> /media/linh/DATA/research/new_cf/result/tool_1.txt
+python multiVAE.py --data_dir=../cf-vae/data/Tool/ --data_type=8p --iter=200 >> /media/linh/DATA/research/new_cf/result/tool_8.txt
+python multiVAE.py --data_dir=../cf-vae/data/Outdoor/ --data_type=8p --iter=200 >> /media/linh/DATA/research/new_cf/result/outdoor_8.txt
+python multiVAE.py --data_dir=../cf-vae/data/Outdoor/ --data_type=1p --iter=200 >> /media/linh/DATA/research/new_cf/result/outdoor_1.txt
+python multiVAE.py --data_dir=../cf-vae/data/Health/ --data_type=1 --iter=200 >> /media/linh/DATA/research/new_cf/result/health_1.txt
+python multiVAE.py --data_dir=../cf-vae/data/Health/ --data_type=80p --iter=200 >> /media/linh/DATA/research/new_cf/result/health_80.txt
+python multiVAE.py --data_dir=../cf-vae/data/kitchen/ --data_type=1 --iter=200 >> /media/linh/DATA/research/new_cf/result/kitchen_1.txt
+python multiVAE.py --data_dir=../cf-vae/data/kitchen/ --data_type=8 --iter=200 >> /media/linh/DATA/research/new_cf/result/kitchen_8.txt
 
