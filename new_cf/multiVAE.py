d9be956b7de9efcbe831bc4b652d9ed451ecb415
Linh Nguyen AI <linh@hblab.vn>
2020-03-17 19:33:00 +0900

    multivae

--- new_cf/multiVAE.py
+++ new_cf/multiVAE.py
@@ -94,7 +94,7 @@ class Translation:
                                                                       axis=1)) + self.lambda_1 * loss_kl_item + tf.losses.get_regularization_loss()
 
         content_matrix = tf.matmul(z_user, tf.transpose(z_item))
-        content_matrix = tf.math.l2_normalize(content_matrix, axis=-1)
+        content_matrix = batch_norm(content_matrix)
         x = self.x * content_matrix
         # VAE for CF
         _, self.x_recon, loss_kl = self.vae(x, self.encode_dim, self.decode_dim, "CF")
