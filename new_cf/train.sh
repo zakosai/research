0f99bb2ffb00f49a4c30ff4cb15320094576b7b9
Linh Nguyen AI <linh@hblab.vn>
2020-03-18 23:05:20 +0900

    retrain

--- new_cf/train.sh
+++ new_cf/train.sh
@@ -1,7 +1,5 @@
 
-python multiVAE.py --data_dir=../cf-vae/data2/Health/ --data_type=8p --iter=100  >> /media/linh/DATA/research/new_cf/result/multi_health_8.txt
-python triple_vae.py --data_dir=../cf-vae/data2/Health/ --data_type=8p  --iter=100 --learning_rate=0.001 >> /media/linh/DATA/research/new_cf/result/health_8.txt
-python multiVAE.py --data_dir=../cf-vae/data2/Health/ --data_type=1p --iter=100  >> /media/linh/DATA/research/new_cf/result/multi_health_1.txt
-python triple_vae.py --data_dir=../cf-vae/data2/Health/ --data_type=1p  --iter=100  >> /media/linh/DATA/research/new_cf/result/health_1.txt
-python multiVAE.py --data_dir=../cf-vae/data2/Outdoor/ --data_type=8p --iter=100 --learning_rate=0.001 >> /media/linh/DATA/research/new_cf/result/multi_outdoor_8.txt
-python triple_vae.py --data_dir=../cf-vae/data2/Outdoor/ --data_type=8p  --iter=100 --learning_rate=0.001 >> /media/linh/DATA/research/new_cf/result/outdoor_8.txt
+python multiVAE.py --data_dir=../cf-vae/data2/Health/ --data_type=1p --iter=200  >> /media/linh/DATA/research/new_cf/result/multi_health_1.txt
+python triple_vae.py --data_dir=../cf-vae/data2/Health/ --data_type=1p  --iter=200  >> /media/linh/DATA/research/new_cf/result/health_1.txt
+python multiVAE.py --data_dir=../cf-vae/data2/Outdoor/ --data_type=8p --iter=200 --learning_rate=0.001 >> /media/linh/DATA/research/new_cf/result/multi_outdoor_8.txt
+python triple_vae.py --data_dir=../cf-vae/data2/Outdoor/ --data_type=8p  --iter=200 --learning_rate=0.001 >> /media/linh/DATA/research/new_cf/result/outdoor_8.txt
