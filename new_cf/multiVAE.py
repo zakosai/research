4745f1f3d11b40b36a23254f548e6bc8f9ff2e3d
Linh Nguyen AI <linh@hblab.vn>
2020-03-18 10:12:53 +0900

    change multiple to +

--- new_cf/multiVAE.py
+++ new_cf/multiVAE.py
@@ -95,7 +95,7 @@ class Translation:
 
         content_matrix = tf.matmul(z_user, tf.transpose(z_item))
         content_matrix = tf.keras.backend.l2_normalize(content_matrix, axis=-1)
-        x = self.x * content_matrix
+        x = self.x + content_matrix
         # VAE for CF
         _, self.x_recon, loss_kl = self.vae(x, self.encode_dim, self.decode_dim, "CF")
         # Loss VAE
