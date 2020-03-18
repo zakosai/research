ea974a25c1a033843aa52c2b1db477b91eca4773
Linh Nguyen AI <linh@hblab.vn>
2020-03-18 18:14:45 +0900

    retrain toy

--- new_cf/train.sh
+++ new_cf/train.sh
@@ -1,8 +1,4 @@
-python multiVAE.py --data_dir=../cf-vae/data2/TV/ --data_type=8p --iter=100 >> /media/linh/DATA/research/new_cf/result/multi_tv_8.txt
-python triple_vae.py --data_dir=../cf-vae/data2/TV/ --data_type=8p  --iter=100 >> /media/linh/DATA/research/new_cf/result/tv_8.txt
-python multiVAE.py --data_dir=../cf-vae/data2/TV/ --data_type=1p --iter=100 >> /media/linh/DATA/research/new_cf/result/multi_tv_1.txt
-python triple_vae.py --data_dir=../cf-vae/data2/TV/ --data_type=1p  --iter=100 >> /media/linh/DATA/research/new_cf/result/tv_1.txt
-python multiVAE.py --data_dir=../cf-vae/data2/Beauty/ --data_type=8p --iter=100 >> /media/linh/DATA/research/new_cf/result/multi_beauty_8.txt
-python triple_vae.py --data_dir=../cf-vae/data2/Beauty/ --data_type=8p  --iter=100 >> /media/linh/DATA/research/new_cf/result/beauty_8.txt
-python multiVAE.py --data_dir=../cf-vae/data2/Beauty/ --data_type=1p --iter=100 >> /media/linh/DATA/research/new_cf/result/multi_beauty_1.txt
-python triple_vae.py --data_dir=../cf-vae/data2/Beauty/ --data_type=1p  --iter=100 >> /media/linh/DATA/research/new_cf/result/beauty_1.txt
+python multiVAE.py --data_dir=../cf-vae/data2/TV/ --data_type=8p --iter=100 --learning_rate=0.001 >> /media/linh/DATA/research/new_cf/result/multi_tv_8.txt
+python triple_vae.py --data_dir=../cf-vae/data2/TV/ --data_type=8p  --iter=100 --learning_rate = 0.001 >> /media/linh/DATA/research/new_cf/result/tv_8.txt
+python multiVAE.py --data_dir=../cf-vae/data2/Toy/ --data_type=1p --iter=100 >> /media/linh/DATA/research/new_cf/result/multi_toy_1.txt
+python triple_vae.py --data_dir=../cf-vae/data2/Toy/ --data_type=1p  --iter=100 >> /media/linh/DATA/research/new_cf/result/toy_1.txt
