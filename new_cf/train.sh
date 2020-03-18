7242a695b1c395d3924112ec1c6832e0524f7ac6
Linh Nguyen AI <linh@hblab.vn>
2020-03-18 15:26:54 +0900

    retrain multivae

--- new_cf/train.sh
+++ new_cf/train.sh
@@ -1,9 +1,8 @@
-python multiVAE.py --data_dir=../cf-vae/data/Tool/ --data_type=1p --iter=200 >> /media/linh/DATA/research/new_cf/result/tool_1.txt
-python multiVAE.py --data_dir=../cf-vae/data/Tool/ --data_type=8p --iter=200 >> /media/linh/DATA/research/new_cf/result/tool_8.txt
-python multiVAE.py --data_dir=../cf-vae/data/Outdoor/ --data_type=8p --iter=200 >> /media/linh/DATA/research/new_cf/result/outdoor_8.txt
-python multiVAE.py --data_dir=../cf-vae/data/Outdoor/ --data_type=1p --iter=200 >> /media/linh/DATA/research/new_cf/result/outdoor_1.txt
-python multiVAE.py --data_dir=../cf-vae/data/Health/ --data_type=1 --iter=200 >> /media/linh/DATA/research/new_cf/result/health_1.txt
-python multiVAE.py --data_dir=../cf-vae/data/Health/ --data_type=80p --iter=200 >> /media/linh/DATA/research/new_cf/result/health_80.txt
-python multiVAE.py --data_dir=../cf-vae/data/kitchen/ --data_type=1 --iter=200 >> /media/linh/DATA/research/new_cf/result/kitchen_1.txt
-python multiVAE.py --data_dir=../cf-vae/data/kitchen/ --data_type=8 --iter=200 >> /media/linh/DATA/research/new_cf/result/kitchen_8.txt
-
+python multiVAE.py --data_dir=../cf-vae/data2/TV/ --data_type=8p --iter=100 >> /media/linh/DATA/research/new_cf/result/multi_tv_8.txt
+python triple_vae.py --data_dir=../cf-vae/data2/TV/ --data_type=8p  --iter=100 >> /media/linh/DATA/research/new_cf/result/tv_8.txt
+python multiVAE.py --data_dir=../cf-vae/data2/TV/ --data_type=1p --iter=100 >> /media/linh/DATA/research/new_cf/result/multi_tv_1.txt
+python triple_vae.py --data_dir=../cf-vae/data2/TV/ --data_type=1p  --iter=100 >> /media/linh/DATA/research/new_cf/result/tv_1.txt
+python multiVAE.py --data_dir=../cf-vae/data2/Beauty/ --data_type=8p --iter=100 >> /media/linh/DATA/research/new_cf/result/multi_beauty_8.txt
+python triple_vae.py --data_dir=../cf-vae/data2/Beauty/ --data_type=8p  --iter=100 >> /media/linh/DATA/research/new_cf/result/beauty_8.txt
+python multiVAE.py --data_dir=../cf-vae/data2/Beauty/ --data_type=1p --iter=100 >> /media/linh/DATA/research/new_cf/result/multi_beauty_1.txt
+python triple_vae.py --data_dir=../cf-vae/data2/Beauty/ --data_type=1p  --iter=100 >> /media/linh/DATA/research/new_cf/result/beauty_1.txt
