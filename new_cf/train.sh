f82bbaf79cc97cf54a143bf26bd55724500627e6
Linh Nguyen AI <linh@hblab.vn>
2020-03-19 17:00:35 +0900

    train all

--- new_cf/train.sh
+++ new_cf/train.sh
@@ -1,5 +1,8 @@
-
-python multiVAE.py --data_dir=../cf-vae/data2/Health/ --data_type=1p --iter=200  >> /media/linh/DATA/research/new_cf/result/multi_health_1.txt
-python triple_vae.py --data_dir=../cf-vae/data2/Health/ --data_type=1p  --iter=200  >> /media/linh/DATA/research/new_cf/result/health_1.txt
-python multiVAE.py --data_dir=../cf-vae/data2/Outdoor/ --data_type=8p --iter=200 --learning_rate=0.001 >> /media/linh/DATA/research/new_cf/result/multi_outdoor_8.txt
-python triple_vae.py --data_dir=../cf-vae/data2/Outdoor/ --data_type=8p  --iter=200 --learning_rate=0.001 >> /media/linh/DATA/research/new_cf/result/outdoor_8.txt
+folders="Automotive Baby CD Clothing Garden Grocery Instrument Kindle Music Office Pet Phone Video"
+for f in $folders; do
+    echo ${f}
+    python multiVAE.py --data_dir=/media/linh/DATA/research/cf-vae/data2/${f}/ --data_type=1p --iter=200  >> /media/linh/DATA/research/new_cf/result/multi_${f}_1.txt
+    python triple_vae.py --data_dir=/media/linh/DATA/research/cf-vae/data2/${f}/ --data_type=1p  --iter=200  >> /media/linh/DATA/research/new_cf/result/${f}_1.txt
+    python multiVAE.py --data_dir=/media/linh/DATA/research/cf-vae/data2/${f}/ --data_type=8p --iter=200 >> /media/linh/DATA/research/new_cf/result/multi_${f}_8.txt
+    python triple_vae.py --data_dir=../cf-vae/data2/${f}/ --data_type=8p  --iter=200 --learning_rate=0.001 >> /media/linh/DATA/research/new_cf/result/${f}_8.txt
+done
