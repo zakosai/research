9a7b0c887da7614d136579a2cc6dab9ad7a0e848
Linh Nguyen AI <linh@hblab.vn>
2020-03-20 17:50:16 +0900

    train CML

--- new_cf/train.sh
+++ new_cf/train.sh
@@ -1,8 +1,20 @@
 folders="Automotive Baby CD Clothing Garden Grocery Instrument Kindle Music Office Pet Phone Video"
 for f in $folders; do
     echo ${f}
-    python multiVAE.py --data_dir=/media/linh/DATA/research/cf-vae/data2/${f}/ --data_type=1p --iter=200  >> /media/linh/DATA/research/new_cf/result/multi_${f}_1.txt
-    python triple_vae.py --data_dir=/media/linh/DATA/research/cf-vae/data2/${f}/ --data_type=1p  --iter=200  >> /media/linh/DATA/research/new_cf/result/${f}_1.txt
-    python multiVAE.py --data_dir=/media/linh/DATA/research/cf-vae/data2/${f}/ --data_type=8p --iter=200 >> /media/linh/DATA/research/new_cf/result/multi_${f}_8.txt
-    python triple_vae.py --data_dir=../cf-vae/data2/${f}/ --data_type=8p  --iter=200 --learning_rate=0.001 >> /media/linh/DATA/research/new_cf/result/${f}_8.txt
+#    python multiVAE.py --data_dir=/media/linh/DATA/research/cf-vae/data2/${f}/ --data_type=1p --iter=200  >> /media/linh/DATA/research/new_cf/result/multi_${f}_1.txt
+#    python triple_vae.py --data_dir=/media/linh/DATA/research/cf-vae/data2/${f}/ --data_type=1p  --iter=200  >> /media/linh/DATA/research/new_cf/result/${f}_1.txt
+#    python multiVAE.py --data_dir=/media/linh/DATA/research/cf-vae/data2/${f}/ --data_type=8p --iter=200 >> /media/linh/DATA/research/new_cf/result/multi_${f}_8.txt
+#    python triple_vae.py --data_dir=../cf-vae/data2/${f}/ --data_type=8p  --iter=200 --learning_rate=0.001 >> /media/linh/DATA/research/new_cf/result/${f}_8.txt
+    mkdir experiment/${f}/
+    mkdir experiment/${f}/1
+    python train_vae.py --ckpt_folder=experiment/${f}/1 --data_dir=../cf-vae/data2/${f}/ --data_type=1
+    python train_vae.py --ckpt_folder=experiment/${f}/1 --data_dir=data/${f}/ --data_type=1 --type=user
+     python train_cvae_user.py --data_dir=../cf-vae/data2/${f}/ --ckpt_folder=experiment/${f}/1 --data_type=1 --gridsearch=1
+
+     mkdir experiment/${f}/8
+     python train_vae.py --ckpt_folder=experiment/${f}/8 --data_dir=../cf-vae/data2/${f}/ --data_type=8
+    python train_vae.py --ckpt_folder=experiment/${f}/8 --data_dir=data/${f}/ --data_type=8 --type=user
+     python train_cvae_user.py --data_dir=../cf-vae/data2/${f}/ --ckpt_folder=experiment/${f}/8 --data_type=8 --gridsearch=1
+
+
 done
