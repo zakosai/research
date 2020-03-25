aa97dee4851d6e0b1f8090ae5a5b392ff77ff701
Linh Nguyen AI <linh@hblab.vn>
2020-03-25 20:53:34 +0900

    retrain

--- new_cf/triple_vae.py
+++ new_cf/triple_vae.py
@@ -35,7 +35,7 @@ class Translation:
             for i in range(len(encode_dim)):
                 x_ = fully_connected(x_, encode_dim[i], self.active_function, scope="enc_%d"%i,
                                      weights_regularizer=self.regularizer)
-                # x_ = batch_norm(x_, decay=0.995)
+                x_ = batch_norm(x_, decay=0.995)
         return x_
 
     def dec(self, x, scope, decode_dim, reuse=False):
@@ -97,8 +97,7 @@ class Translation:
         # min = tf.reduce_min(content_matrix, axis=1, keep_dims=True)
         # max = tf.reduce_max(content_matrix, axis=1, keep_dims=True)
         # content_matrix = (content_matrix - min) / (max - min)
-        # x = (self.x * 0.99 + 0.01) * content_matrix
-        x = self.x * content_matrix
+        x = (self.x * 0.99 + 0.01) * content_matrix
         # x = tf.concat((self.x, content_matrix), axis=1)
         # VAE for CF
         # _, self.x_recon, loss_kl = self.vae(x, self.encode_dim, self.decode_dim, "CF")
@@ -119,7 +118,7 @@ def main(args):
 
     dataset = Dataset(args.data_dir, args.data_type)
     model = Translation(batch_size, dataset.no_item, dataset.user_size, dataset.item_size,
-                        [600, 200, 50], [200, 600, dataset.no_item], 50, learning_rate=args.learning_rate)
+                        [100, 50], [100, dataset.no_item], 50, learning_rate=args.learning_rate)
     model.build_model()
 
     sess = tf.Session()
