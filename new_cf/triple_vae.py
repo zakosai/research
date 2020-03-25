f577c90fbef9daccecee44e69422830cfd63f0e0
Linh Nguyen AI <linh@hblab.vn>
2020-03-25 19:51:10 +0900

    retrain

--- new_cf/triple_vae.py
+++ new_cf/triple_vae.py
@@ -126,20 +126,20 @@ def main(args):
     best = 0
     iter_no = int(dataset.no_user / batch_size + 1)
 
-    for i in range(1, 30):
-        shuffle_idx = np.random.permutation(range(dataset.no_user))
-        for j in range(iter_no):
-            list_idx = shuffle_idx[j * batch_size:(j + 1) * batch_size]
-            x = dataset.user_info[list_idx]
-            feed = {model.user_info: x}
-            _, loss_user = sess.run([model.train_op_user, model.loss_user], feed_dict=feed)
-
-        shuffle_idx = np.random.permutation(range(dataset.no_item))
-        for j in range(int(len(shuffle_idx) / batch_size + 1)):
-            list_idx = shuffle_idx[j * batch_size:(j + 1) * batch_size]
-            x = dataset.item_info[list_idx]
-            feed = {model.item_info: x}
-            _, loss_item = sess.run([model.train_op_item, model.loss_item], feed_dict=feed)
+    # for i in range(1, 30):
+    #     shuffle_idx = np.random.permutation(range(dataset.no_user))
+    #     for j in range(iter_no):
+    #         list_idx = shuffle_idx[j * batch_size:(j + 1) * batch_size]
+    #         x = dataset.user_info[list_idx]
+    #         feed = {model.user_info: x}
+    #         _, loss_user = sess.run([model.train_op_user, model.loss_user], feed_dict=feed)
+    #
+    #     shuffle_idx = np.random.permutation(range(dataset.no_item))
+    #     for j in range(int(len(shuffle_idx) / batch_size + 1)):
+    #         list_idx = shuffle_idx[j * batch_size:(j + 1) * batch_size]
+    #         x = dataset.item_info[list_idx]
+    #         feed = {model.item_info: x}
+    #         _, loss_item = sess.run([model.train_op_item, model.loss_item], feed_dict=feed)
 
     for i in range(1, iter):
         shuffle_idx = np.random.permutation(range(dataset.no_user))
