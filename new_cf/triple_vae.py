9435b0ae46f93e53f6251212ce1087ccc9d2666e
Linh Nguyen AI <linh@hblab.vn>
2020-03-25 19:03:19 +0900

    retrain

--- new_cf/triple_vae.py
+++ new_cf/triple_vae.py
@@ -84,13 +84,13 @@ class Translation:
 
         # VAE for user
         z_user, user_recon, loss_kl_user = self.vae(self.user_info, [], [self.user_info_dim], "user")
-        self.loss_user = self.lambda_2 * tf.reduce_mean(tf.reduce_sum(binary_crossentropy(self.user_info, user_recon), axis=1)) +\
-             self.lambda_1 * loss_kl_user + self.lambda_1 * tf.losses.get_regularization_loss()
+        self.loss_user = tf.reduce_mean(tf.reduce_sum(binary_crossentropy(self.user_info, user_recon), axis=1)) +\
+              loss_kl_user + 2 * tf.losses.get_regularization_loss()
 
         # VAE for item
         z_item, item_recon, loss_kl_item = self.vae(self.item_info, [200], [200, self.item_info_dim], "item")
-        self.loss_item = self.lambda_2 * tf.reduce_mean(tf.reduce_sum(binary_crossentropy(self.item_info, item_recon), axis=1)) +\
-                         self.lambda_1 * loss_kl_item + self.lambda_1 * tf.losses.get_regularization_loss()
+        self.loss_item = tf.reduce_mean(tf.reduce_sum(binary_crossentropy(self.item_info, item_recon), axis=1)) +\
+                         loss_kl_item + 2 * tf.losses.get_regularization_loss()
 
         content_matrix = tf.matmul(z_user, tf.transpose(z_item))
         content_matrix = tf.keras.backend.l2_normalize(content_matrix)
