d1ce487910209604f9217c32e905ba06ec899489
Linh Nguyen AI <linh@hblab.vn>
2020-03-26 14:14:44 +0900

    retrain

--- new_cf/triple_vae.py
+++ new_cf/triple_vae.py
@@ -84,13 +84,13 @@ class Translation:
 
         # VAE for user
         z_user, user_recon, loss_kl_user = self.vae(self.user_info, [100], [100, self.user_info_dim], "user")
-        self.loss_user = 100 * tf.reduce_mean(tf.reduce_sum(binary_crossentropy(self.user_info, user_recon), axis=1)) +\
-              0.1 * loss_kl_user + 0.1 * tf.losses.get_regularization_loss()
+        self.loss_user = tf.reduce_mean(tf.reduce_sum(binary_crossentropy(self.user_info, user_recon), axis=1)) +\
+              loss_kl_user + 2 * tf.losses.get_regularization_loss()
 
         # VAE for item
         z_item, item_recon, loss_kl_item = self.vae(self.item_info, [600, 200], [200, 600, self.item_info_dim], "item")
-        self.loss_item = 100 * tf.reduce_mean(tf.reduce_sum(binary_crossentropy(self.item_info, item_recon), axis=1)) +\
-                         0.1 * loss_kl_item + 0.1 * tf.losses.get_regularization_loss()
+        self.loss_item = tf.reduce_mean(tf.reduce_sum(binary_crossentropy(self.item_info, item_recon), axis=1)) +\
+                         loss_kl_item + 2 * tf.losses.get_regularization_loss()
 
         content_matrix = tf.matmul(z_user, tf.transpose(z_item))
         content_matrix = tf.keras.backend.l2_normalize(content_matrix)
@@ -103,8 +103,8 @@ class Translation:
         # VAE for CF
         _, self.x_recon, loss_kl = self.vae(x, self.encode_dim, self.decode_dim, "CF")
         # Loss VAE
-        self.loss = loss_kl + self.loss_reconstruct(self.x, self.x_recon) + \
-                    2 * tf.losses.get_regularization_loss()
+        self.loss = 0.1 * loss_kl + 100 * self.loss_reconstruct(self.x, self.x_recon) + \
+                    0.1 * tf.losses.get_regularization_loss()
         # self.x_recon = self.vae(x, self.encode_dim, self.decode_dim, "CF")
         # self.loss = 100 * self.loss_reconstruct(self.x, self.x_recon) + 0.1 * tf.losses.get_regularization_loss()
 
