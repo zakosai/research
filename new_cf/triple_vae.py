130406b0e6afaba887d8f41830e747d7fdf1e4ee
Linh Nguyen AI <linh@hblab.vn>
2020-03-26 15:26:04 +0900

    retrain

--- new_cf/triple_vae.py
+++ new_cf/triple_vae.py
@@ -93,9 +93,9 @@ class Translation:
                          self.lambda_1 * loss_kl_item + self.lambda_1 * tf.losses.get_regularization_loss()
 
         content_matrix = tf.matmul(z_user, tf.transpose(z_item))
-        min = tf.reduce_min(content_matrix, axis=1, keep_dims=True)
-        max = tf.reduce_max(content_matrix, axis=1, keep_dims=True)
-        content_matrix = (content_matrix - min) / (max - min)
+        # min = tf.reduce_min(content_matrix, axis=1, keep_dims=True)
+        # max = tf.reduce_max(content_matrix, axis=1, keep_dims=True)
+        # content_matrix = (content_matrix - min) / (max - min)
         x = (self.x * (1 - 1e-2) + 1e-2) * content_matrix
         # x = tf.concat((self.x, content_matrix), axis=1)
         # VAE for CF
