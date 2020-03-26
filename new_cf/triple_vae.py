48f63c6f1e5c1550cc52074c8e7b6f1734e92e1a
Linh Nguyen AI <linh@hblab.vn>
2020-03-26 18:01:00 +0900

    shallow multi-VAE

--- new_cf/triple_vae.py
+++ new_cf/triple_vae.py
@@ -180,7 +180,7 @@ def main(args):
             model.train = True
             if recall > best:
                 best = recall
-        if (i%10 == 0) and (i < 30):
+        if (i%20 == 0) and (model.learning_rate >= 1e-6):
             model.learning_rate /= 10
     print(best)
 
