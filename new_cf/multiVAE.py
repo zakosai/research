3f374f98a88af5b9e2104f8843d204fe75c1ec9c
Linh Nguyen AI <linh@hblab.vn>
2020-03-31 09:56:52 +0900

    retrain

--- new_cf/multiVAE.py
+++ new_cf/multiVAE.py
@@ -112,7 +112,7 @@ def main(args):
 
     dataset = Dataset(args.data_dir, args.data_type)
     model = Translation(batch_size, dataset.no_item, dataset.user_size, dataset.item_size,
-                        [], [dataset.no_item], 50, learning_rate=args.learning_rate)
+                        [200], [200, dataset.no_item], 100, learning_rate=args.learning_rate)
     model.build_model()
 
     sess = tf.Session()
--- new_cf/train.sh
+++ new_cf/train.sh
@@ -2,10 +2,10 @@
 folders="Tool Outdoor Kitchen TV Beauty Toy Automotive Baby CD Clothing Garden Grocery Kindle Music Office Pet Phone Vide"
 for f in $folders; do
     echo ${f}
-    python multiVAE.py --data_dir=/media/linh/DATA/research/cf-vae/data2/${f}/ --data_type=1p --iter=200 --learning_rate=0.001 >> /media/linh/DATA/research/new_cf/result6/multi_${f}_1.txt
-    python triple_vae.py --data_dir=/media/linh/DATA/research/cf-vae/data2/${f}/ --data_type=1p  --iter=200 --learning_rate=0.001 >> /media/linh/DATA/research/new_cf/result6/${f}_1.txt
-    python multiVAE.py --data_dir=/media/linh/DATA/research/cf-vae/data2/${f}/ --data_type=8p --iter=200 --learning_rate=0.001 >> /media/linh/DATA/research/new_cf/result6/multi_${f}_8.txt
-    python triple_vae.py --data_dir=../cf-vae/data2/${f}/ --data_type=8p  --iter=200 --learning_rate=0.001 >> /media/linh/DATA/research/new_cf/result6/${f}_8.txt
+    python multiVAE.py --data_dir=/media/linh/DATA/research/cf-vae/data2/${f}/ --data_type=1p --iter=200 --learning_rate=0.001 >> /media/linh/DATA/research/new_cf/result7/multi_${f}_1.txt
+    python triple_vae.py --data_dir=/media/linh/DATA/research/cf-vae/data2/${f}/ --data_type=1p  --iter=200 --learning_rate=0.001 >> /media/linh/DATA/research/new_cf/result7/${f}_1.txt
+    python multiVAE.py --data_dir=/media/linh/DATA/research/cf-vae/data2/${f}/ --data_type=8p --iter=200 --learning_rate=0.001 >> /media/linh/DATA/research/new_cf/result7/multi_${f}_8.txt
+    python triple_vae.py --data_dir=../cf-vae/data2/${f}/ --data_type=8p  --iter=200 --learning_rate=0.001 >> /media/linh/DATA/research/new_cf/result7/${f}_8.txt
 #    mkdir experiment/${f}/
 #    mkdir experiment/${f}/1
 #    python train_vae.py --ckpt_folder=experiment/${f}/1 --data_dir=../cf-vae/data2/${f}/ --data_type=1
