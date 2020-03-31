92bb49d6d4c9ac8e525a62e0a537b67be898046e
Linh Nguyen AI <linh@hblab.vn>
2020-03-31 19:17:31 +0900

    retrain

--- new_cf/new_model.py
+++ new_cf/new_model.py
@@ -95,8 +95,8 @@ class Translation:
         self.loss_item = tf.reduce_mean(tf.reduce_sum(binary_crossentropy(self.item_info, item_recon), axis=1)) +\
                          loss_kl_item + tf.losses.get_regularization_loss()
         self.z_item = z_item
-        x = tf.multiply(self.x, self.item_value)
-        # x = self.x
+        # x = tf.multiply(self.x, self.item_value)
+        x = self.x
 
         # content_matrix = tf.matmul(z_user, tf.transpose(z_item))
         # min = tf.reduce_min(content_matrix, axis=1, keep_dims=True)
@@ -123,7 +123,7 @@ def main(args):
 
     dataset = Dataset(args.data_dir, args.data_type)
     model = Translation(batch_size, dataset.no_item, dataset.user_size, dataset.item_size,
-                        [600, 200], [dataset.no_item], 50, learning_rate=args.learning_rate)
+                        [1000, 600, 200], [dataset.no_item], 50, learning_rate=args.learning_rate)
     model.build_model()
 
     sess = tf.Session()
