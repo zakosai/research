08ee56b9f03b6a5f70a9291c14648aa8e1ab4edf
Linh Nguyen AI <linh@hblab.vn>
2020-04-01 23:15:00 +0900

    finetune

--- new_cf/train.sh
+++ new_cf/train.sh
@@ -4,8 +4,8 @@ for f in $folders; do
     echo ${f}
 #    python multiVAE.py --data_dir=/media/linh/DATA/research/cf-vae/data2/${f}/ --data_type=1p --iter=200 --learning_rate=0.001 >> /media/linh/DATA/research/new_cf/result7/multi_${f}_1.txt
 #    python triple_vae.py --data_dir=/media/linh/DATA/research/cf-vae/data2/${f}/ --data_type=1p  --iter=200 --learning_rate=0.001 >> /media/linh/DATA/research/new_cf/result7/${f}_1.txt
-#    python new_model.py --data_dir=/media/linh/DATA/research/cf-vae/data2/${f}/ --data_type=8p --iter=200 --learning_rate=0.001 >> /media/linh/DATA/research/new_cf/result7/new_${f}_8.txt
-    python triple_vae.py --data_dir=../cf-vae/data2/${f}/ --data_type=8p  --iter=200 --learning_rate=0.0001 >> /media/linh/DATA/research/new_cf/result7/${f}_8.txt
+    python new_model.py --data_dir=/media/linh/DATA/research/cf-vae/data2/${f}/ --data_type=8p --iter=200 --learning_rate=0.001 >> /media/linh/DATA/research/new_cf/result8/new_${f}_8.txt
+    python triple_vae.py --data_dir=../cf-vae/data2/${f}/ --data_type=8p  --iter=200 --learning_rate=0.0001 >> /media/linh/DATA/research/new_cf/result8/${f}_8.txt
 #    mkdir experiment/${f}/
 #    mkdir experiment/${f}/1
 #    python train_vae.py --ckpt_folder=experiment/${f}/1 --data_dir=../cf-vae/data2/${f}/ --data_type=1
@@ -20,4 +20,4 @@ for f in $folders; do
 
 done
 
-#python triple_vae.py --data_dir=../cf-vae/data2/Instrument/ --data_type=8p  --iter=200 --learning_rate=0.001
+python triple_vae.py --data_dir=../cf-vae/data2/Music/ --data_type=8p  --iter=200 --learning_rate=0.001
