14cf1a514ff44d1381f710bb37d0f39b3ddd51d1
Linh Nguyen AI <linh@hblab.vn>
2020-04-01 15:31:44 +0900

    finetune

--- new_cf/train.sh
+++ new_cf/train.sh
@@ -1,11 +1,11 @@
 #folders="Automotive Baby CD Clothing Garden Grocery Kindle Music Office Pet Phone Video"
-folders="Tool Outdoor Kitchen TV Beauty Toy Automotive Baby CD Garden Grocery Kindle Music Office Pet Phone Video"
+folders="Kitchen TV Beauty Toy Automotive Baby CD Garden Grocery Kindle Music Office Pet Phone Video"
 for f in $folders; do
     echo ${f}
 #    python multiVAE.py --data_dir=/media/linh/DATA/research/cf-vae/data2/${f}/ --data_type=1p --iter=200 --learning_rate=0.001 >> /media/linh/DATA/research/new_cf/result7/multi_${f}_1.txt
 #    python triple_vae.py --data_dir=/media/linh/DATA/research/cf-vae/data2/${f}/ --data_type=1p  --iter=200 --learning_rate=0.001 >> /media/linh/DATA/research/new_cf/result7/${f}_1.txt
 #    python new_model.py --data_dir=/media/linh/DATA/research/cf-vae/data2/${f}/ --data_type=8p --iter=200 --learning_rate=0.001 >> /media/linh/DATA/research/new_cf/result7/new_${f}_8.txt
-    python triple_vae.py --data_dir=../cf-vae/data2/${f}/ --data_type=8p  --iter=200 --learning_rate=0.001 >> /media/linh/DATA/research/new_cf/result7/${f}_8.txt
+    python triple_vae.py --data_dir=../cf-vae/data2/${f}/ --data_type=8p  --iter=200 --learning_rate=0.0001 >> /media/linh/DATA/research/new_cf/result7/${f}_8.txt
 #    mkdir experiment/${f}/
 #    mkdir experiment/${f}/1
 #    python train_vae.py --ckpt_folder=experiment/${f}/1 --data_dir=../cf-vae/data2/${f}/ --data_type=1
