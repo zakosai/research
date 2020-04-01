ec7a44c61f1772b116e7536dc274a740998e01aa
Linh Nguyen AI <linh@hblab.vn>
2020-04-01 14:10:06 +0900

    finetune

--- new_cf/train.sh
+++ new_cf/train.sh
@@ -4,7 +4,7 @@ for f in $folders; do
     echo ${f}
 #    python multiVAE.py --data_dir=/media/linh/DATA/research/cf-vae/data2/${f}/ --data_type=1p --iter=200 --learning_rate=0.001 >> /media/linh/DATA/research/new_cf/result7/multi_${f}_1.txt
 #    python triple_vae.py --data_dir=/media/linh/DATA/research/cf-vae/data2/${f}/ --data_type=1p  --iter=200 --learning_rate=0.001 >> /media/linh/DATA/research/new_cf/result7/${f}_1.txt
-    python new_model.py --data_dir=/media/linh/DATA/research/cf-vae/data2/${f}/ --data_type=8p --iter=200 --learning_rate=0.001 >> /media/linh/DATA/research/new_cf/result7/new_${f}_8.txt
+#    python new_model.py --data_dir=/media/linh/DATA/research/cf-vae/data2/${f}/ --data_type=8p --iter=200 --learning_rate=0.001 >> /media/linh/DATA/research/new_cf/result7/new_${f}_8.txt
     python triple_vae.py --data_dir=../cf-vae/data2/${f}/ --data_type=8p  --iter=200 --learning_rate=0.001 >> /media/linh/DATA/research/new_cf/result7/${f}_8.txt
 #    mkdir experiment/${f}/
 #    mkdir experiment/${f}/1
