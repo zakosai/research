b833413c7f9fb6e640cdebe9c365c5be8f6cf3cf
Linh Nguyen AI <linh@hblab.vn>
2020-04-07 18:52:54 +0900

    add z_user into middle

--- new_cf/triple_vae.py
+++ new_cf/triple_vae.py
@@ -118,7 +118,7 @@ def main(args):
 
     dataset = Dataset(args.data_dir, args.data_type)
     model = Translation(batch_size, dataset.no_item, dataset.user_size, dataset.item_size,
-                        [1000, 200], [dataset.no_item], 50, learning_rate=args.learning_rate)
+                        [100], [dataset.no_item], 50, learning_rate=args.learning_rate)
     model.build_model()
 
     sess = tf.Session()
