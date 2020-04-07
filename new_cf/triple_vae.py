845645a54abac31c9b0f0868fdc1bebf7587d4ce
Linh Nguyen AI <linh@hblab.vn>
2020-04-07 19:55:34 +0900

    add z_user into middle

--- new_cf/triple_vae.py
+++ new_cf/triple_vae.py
@@ -19,7 +19,7 @@ class Translation:
         self.lambda_1 = lambda_1
         self.lambda_2 = lambda_2
         self.learning_rate = learning_rate
-        self.active_function = tf.nn.relu
+        self.active_function = tf.nn.tanh
         self.user_info_dim = user_info_dim
         self.item_info_dim = item_info_dim
         # self.z_A = z_A
@@ -88,12 +88,12 @@ class Translation:
         # VAE for user
         z_user, user_recon, loss_kl_user = self.vae(self.user_info, [200], [200, self.user_info_dim], "user")
         self.loss_user = self.lambda_2 * tf.reduce_mean(tf.reduce_sum(binary_crossentropy(self.user_info, user_recon), axis=1)) +\
-             self.lambda_1 * loss_kl_user + self.lambda_1 * tf.losses.get_regularization_loss()
+             self.lambda_1 * loss_kl_user + 10 * tf.losses.get_regularization_loss()
 
         # VAE for item
         z_item, item_recon, loss_kl_item = self.vae(self.item_info, [400, 200], [200, 400, self.item_info_dim], "item")
         self.loss_item = self.lambda_2 * tf.reduce_mean(tf.reduce_sum(binary_crossentropy(self.item_info, item_recon), axis=1)) +\
-                         self.lambda_1 * loss_kl_item + self.lambda_1 * tf.losses.get_regularization_loss()
+                         self.lambda_1 * loss_kl_item + 10 * tf.losses.get_regularization_loss()
 
         content_matrix = tf.matmul(z_user, tf.transpose(z_item))
         min = tf.reduce_min(content_matrix, axis=1, keep_dims=True)
