6956074a1173d5e29f6f54a5cc82482b392553c7
Linh Nguyen AI <linh@hblab.vn>
2020-04-10 20:39:26 +0900

    cv

--- new_cf/triple_vae.py
+++ new_cf/triple_vae.py
@@ -27,7 +27,7 @@ class Translation:
         self.train = True
         self.regularizer = tf.contrib.layers.l2_regularizer(scale=0.1)
 
-    def enc(self, x, scope, encode_dim, reuse=False):
+    def enc(self, x, scope, encode_dim, reuse=False, activation=None):
         x_ = x
 
         # x_ = tf.nn.l2_normalize(x_, 1)
@@ -35,39 +35,39 @@ class Translation:
         #     x_ = tf.nn.dropout(x_, 0.7)
         with tf.variable_scope(scope, reuse=reuse):
             for i in range(len(encode_dim)):
-                x_ = fully_connected(x_, encode_dim[i], self.active_function, scope="enc_%d"%i,
+                x_ = fully_connected(x_, encode_dim[i], activation, scope="enc_%d"%i,
                                      weights_regularizer=self.regularizer)
                 x_ = batch_norm(x_, decay=0.995)
         return x_
 
-    def dec(self, x, scope, decode_dim, reuse=False):
+    def dec(self, x, scope, decode_dim, reuse=False, activation=None):
         x_ = x
         # if self.train:
         #     x_ = tf.nn.dropout(x_, 0.7)
         with tf.variable_scope(scope, reuse=reuse):
             for i in range(len(decode_dim)):
-                x_ = fully_connected(x_, decode_dim[i], self.active_function, scope="dec_%d" % i,
+                x_ = fully_connected(x_, decode_dim[i], activation, scope="dec_%d" % i,
                                      weights_regularizer=self.regularizer)
         return x_
 
     def gen_z(self, h, scope, reuse=False):
         with tf.variable_scope(scope, reuse=reuse):
-            z_mu = fully_connected(h, self.z_dim, self.active_function, scope="z_mu")
-            z_sigma = fully_connected(h, self.z_dim, self.active_function, scope="z_sigma")
+            z_mu = fully_connected(h, self.z_dim, scope="z_mu")
+            z_sigma = fully_connected(h, self.z_dim, scope="z_sigma")
             e = tf.random_normal(tf.shape(z_mu))
             z = z_mu + tf.sqrt(tf.maximum(tf.exp(z_sigma), self.eps)) * e
         return z, z_mu, z_sigma
 
-    def vae(self, x, encode_dim, decode_dim, scope, reuse=False, user_info=None):
+    def vae(self, x, encode_dim, decode_dim, scope, reuse=False, activation=None):
         with tf.variable_scope(scope, reuse=reuse):
-            h = self.enc(x, "encode", encode_dim)
+            h = self.enc(x, "encode", encode_dim, activation)
             if scope == "CF":
                 # h = tf.concat((h, user_info), axis=-1)
-                y = self.dec(h, "decode", decode_dim)
+                y = self.dec(h, "decode", decode_dim, activation)
                 return y
             z, z_mu, z_sigma = self.gen_z(h, "VAE")
             loss_kl = self.loss_kl(z_mu, z_sigma)
-            y = self.dec(z, "decode", decode_dim)
+            y = self.dec(z, "decode", decode_dim, activation)
         return z, y, loss_kl
 
     def loss_kl(self, mu, sigma):
@@ -86,12 +86,12 @@ class Translation:
         self.item_info = tf.placeholder(tf.float32, [None, self.item_info_dim], name='item_info')
 
         # VAE for user
-        z_user, user_recon, loss_kl_user = self.vae(self.user_info, [200], [200, self.user_info_dim], "user")
+        z_user, user_recon, loss_kl_user = self.vae(self.user_info, [200], [200, self.user_info_dim], "user", tf.nn.relu())
         self.loss_user = self.lambda_2 * tf.reduce_mean(tf.reduce_sum(binary_crossentropy(self.user_info, user_recon), axis=1)) +\
-             self.lambda_1 * loss_kl_user +  tf.losses.get_regularization_loss()
+             self.lambda_1 * loss_kl_user + tf.losses.get_regularization_loss()
 
         # VAE for item
-        z_item, item_recon, loss_kl_item = self.vae(self.item_info, [400, 200], [200, 400, self.item_info_dim], "item")
+        z_item, item_recon, loss_kl_item = self.vae(self.item_info, [400, 200], [200, 400, self.item_info_dim], "item", tf.nn.relu())
         self.loss_item = self.lambda_2 * tf.reduce_mean(tf.reduce_sum(binary_crossentropy(self.item_info, item_recon), axis=1)) +\
                          self.lambda_1 * loss_kl_item + tf.losses.get_regularization_loss()
 
@@ -105,7 +105,7 @@ class Translation:
         # # Loss VAE
         # self.loss = loss_kl + self.loss_reconstruct(self.x, self.x_recon) + \
         #             2 * tf.losses.get_regularization_loss()
-        self.x_recon = self.vae(x, self.encode_dim, self.decode_dim, "CF")
+        self.x_recon = self.vae(x, self.encode_dim, self.decode_dim, "CF", tf.nn.tanh)
         self.loss = self.loss_reconstruct(self.x, self.x_recon) + 10 * tf.losses.get_regularization_loss()
 
         self.train_op = tf.train.AdamOptimizer(self.learning_rate).minimize(self.loss)
