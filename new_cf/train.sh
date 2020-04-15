d43c0c98832c7f576fbdbc7c40880de405354f31
Linh Nguyen AI <linh@hblab.vn>
2020-04-15 17:07:41 +0900

    cv

--- new_cf/train.sh
+++ new_cf/train.sh
@@ -1,6 +1,6 @@
 #folders="Automotive Baby CD Clothing Garden Grocery Kindle Music Office Pet Phone Video"
 #folders="Tool Outdoor Kitchen TV Beauty Toy Automotive Baby CD Garden Grocery Kindle Music Office Pet Phone Video"
-folders="Outdoor Beauty CD Kindle Music Pet Phone Video"
+folders="Outdoor Beauty CD Music Pet Phone Video"
 for f in $folders; do
     echo ${f}
     python triple_vae.py --data_dir=/media/linh/DATA/research/cf-vae/data2/${f}/ --data_type=8p --iter=400 --learning_rate=0.0001 >> /media/linh/DATA/research/new_cf/result9/${f}_8.txt
@@ -22,4 +22,4 @@ for f in $folders; do
 
 done
 
-#python triple_vae.py --data_dir=../cf-vae/data2/Beauty/ --data_type=1p  --iter=200 --learning_rate=0.0001
+#python triple_vae.py --data_dir=../cf-vae/data2/Phone/ --data_type=1p  --iter=200 --learning_rate=0.0001
