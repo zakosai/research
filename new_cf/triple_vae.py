ca0680e12d93f68f66a96867259e333200a01caf
Linh Nguyen AI <linh@hblab.vn>
2020-04-17 18:29:37 +0900

    re train

--- new_cf/triple_vae.py
+++ new_cf/triple_vae.py
@@ -78,8 +78,8 @@ class Translation:
             for i in range(len(encode_dim)):
                 x_ = fully_connected(x_, encode_dim[i], activation, scope="enc_%d" % i,
                                      weights_regularizer=regular)
-            for i in range(len(encode_dim)):
-                x_ = fully_connected(x_, decode_dim[i], activation, scope="enc_%d" % i,
+            for i in range(len(decode_dim)):
+                x_ = fully_connected(x_, decode_dim[i], activation, scope="dec_%d" % i,
                                      weights_regularizer=regular)
         return x_
 
@@ -121,7 +121,7 @@ class Translation:
         # # Loss VAE
         # self.loss = loss_kl + self.loss_reconstruct(self.x, self.x_recon) + \
         #             2 * tf.losses.get_regularization_loss()
-        self.x_recon = self.dae(x, self.encode_dim, self.decode_dim, "pred", activation=tf.nn.tanh)
+        self.x_recon = self.dae(x, self.encode_dim, self.decode_dim, "CF", activation=tf.nn.tanh)
         self.loss = self.loss_reconstruct(self.x, self.x_recon) + 2 * tf.losses.get_regularization_loss()
 
         self.train_op = tf.train.AdamOptimizer(self.learning_rate).minimize(self.loss)
