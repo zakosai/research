5c0aea1bee0caded70b58c0b63c19294cb7cfc98
Linh Nguyen AI <linh@hblab.vn>
2020-04-23 20:36:20 +0900

    debug

--- new_cf/cvae_cf.py
+++ new_cf/cvae_cf.py
@@ -94,7 +94,7 @@ class Translation:
         self.loss_enc = loss_recon + kl_z_y + 0.1 * kl_h_x + kl_h_xy
 
         self.train_op_enc = tf.train.AdamOptimizer(self.learning_rate).minimize(self.loss_enc)
-        self.loss_values = [self.loss_enc, loss_recon, kl_z_y, kl_h_x, kl_z_y]
+        self.loss_values = [self.loss_enc, loss_recon, kl_z_y, kl_h_x, kl_h_xy]
 
 
 def main(args):
