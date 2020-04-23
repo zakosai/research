1843a0b5750ca80fdb8a42209a152efa4ed9d7e0
Linh Nguyen AI <linh@hblab.vn>
2020-04-23 21:47:45 +0900

    finetune

--- new_cf/train.sh
+++ new_cf/train.sh
@@ -9,7 +9,7 @@ for f in $folders; do
 #    python multiVAE.py --data_dir=/media/linh/DATA/research/cf-vae/data2/${f}/ --data_type=8p --iter=200 --learning_rate=0.001 >> /media/linh/DATA/research/new_cf/result8/multi_${f}_8.txt
 #    python triple_vae.py --data_dir=/media/linh/DATA/research/cf-vae/data2/${f}/ --data_type=1p  --iter=200 --learning_rate=0.001 >> /media/linh/DATA/research/new_cf/result7/${f}_1.txt
 #    python new_model.py --data_dir=/media/linh/DATA/research/cf-vae/data2/${f}/ --data_type=8p --iter=200 --learning_rate=0.001 >> /media/linh/DATA/research/new_cf/result8/new_${f}_8.txt
-    python triple_vae.py --data_dir=../cf-vae/data2/${f}/ --data_type=1p  --iter=400 --learning_rate=0.001 >> /media/linh/DATA/research/new_cf/result9/${f}_9.txt
+    python triple_vae.py --data_dir=../cf-vae/data2/${f}/ --data_type=8p  --iter=400 --learning_rate=0.0001 >> /media/linh/DATA/research/new_cf/result9/${f}_8.txt
 #    mkdir experiment/${f}/
 #    mkdir experiment/${f}/1
 #    python train_vae.py --ckpt_folder=experiment/${f}/1 --data_dir=../cf-vae/data2/${f}/ --data_type=1
