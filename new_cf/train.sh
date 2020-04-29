86ef0440b848713aa909f69c2d0c115ecd7ce589
Linh Nguyen AI <linh@hblab.vn>
2020-04-29 10:50:12 +0900

    retrain

--- new_cf/train.sh
+++ new_cf/train.sh
@@ -5,11 +5,11 @@ for f in $folders; do
     echo ${f}
 #    python cvae_cf.py --data_dir=/media/linh/DATA/research/cf-vae/data2/${f}/ --data_type=1p --iter=400 --learning_rate=0.001 >> /media/linh/DATA/research/new_cf/result9/cvaecf_${f}_1.txt
 #    python cvae_cf.py --data_dir=/media/linh/DATA/research/cf-vae/data2/${f}/ --data_type=8p --iter=400 --learning_rate=0.001 >> /media/linh/DATA/research/new_cf/result9/cvaecf_${f}_8.txt
-    python multiVAE_old.py --data_dir=/media/linh/DATA/research/cf-vae/data2/${f}/ --data_type=1p --iter=200 --learning_rate=0.001 >> /media/linh/DATA/research/new_cf/result8/multi_${f}_1.txt
-    python multiVAE_old.py --data_dir=/media/linh/DATA/research/cf-vae/data2/${f}/ --data_type=8p --iter=200 --learning_rate=0.001 >> /media/linh/DATA/research/new_cf/result8/multi_${f}_8.txt
+#    python multiVAE_old.py --data_dir=/media/linh/DATA/research/cf-vae/data2/${f}/ --data_type=1p --iter=200 --learning_rate=0.001 >> /media/linh/DATA/research/new_cf/result8/multi_${f}_1.txt
+#    python multiVAE_old.py --data_dir=/media/linh/DATA/research/cf-vae/data2/${f}/ --data_type=8p --iter=200 --learning_rate=0.001 >> /media/linh/DATA/research/new_cf/result8/multi_${f}_8.txt
 #    python triple_vae.py --data_dir=/media/linh/DATA/research/cf-vae/data2/${f}/ --data_type=1p  --iter=200 --learning_rate=0.001 >> /media/linh/DATA/research/new_cf/result7/${f}_1.txt
 #    python new_model.py --data_dir=/media/linh/DATA/research/cf-vae/data2/${f}/ --data_type=8p --iter=200 --learning_rate=0.001 >> /media/linh/DATA/research/new_cf/result8/new_${f}_8.txt
-    python triple_vae.py --data_dir=../cf-vae/data2/${f}/ --data_type=1p  --iter=400 --learning_rate=0.001 >> /media/linh/DATA/research/new_cf/result8/${f}_1.txt
+    python triple_vae.py --data_dir=../cf-vae/data2/${f}/ --data_type=8p  --iter=400 --learning_rate=0.0001 >> /media/linh/DATA/research/new_cf/result8/${f}_8.txt
 #    mkdir experiment/${f}/
 #    mkdir experiment/${f}/1
 #    python train_vae.py --ckpt_folder=experiment/${f}/1 --data_dir=../cf-vae/data2/${f}/ --data_type=1
