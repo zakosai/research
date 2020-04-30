faaaa97e4d7ca4447ad7da9bb89d51da4c866985
Linh Nguyen AI <linh@hblab.vn>
2020-04-30 15:15:57 +0900

    finetune

--- new_cf/triple_vae.py
+++ new_cf/triple_vae.py
@@ -109,7 +109,7 @@ class Translation:
         z_item, item_recon, loss_kl_item = self.vae(self.item_info, [], [self.item_info_dim],
                                                     "item", activation=tf.nn.tanh)
         self.loss_item = tf.reduce_mean(tf.reduce_sum(binary_crossentropy(self.item_info, item_recon), axis=1)) +\
-                         loss_kl_item + 10 * tf.losses.get_regularization_loss()
+                         loss_kl_item + 2 * tf.losses.get_regularization_loss()
 
         content_matrix = tf.matmul(z_user, tf.transpose(z_item))
         min = tf.reduce_min(content_matrix, axis=1, keepdims=True)
