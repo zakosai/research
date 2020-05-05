8e87093b9746f16edea15613f36370a65f72e602
Linh Nguyen AI <linh@hblab.vn>
2020-05-05 13:44:31 +0900

    cv

--- new_cf/triple_vae.py
+++ new_cf/triple_vae.py
@@ -73,7 +73,7 @@ class Translation:
     def dae(self, x, encode_dim, decode_dim, scope, reuse=False, activation=None):
         x_ = x
         # x_ = tf.nn.dropout(x_, 0.7)
-        regular = tf.contrib.layers.l2_regularizer(scale=0.1)
+        regular = tf.contrib.layers.l2_regularizer(scale=0.01)
         with tf.variable_scope(scope, reuse=reuse):
             for i in range(len(encode_dim)):
                 x_ = fully_connected(x_, encode_dim[i], activation, scope="enc_%d" % i,
@@ -123,7 +123,7 @@ class Translation:
         # self.loss = loss_kl + self.loss_reconstruct(self.x, self.x_recon) + \
         #             2 * tf.losses.get_regularization_loss()
         self.x_recon = self.dae(x, self.encode_dim, self.decode_dim, "CF", activation=tf.nn.tanh)
-        self.loss = self.loss_reconstruct(self.x, self.x_recon) + 5 * tf.losses.get_regularization_loss()
+        self.loss = self.loss_reconstruct(self.x, self.x_recon) + 0.1 * tf.losses.get_regularization_loss()
 
         self.train_op = tf.train.AdamOptimizer(self.learning_rate).minimize(self.loss)
         self.train_op_user = tf.train.AdamOptimizer(self.learning_rate).minimize(self.loss_user)
