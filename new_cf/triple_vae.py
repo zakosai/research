14a66506e7c8b055018045c54019cb602f06fa4c
Linh Nguyen AI <linh@hblab.vn>
2020-05-05 13:52:54 +0900

    cv

--- new_cf/triple_vae.py
+++ new_cf/triple_vae.py
@@ -101,16 +101,16 @@ class Translation:
         self.item_info = tf.placeholder(tf.float32, [None, self.item_info_dim], name='item_info')
 
         # VAE for user
-        z_user, user_recon, loss_kl_user = self.vae(self.user_info, [], [self.user_info_dim], "user",
+        z_user, user_recon, loss_kl_user = self.vae(self.user_info, [100], [100, self.user_info_dim], "user",
                                                     activation=tf.nn.tanh)
         self.loss_user = tf.reduce_mean(tf.reduce_sum(binary_crossentropy(self.user_info, user_recon), axis=1)) +\
-             loss_kl_user + tf.losses.get_regularization_loss()
+             loss_kl_user + 10 * tf.losses.get_regularization_loss()
 
         # VAE for item
         z_item, item_recon, loss_kl_item = self.vae(self.item_info, [400, 200], [200, 400, self.item_info_dim],
                                                     "item", activation=tf.nn.tanh)
         self.loss_item = tf.reduce_mean(tf.reduce_sum(binary_crossentropy(self.item_info, item_recon), axis=1)) +\
-                         loss_kl_item + tf.losses.get_regularization_loss()
+                         loss_kl_item + 10 * tf.losses.get_regularization_loss()
 
         content_matrix = tf.matmul(z_user, tf.transpose(z_item))
         min = tf.reduce_min(content_matrix, axis=1, keepdims=True)
@@ -140,7 +140,7 @@ def main(args):
     for layer in layers:
         dataset = Dataset(args.data_dir, args.data_type)
         model = Translation(batch_size, dataset.no_item, dataset.user_size, dataset.item_size,
-                            layer, [dataset.no_item], 50, learning_rate=args.learning_rate)
+                            layer, [dataset.no_item], 100, learning_rate=args.learning_rate)
         model.build_model()
 
         sess = tf.Session()
