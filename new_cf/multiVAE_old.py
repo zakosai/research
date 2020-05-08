cef571724ddb8dfd98a49e7bf393e67009238164
Linh Nguyen AI <linh@hblab.vn>
2020-05-08 09:30:02 +0900

    train sparse

--- new_cf/multiVAE_old.py
+++ new_cf/multiVAE_old.py
@@ -139,7 +139,7 @@ def main(args):
                     best_mAP = mAP
             if (i % 10 == 0) and (model.learning_rate >= 1e-6):
                 model.learning_rate /= 10
-        print(layer, " : ", best, ", ", best_ndcg, ", ", best_mAP)
+        print("Layers sparse ", layer, " : ", best, ", ", best_ndcg, ", ", best_mAP)
         tf.keras.backend.clear_session()
 
     # print(best, best_ndcg)
